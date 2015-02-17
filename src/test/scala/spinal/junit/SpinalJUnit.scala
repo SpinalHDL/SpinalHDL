1cc8b368b8289d020e16fe85c4f25548b7f9844b
dolu1990 <papuchoud@hotmail.com>
2015-02-17 14:16:27 +0100

    SBT boot

--- src/test/scala/spinal/junit/SpinalJUnit.scala
+++ src/test/scala/spinal/junit/SpinalJUnit.scala
@@ -46,7 +46,7 @@ abstract class SpinalJUnit {
 
   def checkHDL(mustSuccess: Boolean): Unit = {
     println("GHDL compilation")
-    assert(((s"ghdl -a $getName.vhd src/test/resources/${getName}_tb.vhd" !) == 0) == mustSuccess, if (mustSuccess) "compilation fail" else "Compilation has not fail :(")
+    assert(((s"ghdl -a $getName.vhd ../src/test/resources/${getName}_tb.vhd" !) == 0) == mustSuccess, if (mustSuccess) "compilation fail" else "Compilation has not fail :(")
 
     println("GHDL elaborate")
     assert(((s"ghdl -e ${getName}_tb" !) == 0) == mustSuccess, if (mustSuccess) "compilation fail" else "Compilation has not fail :(")
