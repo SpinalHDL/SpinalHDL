8e3cf15464c604bdb4aaceb5ee233a853b662410
Pierre-André Mudry <pmudry@gmail.com>
2016-04-16 20:24:59 +0200

    Code clean-up - Named type parameters for boolean values - Explicity return types for implicit functions - Typos removal - Source code reformat - Comments grouped

--- core/src/main/scala/spinal/core.scala
+++ core/src/main/scala/spinal/core.scala
@@ -1,36 +1,37 @@
 package spinal
 
 import scala.collection.immutable.Range
-import scala.collection.mutable.ArrayBuffer
 import scala.language.experimental.macros
 
-package object core extends BaseTypeFactory with BaseTypeCast{
-  import languageFeature._
+package object core extends BaseTypeFactory with BaseTypeCast {
+
+  import languageFeature.implicitConversions
+  import languageFeature.postfixOps
+
   implicit lazy val implicitConversions = scala.language.implicitConversions
   implicit lazy val reflectiveCalls = scala.language.reflectiveCalls
   implicit lazy val postfixOps = scala.language.postfixOps
 
+  implicit def IntToBuilder(value: Int) : IntBuilder = new IntBuilder(value)
 
-  implicit def IntToBuilder(value: Int) = new IntBuilder(value)
-  implicit def BigIntToBuilder(value: BigInt) = new BigIntBuilder(value)
-  implicit def DoubleToBuilder(value: Double) = new DoubleBuilder(value)
+  implicit def BigIntToBuilder(value: BigInt) : BigIntBuilder = new BigIntBuilder(value)
 
-  //def enum(param: Symbol*) = MacroTest.enum(param)
+  implicit def DoubleToBuilder(value: Double) : DoubleBuilder = new DoubleBuilder(value)
+
   def enum(param: Symbol*): Any = macro MacroTest.enum_impl
+  //def enum(param: Symbol*) = MacroTest.enum(param)
 
+  implicit def EnumElementToCraft[T <: SpinalEnum](element: SpinalEnumElement[T]): SpinalEnumCraft[T] = element()
+  //  implicit def EnumElementToCraft[T <: SpinalEnum](enumDef : T) : SpinalEnumCraft[T] = enumDef.craft().asInstanceOf[SpinalEnumCraft[T]]
+  //  implicit def EnumElementToCraft2[T <: SpinalEnum](enumDef : SpinalEnumElement[T]) : SpinalEnumCraft[T] = enumDef.craft().asInstanceOf[SpinalEnumCraft[T]]
 
-  implicit def EnumElementToCraft[T <: SpinalEnum](element : SpinalEnumElement[T]) : SpinalEnumCraft[T] = element()
-//  implicit def EnumElementToCraft[T <: SpinalEnum](enumDef : T) : SpinalEnumCraft[T] = enumDef.craft().asInstanceOf[SpinalEnumCraft[T]]
-//  implicit def EnumElementToCraft2[T <: SpinalEnum](enumDef : SpinalEnumElement[T]) : SpinalEnumCraft[T] = enumDef.craft().asInstanceOf[SpinalEnumCraft[T]]
-
-  class IntBuilder(val i: Int) extends AnyVal{
-//    def x[T <: Data](dataType : T) : Vec[T] = Vec(dataType,i)
+  class IntBuilder(val i: Int) extends AnyVal {
+    //    def x[T <: Data](dataType : T) : Vec[T] = Vec(dataType,i)
 
     def downto(start: Int): Range.Inclusive = Range.inclusive(start, i)
 
     def bit = new BitCount(i)
     def exp = new ExpCount(i)
-
     def hr = new STime(i * 3600)
     def min = new STime(i * 60)
     def sec = new STime(i * 1)
@@ -57,81 +58,76 @@ package object core extends BaseTypeFactory with BaseTypeCast{
     def fs = new STime(d * 1e-15)
   }
 
-  def True = Bool(true)
-  def False = Bool(false)
+  val True = Bool(true)
+  val False = Bool(false)
 
-
- // implicit def RegRefToReg[T <: Data](that : RegRef[T]) : T = that.getReg
-
-
-  implicit def IntToUInt(that: Int) = U(that)
-  implicit def BigIntToUInt(that: BigInt) = U(that)
-
-  implicit def IntToSInt(that: Int) = S(that)
-  implicit def BigIntToSInt(that: BigInt) = S(that)
-
-  implicit def IntToBits(that: Int) = B(that)
-  implicit def BigIntToBits(that: BigInt) = B(that)
-
-
-  implicit def StringToBits(that: String) = bitVectorStringParser(spinal.core.B, that,false)
-  implicit def StringToUInt(that: String) = bitVectorStringParser(spinal.core.U, that,false)
-  implicit def StringToSInt(that: String) = bitVectorStringParser(spinal.core.S, that,true)
+  // implicit def RegRefToReg[T <: Data](that : RegRef[T]) : T = that.getReg
+  implicit def IntToUInt(that: Int) : UInt = U(that)
+  implicit def BigIntToUInt(that: BigInt) : UInt = U(that)
+  implicit def IntToSInt(that: Int) : SInt = S(that)
+  implicit def BigIntToSInt(that: BigInt) : SInt = S(that)
+  implicit def IntToBits(that: Int) : Bits = B(that)
+  implicit def BigIntToBits(that: BigInt) : Bits = B(that)
+  implicit def StringToBits(that: String) : Bits = bitVectorStringParser(spinal.core.B, that, signed = false)
+  implicit def StringToUInt(that: String) : UInt = bitVectorStringParser(spinal.core.U, that, signed = false)
+  implicit def StringToSInt(that: String) : SInt = bitVectorStringParser(spinal.core.S, that, signed = true)
 
   implicit class LiteralBuilder(private val sc: StringContext) extends AnyVal {
-    def B(args: Any*): Bits = bitVectorStringParser(spinal.core.B, getString(args),false)
-    def U(args: Any*): UInt = bitVectorStringParser(spinal.core.U, getString(args),false)
-    def S(args: Any*): SInt = bitVectorStringParser(spinal.core.S, getString(args),true)
+    def B(args: Any*): Bits = bitVectorStringParser(spinal.core.B, getString(args), signed = false)
+    def U(args: Any*): UInt = bitVectorStringParser(spinal.core.U, getString(args), signed = false)
+    def S(args: Any*): SInt = bitVectorStringParser(spinal.core.S, getString(args), signed = true)
     def M(args: Any*): MaskedLiteral = MaskedLiteral(sc.parts(0))
-
-
     def Bits(args: Any*): Bits = B(args)
     def UInt(args: Any*): UInt = U(args)
     def SInt(args: Any*): SInt = S(args)
 
     private def getString(args: Any*): String = {
-     // println(sc.parts.size + " " + args.size)
-     // println(sc.parts.head + "-----" + args.head)
-    //  sc.standardInterpolator(_.toString(), args)
+      // println(sc.parts.size + " " + args.size)
+      // println(sc.parts.head + "-----" + args.head)
+      // sc.standardInterpolator(_.toString(), args)
 
       val pi = sc.parts.iterator
       val ai = args.iterator
       val bldr = new StringBuilder(pi.next().toString)
+
       while (ai.hasNext) {
-        if(ai.hasNext && !ai.next.isInstanceOf[List[_]])bldr append ai.next
-        if(pi.hasNext && !pi.next.isInstanceOf[List[_]])bldr append pi.next
+        if (ai.hasNext && !ai.next.isInstanceOf[List[_]]) bldr append ai.next
+        if (pi.hasNext && !pi.next.isInstanceOf[List[_]]) bldr append pi.next
       }
+
       //println(bldr.result)
       bldr.result.replace("_", "")
     }
   }
 
-
-  private[core] def bitVectorStringParser[T <: BitVector](builder: BitVectorLiteralFactory[T], arg: String,signed : Boolean): T = {
+  private[core] def bitVectorStringParser[T <: BitVector](builder: BitVectorLiteralFactory[T], arg: String, signed: Boolean): T = {
     def error() = SpinalError(s"$arg literal is not well formed [bitCount'][radix]value")
 
-    def strBinToInt(valueStr : String,radix : Int,bitCount : Int) = if(!signed){
+    def strBinToInt(valueStr: String, radix: Int, bitCount: Int) = if (!signed) {
       BigInt(valueStr, radix)
-    }else{
+    } else {
       val v = BigInt(valueStr, radix)
       val bitCountPow2 = BigInt(1) << bitCount
-      if(v >= bitCountPow2)  SpinalError("Value is bigger than bit count")
-      if(!v.testBit(bitCount-1)) v else  -bitCountPow2+ v
+      if (v >= bitCountPow2) SpinalError("Value is bigger than bit count")
+      if (!v.testBit(bitCount - 1)) v else -bitCountPow2 + v
     }
 
     var str = arg.replace("_", "").toLowerCase
-    if(str == "") return builder(0,0 bit)
+    if (str == "") return builder(0, 0 bit)
 
     var bitCount = -1
-    if(str.contains(''')){
+
+    if (str.contains(''')) {
       val split = str.split(''')
       bitCount = split(0).toInt
       str = split(1)
     }
+
     var radix = -1
-    if("01".contains(str.charAt(0))){
+
+    if ("01".contains(str.charAt(0))) {
       radix = 2
-    }else{
+    } else {
       radix = str.charAt(0) match {
         case 'x' => 16
         case 'h' => 16
@@ -143,47 +139,44 @@ package object core extends BaseTypeFactory with BaseTypeCast{
       str = str.tail
     }
 
-    val minus = if(str.charAt(0) == '-'){
+    val minus = if (str.charAt(0) == '-') {
       str = str.tail
-      if(radix != 10) SpinalError("Can't have minus on non decimal values")
-     true
-    }else{
+      if (radix != 10) SpinalError("Can't have minus on non decimal values")
+      true
+    } else {
       false
     }
 
     val digitCount = str.length
-    if(bitCount == -1) bitCount = radix match {
-      case 16 =>  digitCount * 4
-      case 8 =>  digitCount * 3
-      case 2 =>  digitCount
+    if (bitCount == -1) bitCount = radix match {
+      case 16 => digitCount * 4
+      case 8 => digitCount * 3
+      case 2 => digitCount
       case _ => -1
     }
 
     val value = radix match {
-      case 10 => if(minus) - BigInt(str, radix) else BigInt(str, radix)
-      case _ =>  strBinToInt(str,radix,bitCount)
+      case 10 => if (minus) -BigInt(str, radix) else BigInt(str, radix)
+      case _ => strBinToInt(str, radix, bitCount)
     }
 
-    if(bitCount == -1){
+    if (bitCount == -1) {
       builder(value)
-    }else{
-      builder(value,bitCount bit)
+    } else {
+      builder(value, bitCount bit)
     }
   }
 
-  //implicit def UIntToLitBuilder(sc: StringContext) = new UIntLitBuilder(sc)
+  implicit def DataPimped[T <: Data](that: T) : DataPimper[T] = new DataPimper(that)
+  implicit def BitVectorPimped[T <: BitVector](that: T)  : BitVectorPimper[T] = new BitVectorPimper(that)
 
-  //  implicit def IntToUInt(that : Int) = UInt(that lit)
-  //  implicit def BigIntToUInt(that : BigInt) = UInt(that lit)
-  //
+  // @TODO Should those be removed ?
+  // implicit def UIntToLitBuilder(sc: StringContext) = new UIntLitBuilder(sc)
+  // implicit def IntToUInt(that : Int) = UInt(that lit)
+  // implicit def BigIntToUInt(that : BigInt) = UInt(that lit)
   // implicit def BooleanToBool(that : Boolean) = Bool(that)
-  implicit def DataPimped[T <: Data](that : T) = new DataPimper(that)
-  implicit def BitVectorPimped[T <: BitVector](that : T) = new BitVectorPimper(that)
-
-//    implicit def autoCast[T <: Data, T2 <: T](that: T): T2#SSelf = that.asInstanceOf[T2#SSelf]
-//  implicit def autoCast[T <: Data](that: T): T#SSelf = that.asInstanceOf[T#SSelf]
-
-
+  // implicit def autoCast[T <: Data, T2 <: T](that: T): T2#SSelf = that.asInstanceOf[T2#SSelf]
+  // implicit def autoCast[T <: Data](that: T): T#SSelf = that.asInstanceOf[T#SSelf]
 
   implicit class SIntPimper(pimped: SInt) {
     def toSFix: SFix = {
@@ -195,12 +188,11 @@ package object core extends BaseTypeFactory with BaseTypeCast{
   }
 
   implicit class UIntPimper(pimped: UInt) {
-    def toUFix : UFix = {
+    def toUFix: UFix = {
       val width = pimped.getWidth
       val fix = UFix(width exp, width bit)
       fix.raw := pimped
       fix
     }
   }
-
 }
