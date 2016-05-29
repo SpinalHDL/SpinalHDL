b18ca19eb837aed5fc79c0288dd2f90d5b95a34d
Dolu1990 <charles.papon.90@gmail.com>
2016-05-29 10:38:03 +0200

    Add bitOffset in AxiLitSlaveFactory

--- lib/src/main/scala/spinal/lib/bus/amba4/axilite/AxiLiteSlaveFactory.scala
+++ lib/src/main/scala/spinal/lib/bus/amba4/axilite/AxiLiteSlaveFactory.scala
@@ -8,27 +8,30 @@ import scala.collection.mutable.ArrayBuffer
 trait AxiLiteSlaveFactoryElement
 
 case class AxiLiteSlaveFactoryRead(that : Data,
-                                   base : BigInt,
+                                   address : BigInt,
+                                   bitOffset : Int,
                                    onRsp :  (BigInt) => Unit) extends AxiLiteSlaveFactoryElement
 case class AxiLiteSlaveFactoryWrite(that : Data,
-                                    base : BigInt,
+                                    address : BigInt,
+                                    bitOffset : Int,
                                     onCmd : (BigInt) => Unit) extends AxiLiteSlaveFactoryElement
-
-
 
 class AxiLiteSlaveFactory(bus : AxiLite) extends Area{
   val elements = ArrayBuffer[AxiLiteSlaveFactoryElement]()
 
   def read(that : Data,
            address : BigInt,
+           bitOffset : Int = null.asInstanceOf[Int],
            onRsp :  (BigInt) => Unit = (x) => {}) : Unit  = {
-    elements += AxiLiteSlaveFactoryRead(that,address,onRsp)
+    elements += AxiLiteSlaveFactoryRead(that,address,bitOffset,onRsp)
   }
   def write(that : Data,
             address : BigInt,
-            onCmd : (BigInt) => Unit = (xy) => {}) : Unit  = {
-    elements += AxiLiteSlaveFactoryWrite(that,address,onCmd)
+            bitOffset : Int = null.asInstanceOf[Int],
+            onCmd : (BigInt) => Unit = (x) => {}) : Unit  = {
+    elements += AxiLiteSlaveFactoryWrite(that,address,bitOffset,onCmd)
   }
+
   def writeOnlyRegOf[T <: Data](dataType: T, baseAddress: BigInt): T = {
     val ret = Reg(dataType)
     write(ret,baseAddress)
@@ -44,7 +47,7 @@ class AxiLiteSlaveFactory(bus : AxiLite) extends Area{
     assert(that.getWidth <= bus.config.dataWidth)
     val reg = Reg(that.clone)
     reg := reg | that
-    read(reg,base,(address) => {
+    read(reg,base,onRsp = (address) => {
       reg := that
     })
   }
@@ -64,12 +67,23 @@ class AxiLiteSlaveFactory(bus : AxiLite) extends Area{
     when(writeJoinEvent.valid){
       for(e <- elements) e match{
         case e : AxiLiteSlaveFactoryWrite => {
-          assert(e.that.isReg,"Should be a Reg")
-          val wordCount = (widthOf(e.that) - 1) / bus.config.dataWidth + 1
-          for (wordId <- (0 until wordCount)) {
-            when(bus.writeCmd.addr === e.base + wordId * bus.config.dataWidth / 8) {
-              e.that.assignFromBits(bus.writeData.data.resized,wordId * bus.config.dataWidth, Math.min(bus.config.dataWidth, widthOf(e.that) - wordId * bus.config.dataWidth) bit)
-              e.onCmd(wordId)
+          if(e.bitOffset != null){
+            assert(e.bitOffset + e.that.getBitsWidth <= bus.config.dataWidth)
+            when(bus.writeCmd.addr === e.address) {
+              e.that.assignFromBits(bus.writeData.data(e.bitOffset,e.that.getBitsWidth bits))
+              when(writeJoinEvent.ready) {
+                e.onCmd(0)
+              }
+            }
+          }else {
+            val wordCount = (widthOf(e.that) - 1) / bus.config.dataWidth + 1
+            for (wordId <- (0 until wordCount)) {
+              when(bus.writeCmd.addr === e.address + wordId * bus.config.dataWidth / 8) {
+                e.that.assignFromBits(bus.writeData.data.resized, wordId * bus.config.dataWidth, Math.min(bus.config.dataWidth, widthOf(e.that) - wordId * bus.config.dataWidth) bit)
+                when(writeJoinEvent.ready) {
+                  e.onCmd(wordId)
+                }
+              }
             }
           }
         }
@@ -79,15 +93,28 @@ class AxiLiteSlaveFactory(bus : AxiLite) extends Area{
 
     //Reads
     //TODO readRsp.resp := OKEY
+    readRsp.data := 0
     for(e <- elements) e match{
       case e : AxiLiteSlaveFactoryRead => {
-        val wordCount = (widthOf(e.that) - 1) / bus.config.dataWidth + 1
-        val valueBits = e.that.asBits.resize(wordCount*bus.config.dataWidth)
-        val words = (0 until wordCount).map(id => valueBits(id * bus.config.dataWidth , bus.config.dataWidth bit))
-        for (wordId <- (0 until wordCount)) {
-          when(readDataStage.addr === e.base + wordId*bus.config.dataWidth/8) {
-            readRsp.data  := words(wordId).resized
-            e.onRsp(wordId)
+        if(e.bitOffset == null){
+          assert(e.bitOffset + e.that.getBitsWidth <= bus.config.dataWidth)
+          when(readDataStage.addr === e.address) {
+            readRsp.data(e.bitOffset,e.that.getBitsWidth bits) := e.that.asBits
+            when(bus.readData.fire) {
+              e.onRsp(0)
+            }
+          }
+        }else{
+          val wordCount = (widthOf(e.that) - 1) / bus.config.dataWidth + 1
+          val valueBits = e.that.asBits.resize(wordCount*bus.config.dataWidth)
+          val words = (0 until wordCount).map(id => valueBits(id * bus.config.dataWidth , bus.config.dataWidth bit))
+          for (wordId <- (0 until wordCount)) {
+            when(readDataStage.addr === e.address + wordId*bus.config.dataWidth/8) {
+              readRsp.data  := words(wordId).resized
+              when(bus.readData.fire) {
+                e.onRsp(wordId)
+              }
+            }
           }
         }
       }
