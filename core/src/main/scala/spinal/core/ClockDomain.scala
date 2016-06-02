01caad78b1d2cc5173e933166b19ea8401dd9656
Dolu1990 <charles.papon.90@gmail.com>
2016-06-02 15:43:40 +0200

    cleaning

--- core/src/main/scala/spinal/core/ClockDomain.scala
+++ core/src/main/scala/spinal/core/ClockDomain.scala
@@ -32,7 +32,6 @@ trait ResetKind
 object ASYNC extends ResetKind
 object SYNC extends ResetKind
 object BOOT extends ResetKind
-object NONE extends ResetKind
 
 trait ActiveKind
 object HIGH extends ActiveKind
