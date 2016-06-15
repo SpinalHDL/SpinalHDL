41c79fbfb93cdfe40b9d8b76fabb3a148202ed69
Dolu1990 <charles.papon.90@gmail.com>
2016-06-15 15:53:40 +0200

    version++

--- project/Version.scala
+++ project/Version.scala
@@ -4,7 +4,7 @@ object SpinalVersion {
 
   val isSnapshot = false
   private def snapshot = if (isSnapshot) "-SNAPSHOT" else ""
-  private val major = "0.8.1"
+  private val major = "0.8.2"
   val all         = s"$major$snapshot"
   val core        = s"$major$snapshot"
   val lib         = s"$major$snapshot"
