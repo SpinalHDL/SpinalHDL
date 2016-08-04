d069124b34d74ff7f68339a6564e36e87854a44c
Dolu1990 <charles.papon.90@gmail.com>
2016-08-04 14:31:42 +0200

    HHz -> Ghz

--- core/src/main/scala/spinal/core.scala
+++ core/src/main/scala/spinal/core.scala
@@ -54,7 +54,7 @@ package object core extends BaseTypeFactory with BaseTypeCast {
 
 
     def THz = (i * BigDecimal(1e12))
-    def HHz = (i * BigDecimal(1e9))
+    def GHz = (i * BigDecimal(1e9))
     def MHz = (i * BigDecimal(1e6))
     def KHz = (i * BigDecimal(1e3))
     def Hz = (i * BigDecimal(1e0))
@@ -78,7 +78,7 @@ package object core extends BaseTypeFactory with BaseTypeCast {
 
 
     def THz = (d * BigDecimal(1e12))
-    def HHz = (d * BigDecimal(1e9))
+    def GHz = (d * BigDecimal(1e9))
     def MHz = (d * BigDecimal(1e6))
     def KHz = (d * BigDecimal(1e3))
     def  Hz = (d * BigDecimal(1e0))
