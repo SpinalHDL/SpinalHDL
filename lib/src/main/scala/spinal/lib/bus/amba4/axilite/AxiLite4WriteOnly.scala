3be2a166b70adb253c3e71b5048b4ce5fce3d0d1
Dolu1990 <charles.papon.90@gmail.com>
2016-08-15 17:42:00 +0200

    Migrate Axilite from a ReadWrite only configuration to separate Bundles

---
+++ lib/src/main/scala/spinal/lib/bus/amba4/axilite/AxiLite4WriteOnly.scala
@@ -0,0 +1,25 @@
+package spinal.lib.bus.amba4.axilite
+
+
+import spinal.core._
+import spinal.lib._
+
+case class AxiLite4WriteOnly(config: AxiLite4Config) extends Bundle with IMasterSlave {
+  val aw = Stream(AxiLite4Ax(config))
+  val w  = Stream(AxiLite4W(config))
+  val b  = Stream(AxiLite4B(config))
+
+
+  def writeCmd  = aw
+  def writeData = w
+  def writeRsp  = b
+
+
+
+  override def asMaster(): this.type = {
+    master(aw,w)
+    slave(b)
+
+    this
+  }
+}
