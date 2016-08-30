176d054643d9a61aff678dadd3c202117d9cc37c
Dolu1990 <charles.papon.90@gmail.com>
2016-08-30 20:41:17 +0200

    version++

--- core/src/main/scala/spinal/core/Spinal.scala
+++ core/src/main/scala/spinal/core/Spinal.scala
@@ -146,7 +146,7 @@ object Spinal{
 
     println({
       SpinalLog.tag("Runtime", Console.YELLOW)
-    } + s" SpinalHDL 0.10.0")
+    } + s" SpinalHDL 0.10.1")
 
 
     val runtime = Runtime.getRuntime
