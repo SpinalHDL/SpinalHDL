9a85e45aa9d71c798ec79475d58ed1aa1a10d0a4
Dolu1990 <charles.papon.90@gmail.com>
2016-09-02 14:39:02 +0200

    pinsec cleaning

--- lib/src/main/scala/spinal/lib/soc/pinsec/Pinsec.scala
+++ lib/src/main/scala/spinal/lib/soc/pinsec/Pinsec.scala
@@ -18,7 +18,7 @@ import spinal.lib.io.TriStateArray
 import spinal.lib.memory.sdram._
 import spinal.lib.system.debugger.{JtagAxi4SharedDebugger, SystemDebuggerConfig}
 
-class Pinsec extends Component{
+class Pinsec(axiFrequency : BigDecimal) extends Component{
   val debug = true
   val interruptCount = 4
   def vgaRgbConfig = RgbConfig(5,6,5)
@@ -27,8 +27,7 @@ class Pinsec extends Component{
     val asyncReset = in Bool
     val axiClk = in Bool
     val vgaClk = in Bool
-    val jtag_tck = in Bool
-    val jtag = slave(Jtag())
+    val jtag  = slave(Jtag(useTck = true))
     val gpioA = master(TriStateArray(32 bits))
     val gpioB = master(TriStateArray(32 bits))
     val timerExternal = in(PinsecTimerCtrlExternal())
@@ -37,9 +36,17 @@ class Pinsec extends Component{
     val vga   = master(Vga(vgaRgbConfig))
   }
 
+  val resetClockDomain = ClockDomain(
+    clock = io.axiClk,
+    config = ClockDomainConfig(
+      resetKind = BOOT
+    )
+  )
 
-  val resetCtrl = new ClockingArea(ClockDomain(io.axiClk,config = ClockDomainConfig(resetKind = BOOT))) {
-    val axiResetOrder = False
+  val resetCtrl = new ClockingArea(resetClockDomain) {
+    val axiResetOrder  = False
+    val coreResetOrder = False setWhen(axiResetOrder)
+
     val axiResetCounter = Reg(UInt(6 bits)) init(0)
     when(axiResetCounter =/= U(axiResetCounter.range -> true)){
       axiResetCounter := axiResetCounter + 1
@@ -49,17 +56,33 @@ class Pinsec extends Component{
       axiResetCounter := 0
     }
 
-    val coreResetOrder = False setWhen(axiResetOrder)
-
     val axiReset  = RegNext(axiResetOrder)
-    val vgaReset  = BufferCC(axiReset)
     val coreReset = RegNext(coreResetOrder)
+    val vgaReset  = BufferCC(axiReset)
   }
 
+  val axiClockDomain = ClockDomain(
+    clock = io.axiClk,
+    reset = resetCtrl.axiReset,
+    frequency = FixedFrequency(axiFrequency)
+  )
 
-  val axi = new ClockingArea(ClockDomain(io.axiClk,resetCtrl.axiReset,frequency = ClockDomain.current.frequency)) {
+  val coreClockDomain = ClockDomain(
+    clock = io.axiClk,
+    reset = resetCtrl.coreReset
+  )
 
-    val core = ClockDomain(io.axiClk,resetCtrl.coreReset){ //The RISCV Core has a separate reset
+  val vgaClockDomain = ClockDomain(
+    clock = io.vgaClk,
+    reset = resetCtrl.vgaReset
+  )
+
+  val jtagClockDomain = ClockDomain(
+    clock = io.jtag.tck
+  )
+
+  val axi = new ClockingArea(axiClockDomain) {
+    val core = coreClockDomain{
       val coreConfig = CoreConfig(
         pcWidth = 32,
         addrWidth = 32,
@@ -90,7 +113,13 @@ class Pinsec extends Component{
         memDataWidth = 32
       )
 
-      new RiscvAxi4(coreConfig, iCacheConfig, null, debug, interruptCount)
+      new RiscvAxi4(
+        coreConfig = coreConfig,
+        iCacheConfig = iCacheConfig,
+        dCacheConfig = null,
+        debug = debug,
+        interruptCount = interruptCount
+      )
     }
 
     val ram = Axi4SharedOnChipRam(
@@ -101,72 +130,85 @@ class Pinsec extends Component{
 
     val sdramCtrl = Axi4SharedSdramCtrl(
       dataWidth = 32,
-      idWidth = 4,
-      layout = IS42x320D.layout,
-      timing = IS42x320D.timingGrade7,
-      CAS = 3
+      idWidth   = 4,
+      layout    = IS42x320D.layout,
+      timing    = IS42x320D.timingGrade7,
+      CAS       = 3
     )
 
     val jtagCtrl = JtagAxi4SharedDebugger(SystemDebuggerConfig(
       memAddressWidth = 32,
-      memDataWidth = 32,
-      remoteCmdWidth = 1,
-      jtagClockDomain = ClockDomain(io.jtag_tck)
+      memDataWidth    = 32,
+      remoteCmdWidth  = 1,
+      jtagClockDomain = jtagClockDomain
     ))
 
 
     val apbBridge = Axi4SharedToApb3Bridge(
       addressWidth = 20,
-      dataWidth = 32,
-      idWidth = 4
+      dataWidth    = 32,
+      idWidth      = 4
     )
 
     val gpioACtrl = Apb3Gpio(32)
     val gpioBCtrl = Apb3Gpio(32)
     val timerCtrl = PinsecTimerCtrl()
-    val uartCtrl = Apb3UartCtrl(UartCtrlMemoryMappedConfig(
+
+    val uartCtrlConfig = UartCtrlMemoryMappedConfig(
       uartCtrlConfig = UartCtrlGenerics(
-        dataWidthMax = 8,
+        dataWidthMax      = 8,
         clockDividerWidth = 20,
-        preSamplingSize = 1,
-        samplingSize = 5,
-        postSamplingSize = 2
+        preSamplingSize   = 1,
+        samplingSize      = 5,
+        postSamplingSize  = 2
       ),
       txFifoDepth = 16,
       rxFifoDepth = 16
-    ))
+    )
+    val uartCtrl = Apb3UartCtrl(uartCtrlConfig)
 
-    val vgaCtrl = Axi4VgaCtrl(Axi4VgaCtrlGenerics(
+
+    val vgaCtrlConfig = Axi4VgaCtrlGenerics(
       axiAddressWidth = 32,
-      axiDataWidth = 32,
-      burstLength = 8,
-      frameSizeMax = 2048*1512*2,
-      fifoSize = 512,
-      rgbConfig = vgaRgbConfig,
-      vgaClock = ClockDomain(io.vgaClk,resetCtrl.vgaReset)
-    ))
+      axiDataWidth    = 32,
+      burstLength     = 8,
+      frameSizeMax    = 2048*1512*2,
+      fifoSize        = 512,
+      rgbConfig       = vgaRgbConfig,
+      vgaClock        = vgaClockDomain
+    )
+    val vgaCtrl = Axi4VgaCtrl(vgaCtrlConfig)
 
     val axiCrossbar = Axi4CrossbarFactory()
-      .addSlaves(
-        ram.io.axi       ->(0x00000000L ,  4 kB),
-        sdramCtrl.io.axi ->(0x40000000L,  64 MB),
-        apbBridge.io.axi ->(0xF0000000L,   1 MB)
-      ).addConnections(
-        core.io.i       -> List(ram.io.axi, sdramCtrl.io.axi),
-        core.io.d       -> List(ram.io.axi, sdramCtrl.io.axi, apbBridge.io.axi),
-        jtagCtrl.io.axi -> List(ram.io.axi, sdramCtrl.io.axi, apbBridge.io.axi),
-        vgaCtrl.io.axi  -> List(                        sdramCtrl.io.axi)
-      ).addPipelining(apbBridge.io.axi,(crossbar,bridge) => {
-        crossbar.sharedCmd.halfPipe() >> bridge.sharedCmd
-        crossbar.writeData.halfPipe() >> bridge.writeData
-        crossbar.writeRsp << bridge.writeRsp
-        crossbar.readRsp << bridge.readRsp
-      }).addPipelining(sdramCtrl.io.axi,(crossbar,ctrl) => {
-        crossbar.sharedCmd.halfPipe() >> ctrl.sharedCmd
-        crossbar.writeData >/-> ctrl.writeData
-        crossbar.writeRsp << ctrl.writeRsp
-        crossbar.readRsp << ctrl.readRsp
-      }).build()
+
+    axiCrossbar.addSlaves(
+      ram.io.axi       -> (0x00000000L,   4 kB),
+      sdramCtrl.io.axi -> (0x40000000L,  64 MB),
+      apbBridge.io.axi -> (0xF0000000L,   1 MB)
+    )
+
+    axiCrossbar.addConnections(
+      core.io.i       -> List(ram.io.axi, sdramCtrl.io.axi),
+      core.io.d       -> List(ram.io.axi, sdramCtrl.io.axi, apbBridge.io.axi),
+      jtagCtrl.io.axi -> List(ram.io.axi, sdramCtrl.io.axi, apbBridge.io.axi),
+      vgaCtrl.io.axi  -> List(                        sdramCtrl.io.axi)
+    )
+
+    axiCrossbar.addPipelining(apbBridge.io.axi,(crossbar,bridge) => {
+      crossbar.sharedCmd.halfPipe() >> bridge.sharedCmd
+      crossbar.writeData.halfPipe() >> bridge.writeData
+      crossbar.writeRsp             << bridge.writeRsp
+      crossbar.readRsp              << bridge.readRsp
+    })
+
+    axiCrossbar.addPipelining(sdramCtrl.io.axi,(crossbar,ctrl) => {
+      crossbar.sharedCmd.halfPipe()  >>  ctrl.sharedCmd
+      crossbar.writeData            >/-> ctrl.writeData
+      crossbar.writeRsp              <<  ctrl.writeRsp
+      crossbar.readRsp               <<  ctrl.readRsp
+    })
+
+    axiCrossbar.build()
 
 
     val apbDecoder = Apb3Decoder(
@@ -195,20 +237,20 @@ class Pinsec extends Component{
     }
   }
 
-  io.gpioA         <> axi.gpioACtrl.io.gpio
-  io.gpioB         <> axi.gpioBCtrl.io.gpio
-  io.timerExternal <> axi.timerCtrl.io.external
-  io.jtag          <> axi.jtagCtrl.io.jtag
-  io.uart          <> axi.uartCtrl.io.uart
-  io.sdram         <> axi.sdramCtrl.io.sdram
-  io.vga           <> axi.vgaCtrl.io.vga
+  io.gpioA          <> axi.gpioACtrl.io.gpio
+  io.gpioB          <> axi.gpioBCtrl.io.gpio
+  io.timerExternal  <> axi.timerCtrl.io.external
+  io.jtag.unclocked <> axi.jtagCtrl.io.jtag
+  io.uart           <> axi.uartCtrl.io.uart
+  io.sdram          <> axi.sdramCtrl.io.sdram
+  io.vga            <> axi.vgaCtrl.io.vga
 }
 
 
 object Pinsec{
   def main(args: Array[String]) {
-    val config = SpinalConfig(defaultClockDomainFrequency = FixedFrequency(100 MHz)).dumpWave()
-    config.generateVerilog(new Pinsec)
-    config.generateVhdl(new Pinsec)
+    val config = SpinalConfig().dumpWave()
+    config.generateVerilog(new Pinsec(100 MHz))
+    config.generateVhdl(new Pinsec(100 MHz))
   }
 }
