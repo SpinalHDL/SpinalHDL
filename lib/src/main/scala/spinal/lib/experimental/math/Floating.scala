6a517ca1b8749f51d2c47349eec2824903339bf7
Sebastien Van Cauwenberghe <svancau@gmail.com>
2016-09-09 11:54:23 +0200

    Allow Float different than 32 bits to work

--- lib/src/main/scala/spinal/lib/experimental/math/Floating.scala
+++ lib/src/main/scala/spinal/lib/experimental/math/Floating.scala
@@ -95,6 +95,12 @@ case class RecFloating(exponentSize: Int,
   /** Sign field (true when negative) */
   val sign = Bool
 
+  /** Value of the recoded exponent corresponding to the smallest exponent */
+  def getExponentZero = (1 << (exponentSize - 2)) + 1
+
+  /** Value of the exponent bias for this float configuration */
+  def getExponentBias = ((1 << (exponentSize - 2)) - 1)
+
   private def isHighSubnormal = exponent(exponentSize - 3 downto 0).asUInt < 2
 
   /** Return true if the number is subnormal */
@@ -172,8 +178,7 @@ case class RecFloating(exponentSize: Int,
     val normalizedInt = (inputValue << normalizationOffset)(inputSize - 1 downto inputSize - mantissaSize)
     this.mantissa := normalizedInt.asBits
 
-    // 0x81 is the reencoded exponent value corresponding to 0 offset in IEEE 754
-    val exponent = (U(0x81 + ((1 << (exponentSize - 2)) - 1) + offset, exponentSize bits) + exponentValue.resize(exponentSize)).asBits
+    val exponent = (U(getExponentZero + getExponentBias + offset, exponentSize bits) + exponentValue.resize(exponentSize)).asBits
     val isZero = that.orR
     this.exponent := (exponent(exponent.high) && isZero) ## (exponent(exponent.high - 1) && isZero) ##
       (exponent(exponent.high - 2) && isZero) ##
@@ -195,7 +200,7 @@ case class RecFloating(exponentSize: Int,
     val isNotZero = exponent(exponentSize-1 downto exponentSize-3).orR
     val extendedMantissa = Bits(width bits)
     extendedMantissa := (isNotZero ## mantissa).resizeLeft(width)
-    val exponentOffset = exponent.asUInt - U(0x81 + ((1 << (exponentSize - 2)) - 1))
+    val exponentOffset = exponent.asUInt - U(getExponentZero + getExponentBias)
     val shift = width - 1 - exponentOffset - offset
     val outputMantissa = (extendedMantissa >> shift)
     outputMantissa.asUInt
@@ -236,9 +241,8 @@ case class RecFloating(exponentSize: Int,
 
     val normalizedInt = (inputValue << normalizationOffset)(inputSize - 1 downto inputSize - mantissaSize)
     this.mantissa := normalizedInt.asBits
-
-    // 0x81 is the reencoded exponent value corresponding to 0 offset in IEEE 754
-    def exponent = (U(0x81 + ((1 << (exponentSize - 2)) - 1) + offset, exponentSize bits) + exponentValue.resize(exponentSize)).asBits
+    
+    def exponent = (U(getExponentZero + getExponentBias + offset, exponentSize bits) + exponentValue.resize(exponentSize)).asBits
     val isZero = that.orR
     this.exponent := (exponent(exponent.high) && isZero) ## (exponent(exponent.high - 1) && isZero) ##
       (exponent(exponent.high - 2) && isZero) ##
@@ -260,7 +264,7 @@ case class RecFloating(exponentSize: Int,
     val isNotZero = exponent(exponentSize-1 downto exponentSize-3).orR
     val extendedMantissa = Bits(32 bits)
     extendedMantissa := (isNotZero ## mantissa).resizeLeft(width)
-    val exponentOffset = exponent.asUInt - U(0x81 + ((1 << (exponentSize - 2)) - 1))
+    val exponentOffset = exponent.asUInt - U(getExponentZero + getExponentBias)
     val shift = width - 1 - exponentOffset - offset
     val outputMantissa = (extendedMantissa >> shift)
     val signedMantissa = (outputMantissa ^ B(width bits, (default -> sign))).asSInt - sign.asSInt
