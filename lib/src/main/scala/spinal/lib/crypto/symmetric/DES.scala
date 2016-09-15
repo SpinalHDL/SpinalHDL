224a9a1f2aa1b3f99772c5bc0389689fc5af7a3d
Morard Dany <danymorard@hotmail.com>
2016-09-15 21:51:39 +0200

    DES code cleaning

--- lib/src/main/scala/spinal/lib/crypto/symmetric/DES.scala
+++ lib/src/main/scala/spinal/lib/crypto/symmetric/DES.scala
@@ -1,5 +1,5 @@
 /******************************************************************************
-  * DES Block
+  * Data Encryption Standard (DES)
   *                      _________
   *                     |         |
   *    -- Plaintext --->|   DES   |-- Ciphertext -->
@@ -14,67 +14,103 @@ package spinal.lib.crypto.symmetric
 import spinal.core._
 import spinal.lib._
 
-case class DESGenerics(initialPermutation  : Seq[Int] = Seq(58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4,
-                                                            62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8,
-                                                            57, 49, 41, 33, 25, 17, 9,  1, 59, 51, 43, 35, 27, 19, 11, 3,
-                                                            61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7),
-                       finalPermutation    : Seq[Int] = Seq(40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31,
-                                                            38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29,
-                                                            36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27,
-                                                            34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9,  49, 17, 57, 25),
-                       expansion           : List[Int] = List(32,  1,  2,  3,  4,  5,  4,  5,  6,  7,  8,  9,
-                                                               8,  9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17,
-                                                              16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25,
-                                                              24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32,  1),
-                       fixedPermutation    : List[Int] =   List(16,  7 , 20, 21, 29, 12, 28, 17,  1, 15, 23, 26,  5, 18, 31, 10,
-                                                                 2,  8, 24, 14, 32, 27,  3,  9, 19, 13, 30,  6, 22, 11,  4, 25),
-                       pc_1                : Seq[Int] =   Seq(57, 49, 41, 33, 25, 17,  9,  1, 58, 50, 42, 34, 26, 18,
-                                                              10,  2, 59, 51, 43, 35, 27, 19, 11,  3, 60, 52, 44, 36,
-                                                              63, 55, 47, 39, 31, 23, 15,  7, 62, 54, 46, 38, 30, 22,
-                                                              14,  6, 61, 53, 45, 37, 29, 21, 13,  5, 28, 20, 12,  4),
-                       pc_2                : Seq[Int] =   Seq(14, 17, 11, 24,  1,  5,  3, 28, 15,  6, 21, 10,
-                                                              23, 19, 12,  4, 26,  8, 16,  7, 27, 20, 13,  2,
-                                                              41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
-                                                              44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32),
-                       sBox_1              : List[Int] = List(14,  4, 13, 1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9, 0,  7,
-                                                               0, 15,  7, 4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5, 3,  8,
-                                                               4,  1, 14, 8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10, 5,  0,
-                                                              15, 12,  8, 2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0, 6, 13),
-                       sBox_2              : List[Int] = List(15,  1,  8, 14,  6, 11,  3,  4,  9, 7,  2, 13, 12, 0,  5, 10,
-                                                               3, 13,  4,  7, 15,  2,  8, 14, 12, 0,  1, 10,  6, 9, 11,  5,
-                                                               0, 14,  7, 11, 10,  4, 13,  1,  5, 8, 12,  6,  9, 3,  2, 15,
-                                                               13,  8, 10,  1,  3, 15,  4,  2, 11, 6,  7, 12,  0, 5, 14,  9),
-                       sBox_3              : List[Int] = List(10,  0,  9, 14, 6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8,
-                                                              13,  7,  0,  9, 3,  4,  6, 10,  2,  8,  5, 14, 12, 11, 15,  1,
-                                                              13,  6,  4,  9, 8, 15,  3,  0, 11,  1,  2, 12,  5, 10, 14,  7,
-                                                              1, 10, 13,  0, 6,  9,  8,  7,  4, 15, 14,  3, 11,  5,  2, 12),
-                       sBox_4              : List[Int] = List( 7, 13, 14, 3,  0,  6,  9, 10,  1, 2, 8,  5, 11, 12,  4, 15,
-                                                              13,  8, 11, 5,  6, 15,  0,  3,  4, 7, 2, 12,  1, 10, 14,  9,
-                                                              10,  6,  9, 0, 12, 11,  7, 13, 15, 1, 3, 14,  5,  2,  8,  4,
-                                                              3, 15,  0, 6, 10,  1, 13,  8,  9, 4, 5, 11, 12,  7,  2, 14),
-                       sBox_5              : List[Int] = List( 2, 12,  4,  1,  7, 10, 11,  6,  8,  5,  3, 15, 13, 0, 14, 9,
-                                                              14, 11,  2, 12,  4,  7, 13,  1,  5,  0, 15, 10,  3, 9,  8,  6,
-                                                               4,  2,  1, 11, 10, 13,  7,  8, 15,  9, 12,  5,  6, 3,  0, 14,
-                                                              11,  8, 12,  7,  1, 14,  2, 13,  6, 15,  0,  9, 10, 4,  5,  3),
-                       sBox_6              : List[Int] = List(12,  1, 10, 15, 9,  2,  6,  8,  0, 13,  3,  4, 14,  7,  5, 11,
-                                                              10, 15,  4,  2, 7, 12,  9,  5,  6,  1, 13, 14,  0, 11,  3,  8,
-                                                               9, 14, 15,  5, 2,  8, 12,  3,  7,  0,  4, 10,  1, 13, 11,  6,
-                                                               4,  3,  2, 12, 9,  5, 15, 10, 11, 14,  1,  7,  6,  0,  8, 13),
-                       sBox_7              : List[Int] = List( 4, 11,  2, 14, 15, 0,  8, 13,  3, 12, 9,  7,  5, 10, 6,  1,
-                                                               13,  0, 11,  7,  4, 9,  1, 10, 14,  3, 5, 12,  2, 15, 8,  6,
-                                                               1,  4, 11, 13, 12, 3,  7, 14, 10, 15, 6,  8,  0,  5, 9,  2,
-                                                               6, 11, 13,  8,  1, 4, 10,  7,  9,  5, 0, 15, 14,  2, 3, 12),
-                       sBox_8              : List[Int] = List(13,  2,  8, 4,  6, 15, 11,  1, 10,  9,  3, 14,  5,  0, 12,  7,
-                                                               1, 15, 13, 8, 10,  3,  7,  4, 12,  5,  6, 11,  0, 14,  9,  2,
-                                                               7, 11,  4, 1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8,
-                                                               2,  1, 14, 7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11)
-                      ){
+case class DESGenerics(){
 
-  val blockWidth : BitCount = 64 bits
-  val keyWidth   : BitCount = 56 bits
-  val nbrRound   : Int      = 16
+  val initialPermutation = Seq(58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4,
+                               62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8,
+                               57, 49, 41, 33, 25, 17, 9,  1, 59, 51, 43, 35, 27, 19, 11, 3,
+                               61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7)
 
-  val oneShiftRound = List(1,2,9,16) // 1 left for 1,2,9,16 and others 2 left shift
+  val finalPermutation   = Seq(40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31,
+                               38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29,
+                               36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27,
+                               34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9,  49, 17, 57, 25)
+
+  val expansion  = List(32,  1,  2,  3,  4,  5,  4,  5,  6,  7,  8,  9,
+                         8,  9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17,
+                        16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25,
+                        24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32,  1)
+
+  val fixedPermutation  =   List(16,  7 , 20, 21, 29, 12, 28, 17,  1, 15, 23, 26,  5, 18, 31, 10,
+                                  2,  8, 24, 14, 32, 27,  3,  9, 19, 13, 30,  6, 22, 11,  4, 25)
+
+  val pc_1   =  Seq(57, 49, 41, 33, 25, 17,  9,  1, 58, 50, 42, 34, 26, 18,
+                    10,  2, 59, 51, 43, 35, 27, 19, 11,  3, 60, 52, 44, 36,
+                    63, 55, 47, 39, 31, 23, 15,  7, 62, 54, 46, 38, 30, 22,
+                    14,  6, 61, 53, 45, 37, 29, 21, 13,  5, 28, 20, 12,  4)
+
+  val pc_2    =   Seq(14, 17, 11, 24,  1,  5,  3, 28, 15,  6, 21, 10,
+                      23, 19, 12,  4, 26,  8, 16,  7, 27, 20, 13,  2,
+                      41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
+                      44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32)
+
+  /* SBox definition  */
+  val sBox_1   = List(14,  4, 13, 1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9, 0,  7,
+                       0, 15,  7, 4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5, 3,  8,
+                       4,  1, 14, 8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10, 5,  0,
+                      15, 12,  8, 2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0, 6, 13)
+
+  val sBox_2   = List(15,  1,  8, 14,  6, 11,  3,  4,  9, 7,  2, 13, 12, 0,  5, 10,
+                       3, 13,  4,  7, 15,  2,  8, 14, 12, 0,  1, 10,  6, 9, 11,  5,
+                       0, 14,  7, 11, 10,  4, 13,  1,  5, 8, 12,  6,  9, 3,  2, 15,
+                      13,  8, 10,  1,  3, 15,  4,  2, 11, 6,  7, 12,  0, 5, 14,  9)
+
+  val sBox_3   = List(10,  0,  9, 14, 6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8,
+                      13,  7,  0,  9, 3,  4,  6, 10,  2,  8,  5, 14, 12, 11, 15,  1,
+                      13,  6,  4,  9, 8, 15,  3,  0, 11,  1,  2, 12,  5, 10, 14,  7,
+                       1, 10, 13,  0, 6,  9,  8,  7,  4, 15, 14,  3, 11,  5,  2, 12)
+
+  val sBox_4   = List( 7, 13, 14, 3,  0,  6,  9, 10,  1, 2, 8,  5, 11, 12,  4, 15,
+                      13,  8, 11, 5,  6, 15,  0,  3,  4, 7, 2, 12,  1, 10, 14,  9,
+                      10,  6,  9, 0, 12, 11,  7, 13, 15, 1, 3, 14,  5,  2,  8,  4,
+                       3, 15,  0, 6, 10,  1, 13,  8,  9, 4, 5, 11, 12,  7,  2, 14)
+
+  val sBox_5   = List( 2, 12,  4,  1,  7, 10, 11,  6,  8,  5,  3, 15, 13, 0, 14, 9,
+                      14, 11,  2, 12,  4,  7, 13,  1,  5,  0, 15, 10,  3, 9,  8,  6,
+                       4,  2,  1, 11, 10, 13,  7,  8, 15,  9, 12,  5,  6, 3,  0, 14,
+                      11,  8, 12,  7,  1, 14,  2, 13,  6, 15,  0,  9, 10, 4,  5,  3)
+
+  val sBox_6   = List(12,  1, 10, 15, 9,  2,  6,  8,  0, 13,  3,  4, 14,  7,  5, 11,
+                      10, 15,  4,  2, 7, 12,  9,  5,  6,  1, 13, 14,  0, 11,  3,  8,
+                       9, 14, 15,  5, 2,  8, 12,  3,  7,  0,  4, 10,  1, 13, 11,  6,
+                       4,  3,  2, 12, 9,  5, 15, 10, 11, 14,  1,  7,  6,  0,  8, 13)
+
+  val sBox_7   = List( 4, 11,  2, 14, 15, 0,  8, 13,  3, 12, 9,  7,  5, 10, 6,  1,
+                      13,  0, 11,  7,  4, 9,  1, 10, 14,  3, 5, 12,  2, 15, 8,  6,
+                       1,  4, 11, 13, 12, 3,  7, 14, 10, 15, 6,  8,  0,  5, 9,  2,
+                       6, 11, 13,  8,  1, 4, 10,  7,  9,  5, 0, 15, 14,  2, 3, 12)
+
+  val sBox_8   = List(13,  2,  8, 4,  6, 15, 11,  1, 10,  9,  3, 14,  5,  0, 12,  7,
+                       1, 15, 13, 8, 10,  3,  7,  4, 12,  5,  6, 11,  0, 14,  9,  2,
+                       7, 11,  4, 1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8,
+                       2,  1, 14, 7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11)
+
+  /* Data width */
+  val blockWidth     = 64 bits
+  val keyWidth       = 56 bits
+  val keyWidthParity = 8  bits
+
+  /* Number of Round used by DES*/
+  val nbrRound       = 16
+
+  /* Used by the key scheduling */
+  val oneShiftRound = List(1,2,9,16)  // 1 left rotation for round 1,2,9,16 and others 2 left shift rotation
+}
+
+object DES{
+
+  /** Permutation
+    *  This function permute the vector thanks to the table
+    *  The table is a list of index, this list is given for a software application, it means that index 0 is
+    *  the bit on the left.
+    */
+  def permutation(table:Seq[Int], vector:Bits):Bits={ expansion(table.toList, vector) }
+
+  def compression(table:Seq[Int], vector:Bits):Bits={ expansion(table.toList,vector)  }
+
+  def expansion(table:List[Int], vector:Bits):Bits={
+    Cat(table.reverse.map(index => vector(vector.getWidth - index)))
+  }
 }
 
 // TODO : Implement the decrypt function
@@ -82,14 +118,17 @@ case class DESConfig() extends Bundle{
   val encDec = Bool
 }
 
+
 case class DEScmd(g : DESGenerics) extends Bundle{
-  val key   = Bits(64 bits)
+  val key   = Bits(g.keyWidth.value + g.keyWidthParity.value bits)
   val block = Bits(g.blockWidth)
 }
+
 
 case class DESrsp(g: DESGenerics) extends Bundle{
   val block = Bits(g.blockWidth)
 }
+
 
 class DES(g : DESGenerics) extends Component{
 
@@ -100,10 +139,10 @@ class DES(g : DESGenerics) extends Component{
 
   io.cmd.ready  := False
 
-
-  val roundNbr = UInt(log2Up(g.nbrRound) bits)
-  val init : Bool = io.cmd.valid.rise(False)
-  val nextRound   = Reg(Bool) init(False) setWhen(init) clearWhen(roundNbr === (g.nbrRound))
+  val roundNbr    = UInt(log2Up(g.nbrRound) bits)
+  val init        = io.cmd.valid.rise(False)
+  val nextRound   = Reg(Bool) init(False) setWhen(init) clearWhen(roundNbr === (g.nbrRound-2))
+  val rspValid    = Reg(Bool) init(False) setWhen(roundNbr === (g.nbrRound-2)) clearWhen(init)
 
 
   /**
@@ -122,20 +161,20 @@ class DES(g : DESGenerics) extends Component{
   /**
     * Initial permutation
     */
-  val initialPermutation = new Area{
-    val perm = Reverse(Cat(g.initialPermutation.map(index => io.cmd.block(64 - index))))
+  val initialBlockPermutation = new Area{
+    val perm = DES.permutation(g.initialPermutation, io.cmd.block)
   }
 
 
   /**
     * Key scheduling
     */
-  val keySchedule = new Area{
+  val keyScheduling = new Area{
 
     val shiftKey   = Reg(Bits(g.keyWidth))
 
     // parity drop : 64bits -> 56 bits
-    when(init){ shiftKey := Reverse(Cat(g.pc_1.map(index => io.cmd.key(64 - index)))) }
+    when(init){ shiftKey := DES.compression(g.pc_1, io.cmd.key) }
 
     // rotate left the key (key is divided into two groups of 28 bits)
     val shiftRes   = Bits(g.keyWidth)
@@ -149,60 +188,59 @@ class DES(g : DESGenerics) extends Component{
     when(nextRound){ shiftKey := shiftRes }
 
     // compression : (56bits -> 48 bits)
-    val keyRound = Reverse(Cat(g.pc_2.map(index => shiftRes(56 - index))))
+    val keyRound = DES.compression(g.pc_2, shiftRes)
   }
 
 
   /**
-    * Des function
+    * DES function
     */
   val funcDES = new Area{
 
     // list of SBox ROM 1 to 8
-    val sBox     = List(Mem(Bits(4 bits), g.sBox_1.map(B(_))),
-                        Mem(Bits(4 bits), g.sBox_2.map(B(_))),
-                        Mem(Bits(4 bits), g.sBox_3.map(B(_))),
-                        Mem(Bits(4 bits), g.sBox_4.map(B(_))),
-                        Mem(Bits(4 bits), g.sBox_5.map(B(_))),
-                        Mem(Bits(4 bits), g.sBox_6.map(B(_))),
+    val sBox     = List(Mem(Bits(4 bits), g.sBox_8.map(B(_))),
                         Mem(Bits(4 bits), g.sBox_7.map(B(_))),
-                        Mem(Bits(4 bits), g.sBox_8.map(B(_))))
+                        Mem(Bits(4 bits), g.sBox_6.map(B(_))),
+                        Mem(Bits(4 bits), g.sBox_5.map(B(_))),
+                        Mem(Bits(4 bits), g.sBox_4.map(B(_))),
+                        Mem(Bits(4 bits), g.sBox_3.map(B(_))),
+                        Mem(Bits(4 bits), g.sBox_2.map(B(_))),
+                        Mem(Bits(4 bits), g.sBox_1.map(B(_))))
 
-    val rightRound   = Bits(32 bits) // set in roundArea
+    val rightRound   = Bits(32 bits) // set in feistelNetwork
 
     // xor the key with the right block expanded(32 bits -> 48 bits)
-    val xorRes = keySchedule.keyRound ^ Reverse(Cat(g.expansion.map(index => rightRound(32 - index))))
+    val xorRes = keyScheduling.keyRound ^ DES.expansion(g.expansion, rightRound)
 
     // sBox stage
     val boxRes   = Bits(32 bits)
     for(i <- 0 until sBox.size){
       val addrSBox = xorRes(i*6+6-1 downto i*6)
-      boxRes(i*4+4-1 downto i*4) := sBox(sBox.size-i-1).readAsync( (addrSBox(5) ## addrSBox(0) ## addrSBox(4 downto 1)).asUInt )
+      boxRes(i*4+4-1 downto i*4) := sBox(i).readAsync( (addrSBox(5) ## addrSBox(0) ## addrSBox(4 downto 1)).asUInt )
     }
 
     // fixed permutation
-    val rResult = Reverse(Cat(g.fixedPermutation.map(index => boxRes(32 - index))))
+    val rResult = DES.permutation(g.fixedPermutation, boxRes)
   }
 
 
   /**
-    * Feistel
+    * Feistel network
     */
-  val feistel = new Area{
+  val feistelNetwork = new Area{
 
-    val inBlock = Reg(Bits(g.blockWidth))
-
+    val inBlock  = Reg(Bits(g.blockWidth))
     val outBlock = Bits(g.blockWidth)
 
     val xorLeft = inBlock(63 downto 32) ^ funcDES.rResult
 
-    when(roundNbr === 15){
+    when(roundNbr === g.nbrRound-1){ // last round
       outBlock := xorLeft ## inBlock(31 downto 0)
     }otherwise{
       outBlock := inBlock(31 downto 0) ## xorLeft
     }
 
-    when(init){ inBlock := initialPermutation.perm }
+    when(init){ inBlock := initialBlockPermutation.perm }
     when(nextRound){ inBlock := outBlock }
 
     funcDES.rightRound  := inBlock(31 downto 0)
@@ -212,11 +250,10 @@ class DES(g : DESGenerics) extends Component{
   /**
     * Final Permutation of the Block
     */
-  val finalPermutation = new Area{
-    val perm = Reverse(Cat(g.finalPermutation.map(index => feistel.outBlock(64 - index))))
+  val finalBlockPermutation = new Area{
+    val perm = DES.permutation(g.finalPermutation, feistelNetwork.outBlock)
   }
 
-  io.res.block := finalPermutation.perm // TODOD : regNext output ??
-  io.res.valid := False
-
+  io.res.block := finalBlockPermutation.perm // TODO : regNext output ??
+  io.res.valid := rspValid
 }
