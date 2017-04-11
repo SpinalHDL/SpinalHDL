f8130ec95dd8b4d739cccabd2caaea2c74f92aac
Dolu1990 <charles.papon.90@gmail.com>
2017-04-11 23:09:05 +0200

    Axi4SpecRenamer now support all variation of the Axi4 bus

--- lib/src/main/scala/spinal/lib/bus/amba4/axi/Misc.scala
+++ lib/src/main/scala/spinal/lib/bus/amba4/axi/Misc.scala
@@ -1,11 +1,11 @@
 package spinal.lib.bus.amba4.axi
 
 import spinal.core._
-
+import spinal.lib.{IMasterSlave, slave, master}
 
 
 object  Axi4SpecRenamer{
-  def apply(that : Axi4): Unit ={
+  def apply[T <: Bundle with Axi4Bus](that : T): T ={
     def doIt = {
       that.flatten.foreach((bt) => {
         bt.setName(bt.getName().replace("_payload_",""))
@@ -18,5 +18,33 @@ object  Axi4SpecRenamer{
       that.component.addPrePopTask(() => {doIt})
     else
       doIt
+
+    that
+  }
+  
+  def main(args: Array[String]) {
+    SpinalVhdl(new Component{
+      val c = Axi4Config(32,32,4)
+      val masters = new Bundle{
+        def setup[T <: Bundle with IMasterSlave with Axi4Bus](that : T) = Axi4SpecRenamer(master(that))
+        val rw = setup(Axi4(c))
+        val ro = setup(Axi4ReadOnly(c))
+        val wo = setup(Axi4WriteOnly(c))
+        val srw = setup(Axi4Shared(c))
+      }
+      val slaves = new Bundle{
+        def setup[T <: Bundle with IMasterSlave with Axi4Bus](that : T) = Axi4SpecRenamer(slave(that))
+        val rw = setup(Axi4(c))
+        val ro = setup(Axi4ReadOnly(c))
+        val wo = setup(Axi4WriteOnly(c))
+        val srw = setup(Axi4Shared(c))
+      }
+
+      masters.rw <> slaves.rw
+      masters.ro <> slaves.ro
+      masters.wo <> slaves.wo
+      masters.srw <> slaves.srw
+
+    }.setDefinitionName("TopLevel"))
   }
 }
