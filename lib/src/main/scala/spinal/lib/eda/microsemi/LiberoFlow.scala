2a0cd985ca6d7295ba6ad0bc65778f670c67b63d
Oliver A. Gubler <oliver.gubler@gmail.com>
2017-09-14 13:34:39 +0200

    NEW: implemented getFMax and added Total Core %

--- lib/src/main/scala/spinal/lib/eda/microsemi/LiberoFlow.scala
+++ lib/src/main/scala/spinal/lib/eda/microsemi/LiberoFlow.scala
@@ -38,25 +38,27 @@ object LiberoFlow {
     val device_package = device_parameters(1)
     val device_speed = device_parameters(2)
 
-
     val tcl = new java.io.FileWriter(Paths.get(workspacePath, "doit.tcl").toFile)
     tcl.write(
 s"""new_project\\
-                -location {${Paths.get(workspacePath, projectName)}} \\
-                -name {$projectName} \\
-                -project_description {} \\
-                -hdl {${if(isVhdl) "VHDL" else "VERILOG"}} \\
-                -family {$family} \\
-                -die {$device_die} \\
-                -package {$device_package} \\
-                -speed {-$device_speed}
+  -location {${Paths.get(workspacePath, projectName)}} \\
+  -name {$projectName} \\
+  -project_description {} \\
+  -hdl {${if(isVhdl) "VHDL" else "VERILOG"}} \\
+  -family {$family} \\
+  -die {$device_die} \\
+  -package {$device_package} \\
+  -speed {-$device_speed}
 
 create_links \\
-	 -hdl_source {${new File(toplevelPath).getAbsolutePath}}
+  -hdl_source {${new File(toplevelPath).getAbsolutePath}}
 
 run_tool -name {SYNTHESIZE} -script {} -defvar {} -defvars {}
 
-run_tool -name {PLACEROUTE} -script {} -defvar {} -defvars {}"""
+run_tool -name {PLACEROUTE} -script {} -defvar {} -defvars {}
+
+run_tool -name {VERIFYTIMING} -script {} -defvar {} -defvars {}
+"""
     )
 
     tcl.flush();
@@ -66,29 +68,29 @@ run_tool -name {PLACEROUTE} -script {} -defvar {} -defvars {}"""
 
     new Report{
       override def getFMax(): Double =  {
-//        import scala.io.Source
-//        val report = Source.fromFile(workspacePath + "/doit.log").getLines.mkString
-//        val intFind = "-?(\\d+\\.?)+".r
-//        val slack = try {
-//          (family match {
-//            case "ProASIC3E" =>
-//              intFind.findFirstIn("-?(\\d+.?)+ns  \\(required time - arrival time\\)".r.findFirstIn(report).get).get
-//          }).toDouble
-//        }catch{
-//          case e : Exception => -1.0
-//        }
-//        return 1.0/(targetPeriod.toDouble-slack*1e-9)
-        return 0
+        import scala.io.Source
+        val report = Source.fromFile(Paths.get(workspacePath, projectName, "designer/impl1" , s"${projectName}_maxdelay_timing_report.txt").toFile).getLines.mkString
+        val intFind = "-?(\\d+\\.?)+".r
+        val leFreq = try {
+          (family match {
+            case "ProASIC3E" =>
+              intFind.findFirstIn("Frequency[ ]\\(MHz\\)\\:[ ]*(\\d+.\\d+,?)+".r.findFirstIn(report).get).get
+          }).toDouble
+        }catch{
+          case e : Exception => -1.0
+        }
+        return leFreq*1e6
       }
       override def getArea(): String =  {
         import scala.io.Source
         val report = Source.fromFile(Paths.get(workspacePath, projectName, "designer/impl1" , s"${projectName}_place_and_route_report.txt").toFile).getLines.mkString
-        val intFind = "(\\d+,?)+".r
+        val intFind = "(\\d+\\.?)+".r
         val leArea = try {
           family match {
             case "ProASIC3E" =>
-              intFind.findFirstIn("COMB[ ]*\\|[ ]*(\\d+,?)+".r.findFirstIn(report).get).get + " COMB " +
-              intFind.findFirstIn("SEQ[ ]*\\|[ ]*(\\d+,?)+".r.findFirstIn(report).get).get + " SEQ "
+              intFind.findFirstIn("COMB[ ]*\\|[ ]*(\\d+,?)+".r.findFirstIn(report).get).get + " COMB, " +
+              intFind.findFirstIn("SEQ[ ]*\\|[ ]*(\\d+,?)+".r.findFirstIn(report).get).get + " SEQ -> " +
+              intFind.findAllIn("CORE[ ]*Used:[ ]*\\d+[ ]*Total:[ ]*\\d+[ ]*\\((\\d+.\\d+,?)".r.findFirstIn(report).get).toList(2) + "% Total CORE "
           }
         }catch{
           case e : Exception => "???"
