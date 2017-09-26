e09ccb6ed402b7ca0aea09f4832bf638ca063130
Dolu1990 <charles.papon.90@gmail.com>
2017-09-26 14:26:44 +0200

    fix compilation

--- project/Version.scala
+++ project/Version.scala
@@ -4,7 +4,7 @@ object SpinalVersion {
 
   val isSnapshot = false
   private def snapshot = if (isSnapshot) "-SNAPSHOT" else ""
-  private val major = "0.10.15"
+  private val major = "0.10.16" //Do -- !
   val all         = s"$major$snapshot"
   val core        = s"$major$snapshot"
   val lib         = s"$major$snapshot"
--- tester/src/test/scala/spinal/tester/scalatest/Apb3I2cSlaveTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/Apb3I2cSlaveTester.scala
@@ -25,7 +25,7 @@ import spinal.lib.com.i2c._
 class Apb3I2cSlaveTester extends SpinalTesterCocotbBase {
   override def getName: String = "Apb3I2cSlaveTester"
   override def pythonTestLocation: String = "tester/src/test/python/spinal/I2CTester2/Apb3I2cSlaveTester"
-  override def createToplevel: Component = new Apb3I2cSlave(
+  override def createToplevel: Component = new Apb3I2cCtrl(
     I2cSlaveMemoryMappedGenerics(
       I2cSlaveGenerics(
         samplingWindowSize = 3,
