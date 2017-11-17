38cb8e479e3d4592d148d9188532a5acd732b842
Snoopy87 <danymorard@hotmail.com>
2017-11-17 07:33:47 +0100

    Clean when.scala

--- core/src/main/scala/spinal/core/when.scala
+++ core/src/main/scala/spinal/core/when.scala
@@ -1,38 +1,100 @@
-/*
- * SpinalHDL
- * Copyright (c) Dolu, All rights reserved.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3.0 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library.
- */
-
+/*                                                                           *\
+**        _____ ____  _____   _____    __                                    **
+**       / ___// __ \/  _/ | / /   |  / /   HDL Core                         **
+**       \__ \/ /_/ // //  |/ / /| | / /    (c) Dolu, All rights reserved    **
+**      ___/ / ____// // /|  / ___ |/ /___                                   **
+**     /____/_/   /___/_/ |_/_/  |_/_____/                                   **
+**                                                                           **
+**      This library is free software; you can redistribute it and/or        **
+**    modify it under the terms of the GNU Lesser General Public             **
+**    License as published by the Free Software Foundation; either           **
+**    version 3.0 of the License, or (at your option) any later version.     **
+**                                                                           **
+**      This library is distributed in the hope that it will be useful,      **
+**    but WITHOUT ANY WARRANTY; without even the implied warranty of         **
+**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      **
+**    Lesser General Public License for more details.                        **
+**                                                                           **
+**      You should have received a copy of the GNU Lesser General Public     **
+**    License along with this library.                                       **
+\*                                                                           */
 package spinal.core
 
 import spinal.core.internals._
-import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 
-/**
- * Created by PIC18F on 11.01.2015.
- */
 
-
-trait ConditionalContext extends GlobalDataUser{
-
+trait ConditionalContext extends GlobalDataUser {
 }
 
 
+class SwitchContext(val statement: SwitchStatement) {
+}
+
+
+object ConditionalContext {
+
+  def isTrue(rootScope: ScopeStatement): Bool ={
+    val globalData = GlobalData.get
+
+    if(globalData.dslScope.head == rootScope) return True
+
+    rootScope.push()
+
+    val swap = rootScope.swap()
+    val cond = False
+    swap.appendBack()
+
+    rootScope.pop()
+    
+    cond := True
+    cond
+  }
+
+  def isTrue(): Bool = isTrue(GlobalData.get.dslScope.head.component.dslBody)
+}
+
+
+/**
+  * If statement
+  *
+  * @example {{{
+  *     when(cond1){
+  *       myCnt := 0
+  *     }.elsewhen(cond2){
+  *       myCnt := myCnt + 1
+  *     }otherwise{
+  *       myCnt := myCnt - 1
+  *     }
+  * }}}
+  *
+  * @see  [[http://spinalhdl.github.io/SpinalDoc/spinal/core/when_switch/ when Documentation]]
+  */
+object when {
+
+  def apply(cond: Bool)(block: => Unit): WhenContext = {
+
+    val whenStatement = new WhenStatement(cond)
+    val whenContext   = new WhenContext(whenStatement)
+
+    cond.globalData.dslScope.head.append(whenStatement)
+
+    whenStatement.whenTrue.push()
+    block
+    whenStatement.whenTrue.pop()
+
+    whenContext
+  }
+}
+
+
+/**
+  * else / else if  statement
+  *
+  * @see  [[http://spinalhdl.github.io/SpinalDoc/spinal/core/when_switch/ when Documentation]]
+  */
 class WhenContext(whenStatement: WhenStatement) extends ConditionalContext with ScalaLocated {
+
   def otherwise(block: => Unit): Unit = {
     whenStatement.whenFalse.push()
     block
@@ -40,7 +102,7 @@ class WhenContext(whenStatement: WhenStatement) extends ConditionalContext with 
   }
 
   def elsewhen(cond: Bool)(block: => Unit): WhenContext = {
-    var newWhenContext : WhenContext = null
+    var newWhenContext: WhenContext = null
     otherwise {
       newWhenContext = when(cond)(block)
     }
@@ -48,57 +110,59 @@ class WhenContext(whenStatement: WhenStatement) extends ConditionalContext with 
   }
 }
 
-object ConditionalContext{
-  def isTrue(rootScope : ScopeStatement) : Bool ={
-    val globalData = GlobalData.get
-    if(globalData.dslScope.head == rootScope) return True
-    rootScope.push()
-    val swap = rootScope.swap()
-    val cond = False
-    swap.appendBack()
-    rootScope.pop()
-    cond := True
-    cond
-  }
-  def isTrue() : Bool = isTrue(GlobalData.get.dslScope.head.component.dslBody)
-}
-
-object when {
-  def apply(cond: Bool)(block: => Unit): WhenContext = {
-    val whenStatement = new WhenStatement(cond)
-    val whenContext = new WhenContext(whenStatement)
-    cond.globalData.dslScope.head.append(whenStatement)
-    whenStatement.whenTrue.push()
-    block
-    whenStatement.whenTrue.pop()
-    whenContext
-  }
-}
 
 
-class SwitchContext(val statement : SwitchStatement) {
 
-}
+/**
+  * case/switch statement
+  *
+  * @example {{{
+  *     switch(x){
+  *         is(value1){
+  *             //execute when x === value1
+  *         }
+  *         is(value2){
+  *             //execute when x === value2
+  *         }
+  *         default{
+  *            //execute if none of precedent condition meet
+  *         }
+  *      }
+  * }}}
+  *
+  * @see  [[http://spinalhdl.github.io/SpinalDoc/spinal/core/when_switch/ switch Documentation]]
+  */
+object switch {
 
-object switch{
     def apply[T <: BaseType](value: T)(block: => Unit): Unit = {
-      val globalData = value.globalData
+      val globalData      = value.globalData
       val switchStatement = new SwitchStatement(value)
-      val switchContext = new SwitchContext(switchStatement)
+      val switchContext   = new SwitchContext(switchStatement)
+
       globalData.switchStack.push(switchContext)
       block
       globalData.switchStack.pop()
+
       globalData.dslScope.head.append(switchStatement)
   }
 }
 
-object is{
+
+/**
+  * is statement of a switch case
+  *
+  * @see  [[http://spinalhdl.github.io/SpinalDoc/spinal/core/when_switch/ switch Documentation]]
+  */
+object is {
+
   def apply(values:  Any*)(block: => Unit): Unit = list(values.iterator)(block)
+
   def list(values: Iterator[Any])(block: => Unit): Unit = {
-    val globalData = GlobalData.get
+
+    val globalData    = GlobalData.get
     val switchContext = globalData.switchStack.head
     val switchElement = new SwitchStatementElement(ArrayBuffer[Expression](), new ScopeStatement(switchContext.statement))
-    val switchValue = switchContext.statement.value
+    val switchValue   = switchContext.statement.value
 
     def onBaseType(value : BaseType): Unit ={
       if(value.getClass == switchValue.getClass){
@@ -107,44 +171,41 @@ object is{
         SpinalError("is(xxx) doesn't match switch(yyy) type")
       }
     }
-    values.foreach(value => value match {
-      case value : BaseType => onBaseType(value)
 
-      case key: Int => {
+    values.foreach {
+      case value: BaseType => onBaseType(value)
+      case key: Int        =>
         switchValue match {
           case switchValue: Bits => onBaseType(B(key))
           case switchValue: UInt => onBaseType(U(key))
           case switchValue: SInt => onBaseType(S(key))
-          case _ => SpinalError("The switch is not a Bits, UInt or SInt")
+          case _                 => SpinalError("The switch is not a Bits, UInt or SInt")
         }
-      }
-      case key: Long => {
+      case key: Long       =>
         switchValue match {
           case switchValue: Bits => onBaseType(B(key))
           case switchValue: UInt => onBaseType(U(key))
           case switchValue: SInt => onBaseType(S(key))
-          case _ => SpinalError("The switch is not a Bits, UInt or SInt")
+          case _                 => SpinalError("The switch is not a Bits, UInt or SInt")
         }
-      }
-      case key: BigInt => {
+      case key: BigInt     =>
         switchValue match {
           case switchValue: Bits => onBaseType(B(key))
           case switchValue: UInt => onBaseType(U(key))
           case switchValue: SInt => onBaseType(S(key))
-          case _ => SpinalError("The switch is not a Bits, UInt or SInt")
+          case _                 => SpinalError("The switch is not a Bits, UInt or SInt")
         }
-      }
-      case value : SpinalEnumElement[_] => onBaseType(value())
-      case key : MaskedLiteral => switchValue match {
+      case value: SpinalEnumElement[_] => onBaseType(value())
+      case key: MaskedLiteral          => switchValue match {
         case switchValue: Bits => switchElement.keys += SwitchStatementKeyBool(switchValue === key)
         case switchValue: UInt => switchElement.keys += SwitchStatementKeyBool(switchValue === key)
         case switchValue: SInt => switchElement.keys += SwitchStatementKeyBool(switchValue === key)
-        case _ => SpinalError("The switch is not a Bits, UInt or SInt")
+        case _                 => SpinalError("The switch is not a Bits, UInt or SInt")
       }
-//    }
-//              case key: Data => switchValue.isEquals(key)
-//              case key: Seq[_] => key.map(d => analyse(d)).reduce(_ || _)
-    })
+      //    }
+      //              case key: Data => switchValue.isEquals(key)
+      //              case key: Seq[_] => key.map(d => analyse(d)).reduce(_ || _)
+    }
 
 
     switchContext.statement.elements += switchElement
@@ -154,12 +215,22 @@ object is{
   }
 }
 
+
+/**
+  * default statement of a switch case
+  *
+  * @see  [[http://spinalhdl.github.io/SpinalDoc/spinal/core/when_switch/ switch Documentation]]
+  */
 object default {
+
   def apply(block: => Unit): Unit = {
-    val globalData = GlobalData.get
+
+    val globalData    = GlobalData.get
     val switchContext = globalData.switchStack.head
-    val defaultScope =  new ScopeStatement(switchContext.statement)
+    val defaultScope  =  new ScopeStatement(switchContext.statement)
+
     switchContext.statement.defaultScope = defaultScope
+
     defaultScope.push()
     block
     defaultScope.pop()
