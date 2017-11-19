9961792d60e3ee2c4f0a5a310470c86e6d02f96d
Morard Dany <danymorard@hotmail.com>
2017-11-19 09:14:39 +0100

    Clean Misc.scala

--- core/src/main/scala/spinal/core/internals/Misc.scala
+++ core/src/main/scala/spinal/core/internals/Misc.scala
@@ -1,6 +1,28 @@
+/*                                                                           *\
+**        _____ ____  _____   _____    __                                    **
+**       / ___// __ \/  _/ | / /   |  / /   HDL Core                         **
+**       \__ \/ /_/ // //  |/ / /| | / /    (c) Dolu, All rights reserved    **
+**      ___/ / ____// // /|  / ___ |/ /___                                   **
+**     /____/_/   /___/_/ |_/_/  |_/_____/                                   **
+**                                                                           **
+**      This library is free software; you can redistribute it and/or        **
+**    modify it under the terms of the GNU Lesser General Public             **
+**    License as published by the Free Software Foundation; either           **
+**    version 3.0 of the License, or (at your option) any later version.     **
+**                                                                           **
+**      This library is distributed in the hope that it will be useful,      **
+**    but WITHOUT ANY WARRANTY; without even the implied warranty of         **
+**    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      **
+**    Lesser General Public License for more details.                        **
+**                                                                           **
+**      You should have received a copy of the GNU Lesser General Public     **
+**    License along with this library.                                       **
+\*                                                                           */
 package spinal.core.internals
+
 import spinal.core._
 import scala.collection.mutable
+
 
 object ScalaUniverse {
   def isCaseClass(o: Any): Boolean = {
@@ -9,10 +31,13 @@ object ScalaUniverse {
   }
 }
 
+
 trait BaseNode {
   var algoInt, algoIncrementale = 0
+
   private[core] def getClassIdentifier: String = this.getClass.getName.split('.').last.replace("$","")
-  def toStringMultiLine() : String = this.toString
+
+  def toStringMultiLine(): String = this.toString
 }
 
 
@@ -22,9 +47,13 @@ object TypeUInt
 object TypeSInt
 object TypeEnum
 
+
 trait DoubleLinkedContainerElement[SC  <: DoubleLinkedContainer[SC, SE], SE <: DoubleLinkedContainerElement[SC, SE]]{
+
   def dlcParent : SC
+
   var dlceLast, dlceNext : SE = null.asInstanceOf[SE]
+
   def dlcRemove(): Unit = {
     //super.removeStatement()
 
@@ -34,27 +63,33 @@ trait DoubleLinkedContainerElement[SC  <: DoubleLinkedContainer[SC, SE], SE <: D
     } else {
       dlcParent.dlcHead = dlceNext
     }
+
     if(dlceNext != null){
       dlceNext.dlceLast = dlceLast
     } else {
       dlcParent.dlcLast = dlceLast
     }
+
     dlceLast = null.asInstanceOf[SE]
     dlceNext = null.asInstanceOf[SE]
   }
 }
 
+
 trait DoubleLinkedContainer[SC <: DoubleLinkedContainer[SC, SE], SE <: DoubleLinkedContainerElement[SC, SE]]{
   var dlcHead, dlcLast : SE = null.asInstanceOf[SE]
+
   def dlcHasOnlyOne = dlcHead == dlcLast && dlcHead != null
-  def dlcIsEmpty = dlcHead == null
+  def dlcIsEmpty    = dlcHead == null
   //  def sizeIsOne = head != null && head == last
-  def dlcPrepend(that : SE) : this.type = {
+
+  def dlcPrepend(that: SE): this.type = {
     if(dlcHead != null){
       dlcHead.dlceLast = dlcHead
     } else {
       dlcLast = that
     }
+
     that.dlceNext = dlcHead
     that.dlceLast = null.asInstanceOf[SE]
 
@@ -63,9 +98,10 @@ trait DoubleLinkedContainer[SC <: DoubleLinkedContainer[SC, SE], SE <: DoubleLin
     this
   }
 
-  def dlcAppend(that : SE) : this.type = {
+  def dlcAppend(that: SE): this.type = {
     that.dlceNext = null.asInstanceOf[SE]
     that.dlceLast = dlcLast
+
     if(dlcLast != null){
       dlcLast.dlceNext = that
     } else {
@@ -92,7 +128,7 @@ trait DoubleLinkedContainer[SC <: DoubleLinkedContainer[SC, SE], SE <: DoubleLin
   //  }
 
 
-  def dlcForeach[T >: SE](func : T => Unit) : Unit = {
+  def dlcForeach[T >: SE](func: T => Unit): Unit = {
     var ptr = dlcHead
     while(ptr != null){
       val current = ptr
@@ -121,41 +157,40 @@ object Misc {
   addReflectionExclusion(classOf[SpinalEnumCraft[_]])
   addReflectionExclusion(classOf[Area])
 
-
-
-
   def reflect(o: Object, onEach: (String, Object) => Unit, namePrefix: String = ""): Unit = {
     val refs = mutable.Set[Object]()
 
-    def applyNameTo(name : String,fieldRef : Object): Unit ={
+    def applyNameTo(name: String, fieldRef: Object): Unit ={
       if (fieldRef != null && (!refs.isInstanceOf[Data] || !refs.contains(fieldRef))) {
         fieldRef match {
           case range : Range =>
-          case vec: Vec[_] =>
-          case seq: Seq[_] => {
+          case vec: Vec[_]   =>
+          case seq: Seq[_]   =>
             for ((obj, i) <- seq.zipWithIndex) {
               applyNameTo(name + "_" + i, obj.asInstanceOf[Object])
               refs += fieldRef
             }
-          }
-          case seq: Array[_] => {
+          case seq: Array[_] =>
             for ((obj, i) <- seq.zipWithIndex) {
               applyNameTo(name + "_" + i, obj.asInstanceOf[Object])
               refs += fieldRef
             }
-          }
-          case _ =>
+          case _             =>
         }
+
         onEach(name, fieldRef)
         refs += fieldRef
       }
     }
 
     explore(o.getClass)
+
     def explore(c: Class[_]): Unit = {
       if (c == null) return
+
       if (reflectExclusion.contains(c) || c.getName + "$" == Component.getClass.getName)
         return
+
       explore(c.getSuperclass)
 
       //      val fields = c.getDeclaredFields
@@ -164,19 +199,24 @@ object Misc {
       //
       //
       val methods = c.getDeclaredMethods
-      val fields = c.getDeclaredFields.filter(fd => methods.exists(_.getName == fd.getName) && ! AnnotationUtils.isDontName(fd))
+      val fields  = c.getDeclaredFields.filter(fd => methods.exists(_.getName == fd.getName) && ! AnnotationUtils.isDontName(fd))
 
       for (field <- fields) {
         field.setAccessible(true)
+
         val fieldRef = field.get(o)
+
         if (fieldRef != null && (!refs.isInstanceOf[Data] || !refs.contains(fieldRef))) {
-          val methodName = field.getName
+          val methodName     = field.getName
           val firstCharIndex = methodName.lastIndexOf('$')
+
           val postFix = if(firstCharIndex == -1)
             methodName
           else
             methodName.substring(firstCharIndex+1)
+
           val name = namePrefix + postFix
+
           applyNameTo(name,fieldRef)
         }
       }
@@ -217,7 +257,7 @@ object Misc {
 }
 
 object GraphUtils{
-  def walkAllComponents(root : Component, func : Component => Unit) : Unit = {
+  def walkAllComponents(root: Component, func: Component => Unit): Unit = {
     func(root)
     root.children.foreach(walkAllComponents(_, func))
   }
