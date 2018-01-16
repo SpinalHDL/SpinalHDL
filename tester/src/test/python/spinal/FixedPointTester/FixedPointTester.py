d4e6da0951328c832d3207eaafadcf8ab30ccb9b
Dolu1990 <papuchoud@hotmail.com>
2018-01-16 08:39:19 +0100

    #110 Add more fixed point tests (reverted from commit 6e5468a2f0259de0f2865f44d60c166346c83fb2)

--- tester/src/test/python/spinal/FixedPointTester/FixedPointTester.py
+++ tester/src/test/python/spinal/FixedPointTester/FixedPointTester.py
@@ -9,8 +9,6 @@ class Ref:
         self.io_outSFix_0 = truncUInt(sint(dut.io_inSFix_0) + ((sint(dut.io_inSFix_1) << 2)), dut.io_outSFix_0)
         self.io_outSFix_1 = truncUInt((sint(dut.io_inSFix_0) * sint(dut.io_inSFix_1)) >> 5, dut.io_outSFix_1)
         self.io_outBundleA_a_sfix = truncUInt(sint(dut.io_inBundleA_a_sfix) >> 2, dut.io_outBundleA_a_sfix)
-        sfix2 = sint(dut.io_inSFix2)
-        self.io_outSFix2 = truncUInt(((sfix2 << 1) + sfix2) << 1, dut.io_outSFix2)
 
 @cocotb.test()
 def test1(dut):
@@ -22,12 +20,10 @@ def test1(dut):
         randSignal(dut.io_inSFix_0)
         randSignal(dut.io_inSFix_1)
         randSignal(dut.io_inBundleA_a_sfix)
-        randSignal(dut.io_inSFix2)
         yield Timer(1000)
         ref = Ref(dut)
         assertEquals(ref.io_outSFix_0, dut.io_outSFix_0, "io_outSFix_0")
         assertEquals(ref.io_outSFix_1, dut.io_outSFix_1, "io_outSFix_1")
-        assertEquals(ref.io_outSFix2, dut.io_outSFix2, "io_outSFix2")
         assertEquals(ref.io_outBundleA_a_sfix, dut.io_outBundleA_a_sfix, "io_outBundleA_a_sfix")
 
     dut.log.info("Cocotb test done")
--- tester/src/test/scala/spinal/tester/scalatest/FixedPointTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/FixedPointTester.scala
@@ -18,25 +18,17 @@ object FixedPointTester{
       val inSFix = in(Vec(Seq(SFix(4 exp,16 bit),SFix(2 exp,12 bit))))
       val outSFix = out(Vec(Seq(SFix(4 exp,16 bit),SFix(8 exp,24 bit))))
 
-      val inSFix2 = in SFix(0 exp, -8 exp)
-      val outSFix2 = out(SFix(2 exp, -8 exp))
-
       val inBundleA = in(BundleA(8))
       val outBundleA = out(BundleA(6))
     }
     io.outSFix(0) := (io.inSFix(0) + io.inSFix(1))
     //28 bit  point at 28-6=22   target is at 24-8=16
     io.outSFix(1) := (io.inSFix(0) * io.inSFix(1)).truncated
-
-    io.outSFix2 := ((io.inSFix2 <<|1) + io.inSFix2 ) <<|1
     //io.outSFix(0).raw := S(0,16 bit)
     //io.outSFix(1).raw := S(0,24 bit)
     //io.outBundleA.a.sfix.raw := S(0,6 bit)
     io.inBundleA.a.sfix.addTag(tagTruncated)
     io.outBundleA <> io.inBundleA
-
-
-
   }
 }
 
@@ -50,5 +42,4 @@ class FixedPointTesterCocotbBoot extends SpinalTesterCocotbBase {
   override def getName: String = "FixedPointTester"
   override def pythonTestLocation: String = "tester/src/test/python/spinal/FixedPointTester"
   override def createToplevel: Component = new FixedPointTester
-  withWaveform = true
 }
