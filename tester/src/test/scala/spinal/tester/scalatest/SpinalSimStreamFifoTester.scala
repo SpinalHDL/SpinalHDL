8f1ea26d3eb333390f2c7be5c616653542840895
Dolu1990 <papuchoud@hotmail.com>
2018-02-05 22:56:10 +0100

    Fix test/clone

--- tester/src/test/scala/spinal/tester/scalatest/SpinalSimStreamFifoTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/SpinalSimStreamFifoTester.scala
@@ -67,6 +67,8 @@ class SpinalSimStreamFifoTester extends FunSuite {
       val flag = Bool
       val data = Bits(8 bits)
       val color = Rgb(5, 6, 5)
+
+      override def clone = Transaction()
     }
 
     val compiled = SimConfig.allOptimisation.compile(
