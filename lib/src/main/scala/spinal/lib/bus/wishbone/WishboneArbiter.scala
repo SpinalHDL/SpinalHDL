5768634e7ad74a85f446035a44b7a9e7189f4159
Luca Cristaldi <wifasoi@gmail.com>
2018-04-27 00:17:16 +0200

    Fix WishboneArbiter
    this patch will modify the WishboneArbiter
    in a way that all the line are driven whenever is possible

--- lib/src/main/scala/spinal/lib/bus/wishbone/WishboneArbiter.scala
+++ lib/src/main/scala/spinal/lib/bus/wishbone/WishboneArbiter.scala
@@ -10,10 +10,9 @@ object WishboneArbiter{
     * @constructor create a WishboneArbiter instance
     * @param config it will use for configuring all the input/output wishbone port
     * @param inputCount the number of master interface
-    * @param priority wich interface is considerate first in the roundrobin algorithm
     * @return a instantiated WishboneArbiter class
     */
-  def apply(config : WishboneConfig, inputCount : Int, priority : Int = 1) = new WishboneArbiter(config, inputCount, priority)
+  def apply(config : WishboneConfig, inputCount : Int) = new WishboneArbiter(config, inputCount)
 
   /** Create a wishbone Arbiter/multiplexer
     * @param masters a list of master interfaces
@@ -34,18 +33,43 @@ object WishboneArbiter{
   * @param inputCount the number of master interface
   * @param priority wich interface is considerate first in the roundrobin algorithm
   */
-class WishboneArbiter(config : WishboneConfig, inputCount : Int, priority : Int = 1) extends Component{
+class WishboneArbiter(config : WishboneConfig, inputCount : Int) extends Component{
   val io = new Bundle{
     val inputs = Vec(slave(Wishbone(config)), inputCount)
     val output = master(Wishbone(config))
   }
-  val status = Vec(io.inputs.map(_.CYC))
-  val select = Reg(UInt(log2Up(inputCount) bits)) init(0)
 
-  io.inputs.map(_.clearAll)
-
-  when(!status(select)){
-    select := OHToUInt(OHMasking.roundRobin(status.asBits, B(priority)))
+  io.inputs.map(_.ACK := False)
+  io.inputs.map{ in =>
+    in.DAT_MISO := io.output.DAT_MISO
+    Wishbone.driveWeak(io.output.TGD_MISO, in.TGD_MISO, null, false, false)
   }
-    io.inputs(select) <> io.output
+  val requests = if(config.useLOCK) Vec(io.inputs.map(func => func.CYC && func.LOCK)) else Vec(io.inputs.map(_.CYC))
+  val selector = Reg(Bits(inputCount bits)) init(1)
+  val maskLock = Reg(Bits(inputCount bits)) init(1)
+
+  when(!io.output.CYC){
+    selector := OHMasking.roundRobin(requests.asBits, maskLock)
+  }.otherwise{
+    maskLock := selector
+  }
+
+  MuxOH(selector, Vec(io.inputs.map(_.ACK))) := io.output.ACK
+  if(config.useSTALL) MuxOH(selector, Vec(io.inputs.map(_.STALL)))  := io.output.STALL
+  if(config.useERR)   MuxOH(selector, Vec(io.inputs.map(_.ERR)))    := io.output.ERR
+  if(config.useRTY)   MuxOH(selector, Vec(io.inputs.map(_.RTY)))    := io.output.RTY
+
+  io.output.CYC       := MuxOH(selector, Vec(io.inputs.map(_.CYC)))
+  io.output.STB       := MuxOH(selector, Vec(io.inputs.map(_.STB)))
+  io.output.WE        := MuxOH(selector, Vec(io.inputs.map(_.WE)))
+  io.output.ADR       := MuxOH(selector, Vec(io.inputs.map(_.ADR)))
+  io.output.DAT_MOSI  := MuxOH(selector, Vec(io.inputs.map(_.DAT_MOSI)))
+
+  if(config.useSEL)   io.output.SEL       := MuxOH(selector, Vec(io.inputs.map(_.SEL)))
+  if(config.useLOCK)  io.output.LOCK      := MuxOH(selector, Vec(io.inputs.map(_.LOCK)))
+  if(config.useCTI)   io.output.CTI       := MuxOH(selector, Vec(io.inputs.map(_.CTI)))
+  if(config.useTGD)   io.output.TGD_MOSI  := MuxOH(selector, Vec(io.inputs.map(_.TGD_MOSI)))
+  if(config.useTGA)   io.output.TGA       := MuxOH(selector, Vec(io.inputs.map(_.TGA)))
+  if(config.useTGC)   io.output.TGC       := MuxOH(selector, Vec(io.inputs.map(_.TGC)))
+  if(config.useBTE)   io.output.BTE       := MuxOH(selector, Vec(io.inputs.map(_.BTE)))
 }
