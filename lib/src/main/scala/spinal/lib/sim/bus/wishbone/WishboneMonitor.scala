2241587f5a7bc87da439844a9bcfcf11d439e8cb
Luca Cristaldi <wifasoi@gmail.com>
2018-10-08 00:31:00 +0200

    Add dockstring

--- lib/src/main/scala/spinal/lib/sim/bus/wishbone/WishboneMonitor.scala
+++ lib/src/main/scala/spinal/lib/sim/bus/wishbone/WishboneMonitor.scala
@@ -10,10 +10,17 @@ object WishboneMonitor{
   def apply(bus : Wishbone, clockdomain: ClockDomain)(callback: (Wishbone) => Unit) = new WishboneMonitor(bus,clockdomain).addCallback(callback)
 }
 
+/** This is a helping class for executing code when an acknoledge happend on the bus
+  * @param bus the wishbone bus to drive
+  * @param clockdomain the clockdomain where the bus reside
+  */
 class WishboneMonitor(bus: Wishbone, clockdomain: ClockDomain){
   val busStatus = WishboneStatus(bus)
   val callbacks = ArrayBuffer[(Wishbone) => Unit]()
 
+  /** Add a callback, this will be executed on every bus acknoledge
+    * @param callback a function or code block that takes a wishbone bus as parameter
+    */
   def addCallback(callback: (Wishbone) => Unit): Unit = callbacks += callback
 
   fork{
