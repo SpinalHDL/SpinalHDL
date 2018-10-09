58e00aa338ddc8898c9507e2039bd4a5cc9048d7
Snoopy87 <danymorar@hotmail.com>
2018-10-09 07:34:19 +0200

    Fix bus when error occurs in AhbLite3ToApb3Bridge + Add remapAddress function to AhbLite3

--- lib/src/main/scala/spinal/lib/bus/amba3/ahblite/AhbLite3.scala
+++ lib/src/main/scala/spinal/lib/bus/amba3/ahblite/AhbLite3.scala
@@ -169,8 +169,9 @@ case class AhbLite3(config: AhbLite3Config) extends Bundle with IMasterSlave {
 
   /** Connect two AhbLite3 bus together with the resized of the address */
   def <<(that: AhbLite3): Unit = {
-    
-    assert(this.config.dataWidth == that.config.dataWidth, "AhbLite3 << : bus must have the same data width")
+
+    assert(that.config.addressWidth >= this.config.addressWidth, "AhbLite3 << : mismatch width address (use remap())")
+    assert(this.config.dataWidth == that.config.dataWidth, "AhbLite3 << : mismatch data width")
 
     that.HREADYOUT := this.HREADYOUT
     that.HRDATA    := this.HRDATA
@@ -189,6 +190,41 @@ case class AhbLite3(config: AhbLite3Config) extends Bundle with IMasterSlave {
   }
 
   def >>(that: AhbLite3): Unit = that << this
+
+
+  /**
+    * Remap a bus
+    *
+    * @example {{{
+    *
+    *     val ahb_0 = AhbLite3(AhbLite3Config(32, 32))
+    *     val ahb_1 = AhbLite3(AhbLite3Config(24, 32))
+    *
+    *     ahb_0 <> ahb_1.remapAddress(addr => U(0x40, 8 bits) @@ addr)
+    *         }}}
+    */
+  def remapAddress(remapping: UInt => UInt): AhbLite3 = {
+    val address = remapping(this.HADDR)
+
+    val busRemap = AhbLite3(AhbLite3Config(dataWidth = this.config.dataWidth, addressWidth = address.getBitsWidth))
+
+    this.HREADYOUT := busRemap.HREADYOUT
+    this.HRDATA    := busRemap.HRDATA
+    this.HRESP     := busRemap.HRESP
+
+    busRemap.HSEL      := this.HSEL
+    busRemap.HREADY    := this.HREADY
+    busRemap.HSIZE     := this.HSIZE
+    busRemap.HWDATA    := this.HWDATA
+    busRemap.HWRITE    := this.HWRITE
+    busRemap.HBURST    := this.HBURST
+    busRemap.HPROT     := this.HPROT
+    busRemap.HMASTLOCK := this.HMASTLOCK
+    busRemap.HTRANS    := this.HTRANS
+    busRemap.HADDR     := address
+
+    busRemap
+  }
 
 
   /** Convert to ahbLite3Master */
