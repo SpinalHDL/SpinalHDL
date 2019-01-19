e85bb36e71475e8d0d49cc564751304c865ba2bd
Tom Verbeure <hombre+github@gmail.com>
2019-01-19 04:01:54 +0000

    Rise/Fall -> Rose/Fall

--- core/src/main/scala/spinal/core/Formal.scala
+++ core/src/main/scala/spinal/core/Formal.scala
@@ -6,8 +6,8 @@ object Formal {
   def past[T <: Data](that : T, delay : Int) : T = that.formalPast(delay)
   def past[T <: Data](that : T) : T = past(that, 1)
 
-  def rise(that : Bool) : Bool = that.wrapUnaryOperator(new Operator.Formal.Rise)
-  def fall(that : Bool) : Bool = that.wrapUnaryOperator(new Operator.Formal.Fall)
+  def rose(that : Bool) : Bool = that.wrapUnaryOperator(new Operator.Formal.Rose)
+  def fell(that : Bool) : Bool = that.wrapUnaryOperator(new Operator.Formal.Fell)
   def changed[T <: BaseType](that : T) : Bool = that.wrapUnaryWithBool(new Operator.Formal.Changed)
   def stable[T <: BaseType](that : T) : Bool = that.wrapUnaryWithBool(new Operator.Formal.Stable)
   def initstate() : Bool = {
