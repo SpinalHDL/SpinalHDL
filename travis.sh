ed8047ef3414ac95cac6633ad818e6f2d1042bba
1138-4EB <1138-4EB@users.noreply.github.com>
2019-02-13 05:07:56 +0100

    update travis decorations

--- travis.sh
+++ travis.sh
@@ -22,29 +22,50 @@ enable_color
 
 #---
 
-# This is a trimmed down copy of
-# https://github.com/travis-ci/travis-build/blob/master/lib/travis/build/templates/header.sh
-travis_time_start() {
-  # `date +%N` returns the date in nanoseconds. It is used as a replacement for $RANDOM, which is only available in bash.
-  travis_timer_id=`date +%N`
-  travis_start_time=$(travis_nanoseconds)
-  echo "travis_time:start:$travis_timer_id"
+travis_start () {
+  :
 }
-travis_time_finish() {
-  travis_end_time=$(travis_nanoseconds)
-  local duration=$(($travis_end_time-$travis_start_time))
-  echo "travis_time:end:$travis_timer_id:start=$travis_start_time,finish=$travis_end_time,duration=$duration"
+travis_finish () {
+  :
 }
 
-if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-  travis_nanoseconds() {
-    date -u '+%s000000000'
+[ -n "$TRAVIS" ] && {
+  # This is a trimmed down copy of
+  # https://github.com/travis-ci/travis-build/blob/master/lib/travis/build/bash/*
+  travis_time_start() {
+    # `date +%N` returns the date in nanoseconds. It is used as a replacement for RANDOM, which is only available in bash.
+    travis_timer_id=`date +%N`
+    travis_start_time=$(travis_nanoseconds)
+    echo "travis_time:start:$travis_timer_id"
   }
-else
-  travis_nanoseconds() {
-    date -u '+%s%N'
+  travis_time_finish() {
+    travis_end_time=$(travis_nanoseconds)
+    local duration=$(($travis_end_time-$travis_start_time))
+    echo "travis_time:end:$travis_timer_id:start=$travis_start_time,finish=$travis_end_time,duration=$duration"
   }
-fi
+
+  if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    travis_nanoseconds() {
+      date -u '+%s000000000'
+    }
+  else
+    travis_nanoseconds() {
+      date -u '+%s%N'
+    }
+  fi
+
+  travis_start () {
+    echo "travis_fold:start:$1"
+    travis_time_start
+    printf "$ANSI_BLUE[$2] $3$ANSI_NOCOLOR\n"
+  }
+
+  travis_finish () {
+    travis_time_finish
+    echo "travis_fold:end:$1"
+  }
+
+}
 
 #--
 
@@ -52,25 +73,20 @@ getDockerCredentialPass () {
   PASS_URL="$(curl -s https://api.github.com/repos/docker/docker-credential-helpers/releases/latest \
     | grep "browser_download_url.*pass-.*-amd64" \
     | cut -d : -f 2,3 \
-    | tr -d \")"
-  PASS_URL="$(echo $PASS_URL | cut -c2-)"
+    | tr -d \" \
+    | cut -c2- )"
 
-  if [ "$(echo "$PASS_URL" | cut -c1-5)" != "https" ]; then
-    PASS_URL="https://github.com/docker/docker-credential-helpers/releases/download/v0.6.0/docker-credential-pass-v0.6.0-amd64.tar.gz"
-  fi
+  [ "$(echo "$PASS_URL" | cut -c1-5)" != "https" ] && PASS_URL="https://github.com/docker/docker-credential-helpers/releases/download/v0.6.0/docker-credential-pass-v0.6.0-amd64.tar.gz"
 
   echo "PASS_URL: $PASS_URL"
-
   curl -fsSL "$PASS_URL" | tar xv
-
   chmod + $(pwd)/docker-credential-pass
 }
 
 #---
 
 dockerLogin () {
-  if [ "$CI" = "true" ]; then
-    gpg --batch --gen-key <<-EOF
+  [ "$CI" = "true" ] && gpg --batch --gen-key <<-EOF ; pass init $(gpg --no-auto-check-trustdb --list-secret-keys | grep ^sec | cut -d/ -f2 | cut -d" " -f1)
 %echo Generating a standard key
 Key-Type: DSA
 Key-Length: 1024
@@ -83,27 +99,25 @@ Expire-Date: 0
 %commit
 %echo done
 EOF
-
-    key=$(gpg --no-auto-check-trustdb --list-secret-keys | grep ^sec | cut -d/ -f2 | cut -d" " -f1)
-    pass init $key
-
-    echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-  fi
+  echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 }
 
 #---
 
 beforeInstall () {
+  travis_start "update" "APT" "update"
   sudo apt update -y -qq
+  travis_finish "update"
 
-  # JDK fix
+  travis_start "fix" "JDK" "fix"
   cat /etc/hosts # optionally check the content *before*
   sudo hostname "$(hostname | cut -c1-63)"
   sed -e "s/^\\(127\\.0\\.0\\.1.*\\)/\\1 $(hostname | cut -c1-63)/" /etc/hosts | sudo tee /etc/hosts
   cat /etc/hosts # optionally check the content *after*
   cd ..
+  travis_finish "fix"
 
-  # GHDL
+  travis_start "ghdl" "GHDL" "build and install"
   sudo apt install -y gnat-4.9 zlib1g-dev
   git clone https://github.com/ghdl/ghdl ghdl-build && cd ghdl-build
   git reset --hard "50da90f509aa6de2961f1795af0be2452bc2c6d9"
@@ -111,8 +125,10 @@ beforeInstall () {
   mv install-mcode ../ghdl
   cd ..
   rm -rf ghdl-build
+  travis_finish "ghdl"
 
-  # iverilog (debian package 9.7 contain bugs)
+  # Debian package 9.7 contain bugs
+  travis_start "iverilog" "iverilog" "build and install"
   sudo apt install -y gperf readline-common bison flex
   curl -fsSL https://github.com/steveicarus/iverilog/archive/v10_2.tar.gz | tar -xvz
   cd iverilog-10_2
@@ -122,14 +138,14 @@ beforeInstall () {
   sudo make install
   cd ..
   rm -rf iverilog-10_2
+  travis_finish "iverilog"
 
-  # cocotb
+  travis_start "cocotb" "cocotb" "install and compile VPI"
   sudo apt install -y git make gcc g++ swig python-dev
   git clone https://github.com/potentialventures/cocotb
   cd cocotb
   git reset --hard a463cee498346cb26fc215ced25c088039490665
   cd ..
-
   # Force cocotb to compile VPI to avoid race condition when tests are start in parallel
   export PATH=$(pwd)/ghdl/bin:$PATH
   export COCOTB=$(pwd)/cocotb
@@ -137,8 +153,9 @@ beforeInstall () {
   make TOPLEVEL_LANG=verilog
   make TOPLEVEL_LANG=vhdl
   cd ../../../../../../..
+  travis_finish "cocotb"
 
-  # Verilator
+  travis_start "verilator" "verilator" "build and install"
   sudo apt install -y git make autoconf g++ flex bison  # First time prerequisites
   git clone http://git.veripool.org/git/verilator   # Only first time
   unset VERILATOR_ROOT  # For bash
@@ -150,6 +167,7 @@ beforeInstall () {
   make -j$(nproc)
   sudo make install
   cd ..
+  travis_finish "verilator"
 
   cd SpinalHDL
 }
@@ -157,65 +175,44 @@ beforeInstall () {
 #---
 
 compileTest () {
-  echo "travis_fold:start:compile"
-  travis_time_start
-  printf "$ANSI_BLUE[SBT] compile $ANSI_NOCOLOR\n"
+  travis_start "compile" "SBT" "compile"
   sbt -J-Xss2m compile
-  travis_time_finish
-  echo "travis_fold:end:compile"
+  travis_finish "compile"
 
-  echo "travis_fold:start:cocotb"
-  travis_time_start
-  printf "$ANSI_BLUE[SBT] cocotb VPI $ANSI_NOCOLOR\n"
-# Force cocotb to compile VPI to avoid race condition when tests are start in parallel
+  travis_start "cocotb" "SBT" "cocotb VPI"
+  # Force cocotb to compile VPI to avoid race condition when tests are start in parallel
   cd tester/src/test/python/spinal/Dummy
   make TOPLEVEL_LANG=verilog
   make TOPLEVEL_LANG=vhdl
   cd ../../../../../..
-  travis_time_finish
-  echo "travis_fold:end:cocotb"
+  travis_finish "cocotb"
 
-  echo "travis_fold:start:test"
-  travis_time_start
-  printf "$ANSI_BLUE[SBT] test $ANSI_NOCOLOR\n"
+  travis_start "test" "SBT" "test"
   sbt -J-Xss2m test
-  travis_time_finish
-  echo "travis_fold:end:test"
+  travis_finish "test"
 }
 
 #---
 
 deploy () {
-  echo "travis_fold:start:build-spinalhdl"
-  travis_time_start
-  printf "$ANSI_BLUE[BUILD] spinalhdl/spinalhdl $ANSI_NOCOLOR\n"
+  travis_start "build-spinalhdl" "BUILD" "spinalhdl/spinalhdl"
   docker build -t spinalhdl/spinalhdl --target spinalhdl .
-  travis_time_finish
-  echo "travis_fold:end:build-spinalhdl"
+  travis_finish "build-spinalhdl"
 
-  echo "travis_fold:start:build-riscv"
-  travis_time_start
-  printf "$ANSI_BLUE[BUILD] spinalhdl/riscv $ANSI_NOCOLOR\n"
+  travis_start "build-riscv" "BUILD" "spinalhdl/riscv"
   docker build -t spinalhdl/riscv --target riscv .
-  travis_time_finish
-  echo "travis_fold:end:build-riscv"
+  travis_finish "build-riscv"
 
   getDockerCredentialPass
   dockerLogin
 
-  echo "travis_fold:start:push-spinalhdl"
-  travis_time_start
-  printf "$ANSI_BLUE[PUSH] spinalhdl/spinalhdl $ANSI_NOCOLOR\n"
+  travis_start "push-spinalhdl" "PUSH" "spinalhdl/spinalhdl"
   docker push spinalhdl/spinalhdl
-  travis_time_finish
-  echo "travis_fold:end:push-spinalhdl"
+  travis_finish "push-spinalhdl"
 
-  echo "travis_fold:start:push-riscv"
-  travis_time_start
-  printf "$ANSI_BLUE[PUSH] spinalhdl/riscv $ANSI_NOCOLOR\n"
+  travis_start "push-riscv" "PUSH" "spinalhdl/riscv"
   docker push spinalhdl/riscv
-  travis_time_finish
-  echo "travis_fold:end:push-riscv"
+  travis_finish "push-riscv"
 
   docker logout
 }
