7e5b51794f7a892da4f6a13905578d7895672eab
Luca Cristaldi <wifasoi@gmail.com>
2019-03-19 23:43:05 +0100

    Rework the Executable trait

--- lib/src/main/scala/spinal/lib/eda/yosys/common.scala
+++ lib/src/main/scala/spinal/lib/eda/yosys/common.scala
@@ -6,47 +6,73 @@ import scala.sys.process._
 import scala.collection._
 import java.nio.file.{Path, Paths}
 
-trait WorkDir{
-  def workDir: String
-  // def getPath(rel: String): File = new File(workDir, rel)
-  def getPath(rel: String) = Paths.get(".",workDir,rel).normalize.toString
-  def createDir: Unit = new File(workDir).mkdir()
-}
 
-trait Executable extends WorkDir{
-  val logFile: String = "verbose.log"
-  val logPath: String = "logs"
+// trait Executable extends WorkDir{
+//   val logFile: String = "verbose.log"
+//   val logPath: String = "logs"
+
+//   val isWindows = System.getProperty("os.name").toLowerCase().contains("win")
+//   def runComand: String = this.toString
+
+//   def run(): Int = {
+//     val dir = new File(workDir)
+//     dir.mkdir
+//     val lgDir = new File(dir, logPath)
+//     lgDir.mkdir
+//     val lgFile = new File(lgDir, logFile)
+//     val str = new PrintWriter(lgFile)
+
+//     str.println("WORDIR: " + dir.toString())
+//     str.println("COMMAND: " + runComand)
+//     val log = new ProcessLogger {
+//       override def err(s: => String): Unit = {
+//         str.println(s)
+//         stderr.println(s)
+//       }
+//       override def out(s: => String): Unit = {
+//         str.println(s)
+//         //stdout.println(s)
+//       }
+//       override def buffer[T](f: => T) = f
+//     }
+//     val ret: Int = if (isWindows)
+//                 Process("cmd /C " + this.runComand, dir) ! (log)
+//               else
+//                 Process(this.runComand, dir) ! (log)
+//     str.close()
+//     ret
+//   }
+// }
+
+trait Executable{
+  val logFile: Option[Path]
 
   val isWindows = System.getProperty("os.name").toLowerCase().contains("win")
   def runComand: String = this.toString
 
   def run(): Int = {
-    val dir = new File(workDir)
-    dir.mkdir
-    val lgDir = new File(dir, logPath)
-    lgDir.mkdir
-    val lgFile = new File(lgDir, logFile)
-    val str = new PrintWriter(lgFile)
+    val dir = if(logFile.nonEmpty) Option(logFile.get.getParent) else None
+    if(dir.nonEmpty) dir.get.toFile.mkdir
 
-    str.println("WORDIR: " + dir.toString())
-    str.println("COMMAND: " + runComand)
+    // val str = new PrintWriter(logFile.getOrElse.toFile)
+    val str = if(logFile.nonEmpty) Option(new PrintWriter(logFile.get.toFile)) else None
+
     val log = new ProcessLogger {
       override def err(s: => String): Unit = {
-        str.println(s)
+        if(str.nonEmpty) str.get.println(s)
         stderr.println(s)
       }
       override def out(s: => String): Unit = {
-        str.println(s)
-        //stdout.println(s)
+        if(str.nonEmpty) str.get.println(s) else
+        stdout.println(s)
       }
       override def buffer[T](f: => T) = f
     }
     val ret: Int = if (isWindows)
-                Process("cmd /C " + this.runComand, dir) ! (log)
+                Process("cmd /C " + this.runComand, dir.getOrElse(Paths.get(".")).toFile) ! (log)
               else
-                Process(this.runComand, dir) ! (log)
-    str.close()
+                Process(this.runComand, dir.getOrElse(Paths.get(".")).toFile) ! (log)
+    if(logFile.nonEmpty) str.get.close()
     ret
   }
-
 }
