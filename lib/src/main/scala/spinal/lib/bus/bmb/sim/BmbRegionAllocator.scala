67284b095397a2db259639b758adca893145f42c
Charles Papon <charles.papon.90@gmail.com>
2019-07-22 19:10:41 +0200

    BmbBridgeTester can now specify the minimal alignement of accesses

--- lib/src/main/scala/spinal/lib/bus/bmb/sim/BmbRegionAllocator.scala
+++ lib/src/main/scala/spinal/lib/bus/bmb/sim/BmbRegionAllocator.scala
@@ -6,15 +6,17 @@ import spinal.lib.bus.misc.SizeMapping
 import scala.collection.mutable
 import scala.util.Random
 
-case class BmbRegionAllocator(){
+case class BmbRegionAllocator(alignmentMinWidth : Int = 0){
   val allocations = mutable.HashSet[SizeMapping]()
 
+  def align(v : Int) = v & ~((1 << alignmentMinWidth) -1)
   def free(region : SizeMapping) = allocations.remove(region)
   def allocate(addressGen : => Int, sizeMax : Int, p : BmbParameter, sizeMin : Int = 1) : SizeMapping = {
+    val sizeMinAligned = Math.max(sizeMin, 1 << alignmentMinWidth)
     while(true){
-      var address = addressGen
+      var address = align(addressGen)
       val boundaryMax = Bmb.boundarySize - (address & (Bmb.boundarySize-1))
-      var size = Math.max(sizeMin,Math.min(boundaryMax, Random.nextInt(sizeMax) + 1))
+      var size = Math.max(sizeMinAligned, align(Math.min(boundaryMax, Random.nextInt(sizeMax) + 1)))
       if(!p.alignment.allowByte) {
         address &= ~p.wordMask
         if(size > p.byteCount){
