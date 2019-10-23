a1fc24f7be69d5f01f0c0094d1c7a8fc8ecda1e7
Charles Papon <charles.papon.90@gmail.com>
2019-10-23 23:53:10 +0200

    Fix Sim package

--- sim/src/main/scala/spinal/sim/package.scala
+++ sim/src/main/scala/spinal/sim/package.scala
@@ -4,26 +4,26 @@ import scala.annotation.StaticAnnotation
 import scala.collection.mutable.ArrayBuffer
 
 package object sim {
-  import scala.collection.IterableLike
+//  import scala.collection.IterableLike
   class suspendable extends StaticAnnotation
-  implicit class cpsIterable[A, Repr](xs: IterableLike[A, Repr]) {
-    @deprecated("You can now use regular scala collections utilities as continuation aren't used anymore.", "1.3.0")
-    def suspendable = new {
-      def foreach[B](f: A => Any): Unit = {
-        val it = xs.iterator
-        while(it.hasNext) f(it.next)
-      }
-
-      def map[R](f: A => R): Seq[R] ={
-        val i = xs.iterator
-        val ret = ArrayBuffer[R]()
-        while(i.hasNext){
-          ret += f(i.next())
-        }
-        ret
-      }
-    }
-  }
+//  implicit class cpsIterable[A, Repr](xs: IterableLike[A, Repr]) {
+//    @deprecated("You can now use regular scala collections utilities as continuation aren't used anymore.", "1.3.0")
+//    def suspendable = new {
+//      def foreach[B](f: A => Any): Unit = {
+//        val it = xs.iterator
+//        while(it.hasNext) f(it.next)
+//      }
+//
+//      def map[R](f: A => R): Seq[R] ={
+//        val i = xs.iterator
+//        val ret = ArrayBuffer[R]()
+//        while(i.hasNext){
+//          ret += f(i.next())
+//        }
+//        ret
+//      }
+//    }
+//  }
 
 
   @deprecated("You can now use regular scala collections utilities as continuation aren't used anymore.", "1.3.0")
