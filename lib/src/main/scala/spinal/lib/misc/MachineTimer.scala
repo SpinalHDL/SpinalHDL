284d6d601602fc64d63c334a273f0d6a927401a3
Charles Papon <charles.papon.90@gmail.com>
2019-11-21 21:17:20 +0100

    Add RISC-V machine timer

---
+++ lib/src/main/scala/spinal/lib/misc/MachineTimer.scala
@@ -0,0 +1,21 @@
+package spinal.lib.misc
+
+import spinal.core._
+import spinal.lib.bus.amba3.apb.{Apb3, Apb3SlaveFactory}
+import spinal.lib.slave
+
+case class MachineTimer() extends Component{
+  val io = new Bundle{
+    val bus = slave(Apb3(4, 32))
+    val mTimeInterrupt = out Bool()
+  }
+
+  val mapper = Apb3SlaveFactory(io.bus, dontCareReadData = true)
+  val counter = Reg(UInt(64 bits)) init(0)
+  val cmp = Reg(UInt(64 bits))
+  val interrupt = RegNext(!(counter - cmp).msb)
+  counter := counter + 1
+  io.mTimeInterrupt := interrupt
+  mapper.readMultiWord(counter, 0x0)
+  mapper.writeMultiWord(cmp, 0x8)
+}
