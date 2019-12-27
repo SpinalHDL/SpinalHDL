678dfd9b4780bd030bedaa65ebeb676a6c258009
Luca Cristaldi <wifasoi@gmail.com>
2019-12-27 23:12:03 +0100

    renaming calsses to fit new naming style

--- lib/src/main/scala/spinal/lib/eda/symbiflow/workflows.scala
+++ lib/src/main/scala/spinal/lib/eda/symbiflow/workflows.scala
@@ -6,14 +6,16 @@ import spinal.core._
 import scala.collection._
 import java.nio.file.{Path, Paths}
 
-object YosysFlow {
-  def ice40Flow[T <: Component](report: SpinalReport[T],
+object SymbiFlow {
+  def icestorm[T <: Component]( report: SpinalReport[T],
+                                target: String = "ice40",
                                 workDir: String = "makeWorkplace") = {
-      InputFile(report) |>(Yosys.load_SystemVerilog(report) + Yosys.synthesize("ice40")).outputFolder(Paths.get(workDir,"syntesys")) |>
-      NextPNR_ice40().outputFolder(Paths.get(workDir,"pnr"))
+    InputFile(report) |>
+    (Yosys.load_SystemVerilog(report) + Yosys.synthesize(target)).outputFolder(Paths.get(workDir,"syntesys")) |>
+    NextPNR_ice40().outputFolder(Paths.get(workDir,"pnr"))
   }
 
-  def formalFlow[T <: Component](report: SpinalReport[T],
+  def formal[T <: Component](report: SpinalReport[T],
                                  mode: String = Mode.bmc,
                                  multiclock: Boolean = false,
                                  memoryMap: Boolean = false,
