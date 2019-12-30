dd6f23865ef7c646c31af91fcce39f59c9c4a18c
Charles Papon <charles.papon.90@gmail.com>
2019-12-30 17:34:02 +0100

    Add AS4C32M16SB timings

--- lib/src/main/scala/spinal/lib/memory/sdram/sdr/SdramDevices.scala
+++ lib/src/main/scala/spinal/lib/memory/sdram/sdr/SdramDevices.scala
@@ -54,6 +54,31 @@ object MT48LC16M16A2 {
 }
 
 
+object AS4C32M16SB {
+  def layout = SdramLayout(
+    generation = SDR,
+    bankWidth   = 2,
+    columnWidth = 10,
+    rowWidth    = 13,
+    dataWidth   = 16
+  )
+
+  def timingGrade7 = SdramTimings(
+    bootRefreshCount =   8,
+    tPOW             = 100 us,
+    tREF             =  64 ms,
+    tRC              =  60 ns,
+    tRFC             =  66 ns,
+    tRAS             =  37 ns,
+    tRP              =  15 ns,
+    tRCD             =  15 ns,
+    cMRD             =   2,
+    tWR              =  10 ns,
+    cWR              =   1
+  )
+}
+
+
 object W9825G6JH6 {
   def layout = SdramLayout(
     generation = SDR,
