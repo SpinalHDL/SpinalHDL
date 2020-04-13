2f9facca72368707acaaa07fa47966cda5523c38
Dolu1990 <charles.papon.90@gmail.com>
2020-04-13 22:31:08 +0200

    fix BmbExclusiveMonitor and BmbInvalidateMonitor memory ordering to properly support cross channel out of order completion

--- lib/src/main/scala/spinal/lib/bus/bmb/BmbExclusive.scala
+++ lib/src/main/scala/spinal/lib/bus/bmb/BmbExclusive.scala
@@ -7,49 +7,138 @@ object BmbExclusiveMonitor{
   def outputParameter(inputParameter : BmbParameter) = inputParameter.copy(canExclusive = false, contextWidth = inputParameter.contextWidth + 1)
 }
 
+object BmbExclusiveMonitorState extends SpinalEnum{
+  val IDLE, FENCE_START, FENCE_BUSY, EMIT = newElement()
+}
+
 //Only LENGTH aligned access for now
 //Ensure exclusive ordering across sources by waiting on conflicts
-case class BmbExclusiveMonitor(inputParameter : BmbParameter) {
+case class BmbExclusiveMonitor(inputParameter : BmbParameter, pendingWriteMax : Int) {
+  import BmbExclusiveMonitorState._
   assert(inputParameter.alignment == BmbParameter.BurstAlignement.LENGTH)
   val io = new Bundle {
     val input = slave(Bmb(inputParameter))
     val output = master(Bmb(BmbExclusiveMonitor.outputParameter(inputParameter)))
   }
 
-  val inputAddress = io.input.cmd.address & ~io.input.cmd.length.maxValue //Pessimistic tracking
-  val sources = for(sourceId <- 0 until 1 << inputParameter.sourceWidth) yield new Area{
-    val valid, lock = RegInit(False)
+  val sourceCount = 1 << inputParameter.sourceWidth
+
+  //Before we create a reservation for a exclusive read, we have to ensure that no pending write could conflict that
+  //future reservation (due to potential reordering), so we implement a fence logic to track pending transaction
+  val fence = new Area{
+    val start = False
+    val done = True
+    val busy = RegInit(False) clearWhen(done) setWhen(start)
+  }
+
+  val exclusiveReadCancel = False
+  val sources = for(sourceId <- 0 until sourceCount) yield new Area{
+    val valid = RegInit(False) //Validity of the reservation for the given source
+    val exclusiveWritePending = RegInit(False)  //While a exclusive write is ongoing, this is used to block all conflicting writes to ensure atomicity
+    val state = RegInit(IDLE)
     val address = Reg(UInt(inputParameter.addressWidth bits))
-    val addressHit = address === inputAddress
+    val length = Reg(UInt(inputParameter.lengthWidth bits))
+    val context = Reg(Bits(inputParameter.contextWidth bits))
+    val addressHit = address >> inputParameter.lengthWidth === io.input.cmd.address >> inputParameter.lengthWidth
     val sourceHit = io.input.cmd.source === sourceId
-    val lockHit = lock && addressHit && !sourceHit
+    val haltSource = state =/= IDLE || exclusiveWritePending
+
+    exclusiveReadCancel setWhen(exclusiveWritePending && addressHit)
 
     when(io.output.rsp.fire && io.output.rsp.source === sourceId && io.output.rsp.context.msb){
-      lock := False
+      exclusiveWritePending := False
     }
 
     when(io.input.cmd.valid && io.input.cmd.isRead && io.input.cmd.exclusive && sourceHit){
-      valid := True
-      address := inputAddress
+      valid := !exclusiveReadCancel
+      address := io.input.cmd.address
+      length := io.input.cmd.length
+      context := io.input.cmd.context
+      state := FENCE_START
     }
     when(addressHit && io.input.cmd.lastFire && io.input.cmd.isWrite){
       valid := False
       when(sourceHit){
-        lock := True
+        exclusiveWritePending := True
       }
+    }
+
+
+    val exclusiveReadCmd = Stream(Fragment(BmbCmd(inputParameter.copy(canWrite = false))))
+    exclusiveReadCmd.valid := False
+    exclusiveReadCmd.opcode := Bmb.Cmd.Opcode.READ
+    exclusiveReadCmd.exclusive := True
+    exclusiveReadCmd.address := address
+    exclusiveReadCmd.length := length
+    exclusiveReadCmd.context := context
+    exclusiveReadCmd.last := True
+
+    switch(state){
+      is(FENCE_START){
+        when(!fence.busy){
+          fence.start := True
+          state := FENCE_BUSY
+        }
+      }
+      is(FENCE_BUSY){
+        when(fence.done){
+          state := EMIT
+        }
+      }
+      is(EMIT){
+        exclusiveReadCmd.valid := True
+        when(exclusiveReadCmd.ready){
+          state := IDLE
+        }
+      }
+    }
+
+    val tracker = new Area{
+      val cmdCounter, rspCounter = Reg(UInt(log2Up(pendingWriteMax) + 1 bits)) init(0)
+      val full = cmdCounter.msb =/= rspCounter.msb && cmdCounter.trim(1) === rspCounter.trim(1)
+      when(io.input.cmd.firstFire){ cmdCounter := cmdCounter + 1 }
+      when(io.input.rsp.firstFire){ rspCounter := rspCounter + 1 }
+
+
+      val target = Reg(UInt(log2Up(pendingWriteMax) + 1 bits))
+      val hit = target === rspCounter
+      val done = Reg(Bool())
+      when(hit){
+        done := True
+      }
+      when(fence.start){
+        target := cmdCounter
+        done := False
+      }
+
+      fence.done clearWhen(!done)
     }
   }
 
+  //output cmd arbitrations
+  val exclusiveReadArbiter = StreamArbiterFactory.roundRobin.transactionLock.build(Fragment(BmbCmd(inputParameter.copy(canWrite = false))), sourceCount)
+  exclusiveReadArbiter.io.inputs <> Vec(sources.map(_.exclusiveReadCmd))
+
+  val cmdArbiter = StreamArbiterFactory.lowerFirst.fragmentLock.build(Fragment(BmbCmd(inputParameter.copy(canWrite = false))), sourceCount)
+  cmdArbiter.io.inputs(0) << exclusiveReadArbiter.io.output
+
+  val inputCmdHalted = io.input.cmd.haltWhen(sources.map(_.haltSource).read(io.input.cmd.source))
+  cmdArbiter.io.inputs(1).arbitrationFrom(inputCmdHalted)
+  cmdArbiter.io.inputs(1).payload.assignSomeByName(inputCmdHalted.payload)
+
   val exclusiveSuccess = sources.map(s => s.valid && s.addressHit).read(io.input.cmd.source)
-  val lockHit = sources.map(_.lockHit).orR
-  val lockSource = sources.map(s => s.lock).read(io.input.cmd.source) //Normaly not used
-  io.output.cmd.arbitrationFrom(io.input.cmd.haltWhen(lockHit || lockSource))
-  io.output.cmd.payload.assignSomeByName(io.input.cmd.payload)
-  io.output.cmd.context.removeAssignments() := (io.input.cmd.exclusive && exclusiveSuccess) ## io.input.cmd.context
+  io.output.cmd.arbitrationFrom(cmdArbiter.io.output.haltWhen(sources.map(_.tracker.full).orR))
+  io.output.cmd.payload.assignSomeByName(cmdArbiter.io.output.payload)
+  io.output.cmd.context.removeAssignments() := (io.input.cmd.exclusive && exclusiveSuccess) ## cmdArbiter.io.output.context
+
+  //output cmd data/mask
+  io.output.cmd.data := io.input.cmd.data
+  io.output.cmd.mask := io.input.cmd.mask
   when(io.input.cmd.exclusive && !exclusiveSuccess){
     io.output.cmd.mask := 0
   }
 
+  //rsp
   io.input.rsp.arbitrationFrom(io.output.rsp)
   io.input.rsp.payload.assignSomeByName(io.output.rsp.payload)
   io.input.rsp.context.removeAssignments() := io.output.rsp.context.resized
--- lib/src/main/scala/spinal/lib/bus/bmb/BmbInvalidateMonitor.scala
+++ lib/src/main/scala/spinal/lib/bus/bmb/BmbInvalidateMonitor.scala
@@ -4,49 +4,60 @@ import spinal.core._
 import spinal.lib._
 
 object BmbInvalidateMonitor{
-  def outputParameter(inputParameter : BmbParameter) = inputParameter.copy(canInvalidate = false, contextWidth = inputParameter.contextWidth + 1)
+  def outputParameter(inputParameter : BmbParameter) = inputParameter.copy(canInvalidate = false, contextWidth = inputParameter.contextWidth + inputParameter.addressWidth + inputParameter.lengthWidth)
 }
 
-case class BmbInvalidateMonitor(inputParameter : BmbParameter, pendingInvMax : Int) {
-  assert(inputParameter.alignment == BmbParameter.BurstAlignement.LENGTH)
+case class BmbInvalidateMonitor(inputParameter : BmbParameter,
+                                pendingInvMax : Int) {
+  val outputParameter = BmbInvalidateMonitor.outputParameter(inputParameter)
+
   val io = new Bundle {
     val input = slave(Bmb(inputParameter))
-    val output = master(Bmb(BmbInvalidateMonitor.outputParameter(inputParameter)))
+    val output = master(Bmb(outputParameter))
+  }
+
+  case class Context() extends Bundle{
+    val context = Bits(inputParameter.contextWidth bits)
+    val address = UInt(inputParameter.addressWidth bits)
+    val length  = UInt(inputParameter.lengthWidth bits)
+    val write = Bool()
+  }
+
+  val cmdLogic = new Area {
+    val cmdContext = Context()
+    cmdContext.context := io.input.cmd.context
+    cmdContext.write := io.input.cmd.isWrite
+    cmdContext.address := io.input.cmd.address
+    cmdContext.length := io.input.cmd.length
+
+    io.output.cmd << io.input.cmd
+    io.output.cmd.context.removeAssignments().assignFromBits(B(cmdContext))
   }
 
 
-  val invSource = io.input.inv.translateWith(io.input.inv.source).toFlow.toStream
-  val ackSource = invSource.queue(pendingInvMax)
-  ackSource.ready := io.input.ack.fire
+  val rspLogic = new Area {
+    val rspContext = io.output.rsp.context.as(Context())
+    val (rspToRsp, rspToInv, rspToSync) = StreamFork3(io.output.rsp)
 
-  val ackCounters = for(sourceId <- 0 until 1 << inputParameter.sourceWidth) yield CounterUpDown(
-    stateCount = log2Up(pendingInvMax) + 1,
-    incWhen    = io.input.ack.fire && ackSource.payload === sourceId,
-    decWhen    = io.output.rsp.fire && io.output.rsp.context.msb
-  )
-  val invCounter = CounterUpDown(
-    stateCount = log2Up(pendingInvMax) + 1,
-    incWhen    = io.input.inv.fire,
-    decWhen    = io.output.rsp.fire && io.output.rsp.context.msb
-  )
+    io.input.rsp << rspToRsp
+    io.input.rsp.context.removeAssignments() := rspContext.context
 
-  val haltInv = invCounter.msb //Protect counter from overflow
-  val haltRsp = ackCounters.map(_.value).read(io.output.rsp.source) === 0 //Ensure that writes is coherent
+    val rspToInvFiltred = rspToInv.takeWhen(rspContext.write)
+    io.input.inv.arbitrationFrom(rspToInvFiltred)
+    io.input.inv.address := rspContext.address
+    io.input.inv.length := rspContext.length
+    io.input.inv.source := rspToInvFiltred.source
+    io.input.inv.all    := False
 
-  val (cmdToOutput, cmdToInv) = StreamFork2(io.input.cmd)
+    val rspToSyncFiltred = rspToInv.translateWith(rspToInv.source).takeWhen(rspContext.write)
+  }
 
-  io.output.cmd << cmdToOutput
-  io.output.rsp.haltWhen(haltRsp) >> io.input.rsp
 
-  io.output.cmd.context.removeAssignments() := cmdToOutput.isWrite ## cmdToOutput.context
-  io.input.rsp.context.removeAssignments()  := io.output.rsp.context.resized
+  val ackLogic = new Area{
+    val syncSource = rspLogic.rspToSyncFiltred.queue(pendingInvMax)
+    syncSource.ready := io.input.ack.fire
 
-  val cmdToInvFiltred = cmdToInv.takeWhen(cmdToInv.isWrite && cmdToInv.isFirst).haltWhen(haltInv)
-  io.input.inv.arbitrationFrom(cmdToInvFiltred)
-  io.input.inv.address := cmdToInvFiltred.address
-  io.input.inv.length := cmdToInvFiltred.length
-  io.input.inv.source := cmdToInvFiltred.source
-  io.input.inv.all    := False
-
-  io.input.ack.ready := True
+    io.input.sync.arbitrationFrom(io.input.ack)
+    io.input.sync.source := syncSource.payload
+  }
 }
