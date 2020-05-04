c9e069704eb18f22dffe794858b1b5e383647847
Andrea Bellandi <andrea.bellandi@desy.de>
2020-05-04 19:32:51 +0200

    fix for 0669b45

--- sim/src/main/resources/VpiPlugin.cpp
+++ sim/src/main/resources/VpiPlugin.cpp
@@ -2,7 +2,11 @@
 #define SHMEM_FILENAME "./shmem_name"
 #endif
 
-#include <boost/interprocess/sync/scoped_lock.hpp>
+#ifndef GLOBAL_RANDOM_SEED
+#define GLOBAL_RANDOM_SEED 0x5EED5EED
+#endif
+
+#include<boost/interprocess/sync/scoped_lock.hpp>
 #include<cassert>
 #include<iostream>
 #include<iomanip>
@@ -19,6 +23,8 @@
 
 using namespace std;
 
+mt19937 global_rand(GLOBAL_RANDOM_SEED);
+uniform_int_distribution<> binary_dis(0, 1);
 managed_shared_memory segment;
 SharedStruct* shared_struct;
 stringstream ss;
@@ -35,8 +41,8 @@ void set_error(string& error_string){
     cout << error_string << endl;
     shared_struct->data.resize(error_string.size());
     std::copy(error_string.begin(),
-         error_string.end(),
-         shared_struct->data.begin());
+            error_string.end(),
+            shared_struct->data.begin());
     shared_struct->data.push_back('\0');
     shared_struct->proc_status.store(ProcStatus::error);
     vpi_control(vpiFinish, -1);
@@ -59,8 +65,8 @@ bool check_error(){
 }
 
 bool register_cb(PLI_INT32(*f)(p_cb_data),
-                 PLI_INT32 reason,
-                 int64_t cycles){
+        PLI_INT32 reason,
+        int64_t cycles){
 
     s_cb_data cbData;
     s_vpi_time simuTime;
@@ -104,7 +110,7 @@ bool print_net_in_module(vpiHandle module_handle, stringstream &msg_ss){
     char* module_name = vpi_get_str(vpiName, module_handle);
     cout << " Signals of " << module_name << endl;
     msg_ss << " Signals of " << module_name << endl;
-    
+
     vpiHandle net_iterator = vpi_iterate(vpiNet,module_handle);
     if(check_error()) return true;
     if(net_iterator){
@@ -112,12 +118,12 @@ bool print_net_in_module(vpiHandle module_handle, stringstream &msg_ss){
             if(check_error()) return true;
             string net_full_name = vpi_get_str(vpiFullName, net_handle);
             if(check_error()) return true;
-            
+
             cout << "    " << net_full_name.c_str() << endl;
             msg_ss << "    " << net_full_name.c_str() << endl;
-    
+
             vpiHandle net = vpi_handle_by_name(const_cast<char*>(net_full_name.c_str()),
-                                                (vpiHandle) NULL);
+                    (vpiHandle) NULL);
 
             if(check_error()) return true;
 
@@ -133,14 +139,14 @@ bool print_net_in_module(vpiHandle module_handle, stringstream &msg_ss){
         cout << "   No handles." << endl;
         msg_ss << "   No handles." << endl;
     }
-    
+
     cout << endl;
     msg_ss << endl;
     return false;
 }
 
 bool print_signals_cmd(){
-        
+
     vpiHandle top_mod_iterator;
     vpiHandle top_mod_handle;
     stringstream msg_ss;
@@ -182,7 +188,7 @@ bool print_signals_cmd(){
 }
 
 bool randomize_in_module(vpiHandle module_handle, mt19937& mt_rand){
-    
+
     vpiHandle net_iterator = vpi_iterate(vpiNet, module_handle);
     if(check_error()) return true;
     if(net_iterator){
@@ -201,19 +207,19 @@ bool randomize_in_module(vpiHandle module_handle, mt19937& mt_rand){
             val_str = ss.str();
             value_struct.value.str = (PLI_BYTE8*)val_str.c_str();
             vpi_put_value(net_handle, 
-                          &value_struct, 
-                          NULL, 
-                          vpiNoDelay);
+                    &value_struct, 
+                    NULL, 
+                    vpiNoDelay);
             if(check_error()) return true;
             vpi_free_object(net_handle);
         }
     } 
-    
+
     return false;
 }
 
 bool randomize_cmd(){
-        
+
     vpiHandle top_mod_iterator;
     vpiHandle top_mod_handle;
     mt19937 mt_rand(shared_struct->seed.load());
@@ -253,9 +259,9 @@ bool randomize_cmd(){
 
 
 bool get_signal_handle_cmd(){
-   
+
     int64_t handle = (int64_t)vpi_handle_by_name((PLI_BYTE8*) shared_struct->data.data(), 
-                                                 NULL);
+            NULL);
     if(check_error()) return true; 
     if(!handle) {
         string error_string("vpi_handle_by_name failed with argument: ");
@@ -268,60 +274,64 @@ bool get_signal_handle_cmd(){
     return false;
 }
 
+
+void sanitize_byte_str(char* byte_str){
+   for(size_t i = 0; i< 8; i++) {
+       if ((byte_str[i] != '0') && (byte_str[i] != '1')) { 
+           char c = binary_dis(global_rand) == 0 ? '0' : '1';
+           byte_str[i] = c;
+       }
+   } 
+}
+
 bool read_cmd(){
-    
-    bool valid = true;
+
     s_vpi_value value_struct;
     value_struct.format = vpiBinStrVal;
     shared_struct->data.clear();
     vpi_get_value((vpiHandle)shared_struct->handle.load(), &value_struct);
     if(check_error()) return true;
     size_t valueStrLen = strlen(value_struct.value.str);
-    
+
     for(size_t i = 0; i < valueStrLen; i++){
         char c = value_struct.value.str[i];
-        if ((c != '0') && (c != '1')){
-            valid = false;
-            cout << "Warning, character \"" << c 
-                 << "\" is not 0 or 1. The returned value is 0" <<endl;
-            break;
+        if ((c != '0') && (c != '1')) {
+            cout << "Warning, value '" << c << "' at position "<< i << " is neither '0' or '1'. value set to a random value." << endl;
         }
     }
-    
-    if(valid) {
-        size_t valueByteLen = valueStrLen/8;
-        size_t bitShift = valueStrLen%8;
 
-        if(bitShift != 0) {
-            char accum_string[9] = "00000000";
-            accum_string[8] = '\0';
-            uint8_t accum = 0;
-            memcpy(accum_string+(8-bitShift),
-                    value_struct.value.str,
-                    bitShift);
+    size_t valueByteLen = valueStrLen/8;
+    size_t bitShift = valueStrLen%8;
 
-            accum = stoul(string(accum_string),
-                    nullptr,
-                    2);
-            shared_struct->data.push_back(accum);
-        }
+    if(bitShift != 0) {
+        char accum_string[9] = "00000000";
+        accum_string[8] = '\0';
+        uint8_t accum = 0;
+        memcpy(accum_string+(8-bitShift),
+                value_struct.value.str,
+                bitShift);
 
-        for(size_t i = 0; i<valueByteLen; i++){
-            char accum_string[9];
-            accum_string[8] = '\0';
-            uint8_t accum = 0;
-            memcpy(accum_string,
-                   value_struct.value.str+bitShift+i*8,
-                   8);
+        sanitize_byte_str(accum_string);
+        accum = stoul(string(accum_string),
+                nullptr,
+                2);
+        shared_struct->data.push_back(accum);
+    }
 
-            accum = stoul(string(accum_string),
-                    nullptr,
-                    2);
+    for(size_t i = 0; i<valueByteLen; i++){
+        char accum_string[9];
+        accum_string[8] = '\0';
+        uint8_t accum = 0;
+        memcpy(accum_string,
+                value_struct.value.str+bitShift+i*8,
+                8);
 
-            shared_struct->data.push_back(accum);
-        }
-    } else {
-        shared_struct->data.push_back(0);
+        sanitize_byte_str(accum_string);
+        accum = stoul(string(accum_string),
+                nullptr,
+                2);
+
+        shared_struct->data.push_back(accum);
     }
 
     return false;
@@ -340,9 +350,9 @@ bool write_cmd(){
     val_str = ss.str();
     value_struct.value.str = (PLI_BYTE8*)val_str.c_str();
     vpi_put_value((vpiHandle)shared_struct->handle.load(), 
-                  &value_struct, 
-                  NULL, 
-                  vpiNoDelay);
+            &value_struct, 
+            NULL, 
+            vpiNoDelay);
 
     return check_error();
 }
@@ -387,12 +397,12 @@ PLI_INT32 rw_cb(p_cb_data){
             case ProcStatus::randomize : run_simulation = randomize_cmd(); break;
             case ProcStatus::close : run_simulation = close_cmd(); break;
             default : {
-                        run_simulation = true; 
-                        string error_string("Invalid state ");
-                        error_string += to_string(static_cast<int8_t>(proc_status));
-                        error_string += " detected";
-                        set_error(error_string);
-                        break;  
+                          run_simulation = true; 
+                          string error_string("Invalid state ");
+                          error_string += to_string(static_cast<int8_t>(proc_status));
+                          error_string += " detected";
+                          set_error(error_string);
+                          break;  
                       }
         }
     } while(!run_simulation);
