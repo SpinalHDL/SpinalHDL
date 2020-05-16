eabb0ccff84e0a7bad075f82b7288eadd8d3ed23
c-thaler <bummberumm@googlemail.com>
2020-05-17 00:39:23 +0200

    add AxiLite4 support

--- lib/src/main/scala/spinal/lib/bus/regif/BusIfAdapter/AxiLite4BusInterface.scala
+++ lib/src/main/scala/spinal/lib/bus/regif/BusIfAdapter/AxiLite4BusInterface.scala
@@ -1,37 +1,60 @@
 package spinal.lib.bus.regif
 
 import spinal.core._
+import spinal.lib._
 import spinal.lib.bus.amba4.axilite.{AxiLite4, AxiLite4B, AxiLite4R}
 import spinal.lib.bus.misc.SizeMapping
 
-//case class AxiLite4BusInterface(bus: AxiLite4, sizeMap: SizeMapping, readSync: Boolean = true)(implicit moduleName: ClassName) extends BusIf{
-//  override def getModuleName = moduleName.name
-//
-//  val readHaltRequest = False
-//  val writeHaltRequest = False
-//
-//  val readError = Bool()
-//  val readData  = Bits(bus.config.dataWidth bits)
-//
-//  if(readSync) {
-//    readError.setAsReg() init False
-//    readData.setAsReg()  init 0
-//  } else {
-//    readError := False
-//    readData := 0
-//  }
-//
-//  val readDataStage = bus.readCmd.stage()
-//  val readRsp = AxiLite4R(bus.config)
-//  val writeRsp = AxiLite4B(bus.config)
-//
-//  writeRsp.setOKAY()
-//  readRsp.setOKAY()
-//  readRsp.data := 0
-//
-//  def readAddress() : UInt = readDataStage.addr
-//  def writeAddress() : UInt = bus.writeCmd.addr
-//
-//  override def readHalt(): Unit = readHaltRequest := True
-//  override def writeHalt(): Unit = writeHaltRequest := True
-//}
+case class AxiLite4BusInterface(bus: AxiLite4, sizeMap: SizeMapping, readSync: Boolean = true, regPre: String = "")(implicit moduleName: ClassName) extends BusIf {
+  override def getModuleName = moduleName.name
+
+  val readError = Bool()
+  val readData  = Bits(bus.config.dataWidth bits)
+
+  if(readSync) {
+    readError.setAsReg() init False
+    readData.setAsReg()  init 0
+  } else {
+    readError := False
+    readData := 0
+  }
+
+  val axiAr = bus.readCmd.stage()
+  val axiR  = Stream(AxiLite4R(bus.config))
+  val axiRValid = Reg(Bool) init(False)
+
+  val axiAw = bus.writeCmd.stage()
+  val axiW  = bus.writeData.stage()
+  val axiB  = Stream(AxiLite4B(bus.config))
+  val axiBValid = Reg(Bool) init(False)
+  
+  axiAr.ready := !axiRValid || axiR.ready
+  axiR.payload.setOKAY()
+  axiR.valid := axiRValid
+  axiR.payload.data := readData
+
+  axiAw.ready := axiAw.valid && axiW.valid
+  axiW.ready  := axiAw.valid && axiW.valid
+  axiB.setOKAY()
+  axiB.valid := axiBValid
+
+  bus.r << axiR
+  bus.b << axiB
+
+  val askWrite  = axiAw.valid
+  val askRead   = axiAr.valid
+  val doWrite   = axiAw.valid && axiW.valid 
+  val doRead    = axiAr.valid && axiAr.ready
+  val writeData = bus.writeData.payload.data
+
+  axiRValid := doRead
+  axiBValid := doWrite
+
+  def readAddress() : UInt = axiAr.addr
+  def writeAddress() : UInt = axiAw.addr
+
+  override def readHalt(): Unit = axiAr.ready := False
+  override def writeHalt(): Unit = axiAw.ready := False
+
+  override def busDataWidth   = bus.config.dataWidth
+}
