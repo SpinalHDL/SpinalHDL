894aa2f024f5a9ba82347789aa4417ef93b20ead
Dolu1990 <charles.papon.90@gmail.com>
2020-05-27 14:07:39 +0200

    Add Apb3Clint

--- lib/src/main/scala/spinal/lib/misc/Clint.scala
+++ lib/src/main/scala/spinal/lib/misc/Clint.scala
@@ -2,6 +2,7 @@ package spinal.lib.misc
 
 import spinal.core._
 import spinal.lib._
+import spinal.lib.bus.amba3.apb.{Apb3, Apb3SlaveFactory}
 import spinal.lib.bus.misc.BusSlaveFactory
 import spinal.lib.bus.wishbone.WishboneConfig
 
@@ -32,5 +33,23 @@ case class Clint(hartCount : Int) extends Area{
       bus.writeMultiWord(harts(hartId).cmp, CMP_ADDR + 8*hartId)
       bus.readAndWrite(harts(hartId).softwareInterrupt, IPI_ADDR + 4*hartId, bitOffset = 0)
     }
+  }
+}
+
+
+case class Apb3Clint(hartCount : Int) extends Component{
+  val io = new Bundle {
+    val bus = slave(Apb3(16, 32))
+    val timerInterrupt = out Bits(hartCount bits)
+    val softwareInterrupt = out Bits(hartCount bits)
+  }
+
+  val factory = Apb3SlaveFactory(io.bus)
+  val logic = Clint(hartCount)
+  logic.driveFrom(factory)
+
+  for(hartId <- 0 until hartCount){
+    io.timerInterrupt(hartId) := logic.harts(hartId).timerInterrupt
+    io.softwareInterrupt(hartId) := logic.harts(hartId).softwareInterrupt
   }
 }
