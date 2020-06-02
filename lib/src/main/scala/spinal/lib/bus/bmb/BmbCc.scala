7d175e77591a0b1e17cf7351aa7561c7ab61e981
Dolu1990 <charles.papon.90@gmail.com>
2020-06-02 19:12:26 +0200

    add BmbCcToggle

--- lib/src/main/scala/spinal/lib/bus/bmb/BmbCc.scala
+++ lib/src/main/scala/spinal/lib/bus/bmb/BmbCc.scala
@@ -16,3 +16,16 @@ case class BmbCcFifo(p: BmbParameter,
   io.output.cmd << io.input.cmd.queue(cmdDepth, inputCd, outputCd)
   io.input.rsp << io.output.rsp.queue(rspDepth, outputCd, inputCd)
 }
+
+
+case class BmbCcToggle(p: BmbParameter,
+                     inputCd: ClockDomain,
+                     outputCd: ClockDomain) extends Component{
+  val io = new Bundle {
+    val input = slave(Bmb(p))
+    val output = master(Bmb(p))
+  }
+
+  io.output.cmd << io.input.cmd.ccToggle(inputCd, outputCd)
+  io.input.rsp << io.output.rsp.ccToggle(outputCd, inputCd)
+}
