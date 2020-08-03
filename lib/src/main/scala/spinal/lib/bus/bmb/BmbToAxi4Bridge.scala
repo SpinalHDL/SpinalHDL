0aa140d43e3ab1a57f0db49dae301dc6bd305db0
Dolu1990 <charles.papon.90@gmail.com>
2020-08-03 14:40:53 +0200

    Add more BmbToAxi4 bridges

--- lib/src/main/scala/spinal/lib/bus/bmb/BmbToAxi4Bridge.scala
+++ lib/src/main/scala/spinal/lib/bus/bmb/BmbToAxi4Bridge.scala
@@ -80,3 +80,104 @@ case class BmbToAxi4SharedBridge(bmbConfig : BmbParameter, pendingMax : Int = 7)
   rspInfo.ready := io.input.rsp.fire && io.input.rsp.last
 
 }
+
+
+
+case class BmbToAxi4ReadOnlyBridge(p : BmbParameter) extends Component{
+  val axiConfig = Axi4Config(
+    addressWidth = p.access.addressWidth,
+    dataWidth    = p.access.dataWidth,
+    idWidth      = p.access.sourceWidth,
+    useId        = true,
+    useSize      = true,
+    useLen       = true,
+    useLast      = true,
+    useResp      = true,
+    useStrb      = true,
+    useProt      = true,
+    useCache     = true,
+    useQos       = false,
+    useRegion    = false,
+    useBurst     = false,
+    useLock      = false
+  )
+
+  val io = new Bundle {
+    val input = slave(Bmb(p))
+    val output = master(Axi4ReadOnly(axiConfig))
+  }
+
+  val contextRemover = BmbContextRemover(p, pendingMax = 7)
+  contextRemover.io.input << io.input
+
+  io.output.ar.arbitrationFrom(contextRemover.io.output.cmd)
+  io.output.ar.addr   := contextRemover.io.output.cmd.address
+  io.output.ar.len    := contextRemover.io.output.cmd.transferBeatCountMinusOne.resized
+  io.output.ar.size   := log2Up(p.access.byteCount)
+  io.output.ar.prot   := "010"
+  io.output.ar.cache  := "1111"
+
+  contextRemover.io.output.rsp.arbitrationFrom(io.output.r)
+  contextRemover.io.output.rsp.last    := io.output.r.last
+  contextRemover.io.output.rsp.data    := io.output.r.data
+  contextRemover.io.output.rsp.source  := io.output.r.id
+  when(io.output.r.isOKAY()){
+    io.input.rsp.setSuccess()
+  } otherwise {
+    io.input.rsp.setError()
+  }
+}
+
+
+
+case class BmbToAxi4WriteOnlyBridge(p : BmbParameter) extends Component{
+  val axiConfig = Axi4Config(
+    addressWidth = p.access.addressWidth,
+    dataWidth    = p.access.dataWidth,
+    idWidth      = p.access.sourceWidth,
+    useId        = true,
+    useSize      = true,
+    useLen       = true,
+    useLast      = true,
+    useResp      = true,
+    useStrb      = true,
+    useProt      = true,
+    useCache     = true,
+    useQos       = false,
+    useRegion    = false,
+    useBurst     = false,
+    useLock      = false
+  )
+
+  val io = new Bundle {
+    val input = slave(Bmb(p))
+    val output = master(Axi4WriteOnly(axiConfig))
+  }
+
+  val contextRemover = BmbContextRemover(p, pendingMax = 7)
+  contextRemover.io.input << io.input
+
+  val (cmdFork, dataFork) = StreamFork2(contextRemover.io.output.cmd)
+  val cmdStage  = cmdFork.throwWhen(!contextRemover.io.output.cmd.first)
+
+  io.output.aw.arbitrationFrom(cmdStage)
+  io.output.aw.addr   := cmdStage.address
+  io.output.aw.len    := cmdStage.transferBeatCountMinusOne.resized
+  io.output.aw.size   := log2Up(p.access.byteCount)
+  io.output.aw.prot   := "010"
+  io.output.aw.cache  := "1111"
+
+  io.output.w.arbitrationFrom(dataFork)
+  io.output.w.data := dataFork.data
+  io.output.w.strb := dataFork.mask
+  io.output.w.last := dataFork.last
+
+  contextRemover.io.output.rsp.arbitrationFrom(io.output.b)
+  contextRemover.io.output.rsp.last    := True
+  contextRemover.io.output.rsp.source  := io.output.b.id
+  when(io.output.b.isOKAY()){
+    io.input.rsp.setSuccess()
+  } otherwise {
+    io.input.rsp.setError()
+  }
+}
