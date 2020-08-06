86b11ac8bb5bcceb5354b8c27d87fb19ca1db437
Dolu1990 <charles.papon.90@gmail.com>
2020-08-06 13:54:29 +0200

    Fix BmbUpSizerBridge when write only

--- tester/src/test/scala/spinal/tester/scalatest/SpinalSimBmbUpSizerBridgeTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/SpinalSimBmbUpSizerBridgeTester.scala
@@ -8,85 +8,88 @@ import spinal.lib.bus.bmb.{BmbAccessParameter, BmbParameter, BmbSourceParameter,
 import scala.collection.mutable
 
 class SpinalSimBmbUpSizerBridgeTester extends SpinalSimFunSuite{
-  test("test1") {
-    SimConfig.compile {
-      val inputParameter =  BmbAccessParameter(
-        addressWidth = 16,
-        dataWidth = 32
-      ).addSources(16, BmbSourceParameter(
-        lengthWidth = 5,
-        contextWidth = 3,
-        canRead = true,
-        canWrite = true,
-        alignment = BmbParameter.BurstAlignement.BYTE
-      )).toBmbParameter()
+  for((canRead, canWrite) <- List((true,true),(true, false), (false, true))) {
+    val prefix = (if(canRead) "R" else "") + (if(canWrite) "W" else "") + "_"
+    test(prefix + "test1") {
+      SimConfig.compile {
+        val inputParameter = BmbAccessParameter(
+          addressWidth = 16,
+          dataWidth = 32
+        ).addSources(16, BmbSourceParameter(
+          lengthWidth = 5,
+          contextWidth = 3,
+          canRead  = canRead,
+          canWrite = canWrite,
+          alignment = BmbParameter.BurstAlignement.BYTE
+        )).toBmbParameter()
 
-      BmbUpSizerBridge(
-        inputParameter = inputParameter,
-        outputParameter = BmbUpSizerBridge.outputParameterFrom(inputParameter.access, 64).toBmbParameter()
-      )
-    }.doSimUntilVoid("test", 42) { dut =>
-      new BmbBridgeTester(
-        master = dut.io.input,
-        masterCd = dut.clockDomain,
-        slave = dut.io.output,
-        slaveCd = dut.clockDomain,
-        rspCountTarget = (300*durationFactor).toInt
-      )
+        BmbUpSizerBridge(
+          inputParameter = inputParameter,
+          outputParameter = BmbUpSizerBridge.outputParameterFrom(inputParameter.access, 64).toBmbParameter()
+        )
+      }.doSimUntilVoid("test", 42) { dut =>
+        new BmbBridgeTester(
+          master = dut.io.input,
+          masterCd = dut.clockDomain,
+          slave = dut.io.output,
+          slaveCd = dut.clockDomain,
+          rspCountTarget = (300 * durationFactor).toInt
+        )
+      }
     }
-  }
 
-  test("test2") {
-    SimConfig.compile {
-      val inputParameter = BmbAccessParameter(
-        addressWidth = 16,
-        dataWidth = 16
-      ).addSources(16, BmbSourceParameter(
-        lengthWidth = 5,
-        contextWidth = 3,
-        canRead = true,
-        canWrite = true,
-        alignment = BmbParameter.BurstAlignement.BYTE
-      )).toBmbParameter()
-      BmbUpSizerBridge(
-        inputParameter = inputParameter,
-        outputParameter = BmbUpSizerBridge.outputParameterFrom(inputParameter.access, 64).toBmbParameter()
-      )
-    }.doSimUntilVoid("test") { dut =>
-      new BmbBridgeTester(
-        master = dut.io.input,
-        masterCd = dut.clockDomain,
-        slave = dut.io.output,
-        slaveCd = dut.clockDomain,
-        rspCountTarget = (300*durationFactor).toInt
-      )
+    test(prefix + "test2") {
+      SimConfig.compile {
+        val inputParameter = BmbAccessParameter(
+          addressWidth = 16,
+          dataWidth = 16
+        ).addSources(16, BmbSourceParameter(
+          lengthWidth = 5,
+          contextWidth = 3,
+          canRead  = canRead,
+          canWrite = canWrite,
+          alignment = BmbParameter.BurstAlignement.BYTE
+        )).toBmbParameter()
+        BmbUpSizerBridge(
+          inputParameter = inputParameter,
+          outputParameter = BmbUpSizerBridge.outputParameterFrom(inputParameter.access, 64).toBmbParameter()
+        )
+      }.doSimUntilVoid("test") { dut =>
+        new BmbBridgeTester(
+          master = dut.io.input,
+          masterCd = dut.clockDomain,
+          slave = dut.io.output,
+          slaveCd = dut.clockDomain,
+          rspCountTarget = (300 * durationFactor).toInt
+        )
+      }
     }
-  }
 
-  test("test3") {
-    SimConfig.compile {
-      val inputParameter = BmbAccessParameter(
-        addressWidth = 16,
-        dataWidth = 32
-      ).addSources(16, BmbSourceParameter(
-        lengthWidth = 5,
-        contextWidth = 3,
-        canRead = true,
-        canWrite = true,
-        alignment = BmbParameter.BurstAlignement.BYTE
-      )).toBmbParameter()
-      BmbUpSizerBridge(
-        inputParameter = inputParameter,
-        outputParameter = BmbUpSizerBridge.outputParameterFrom(inputParameter.access, 256).toBmbParameter()
-      )
-    }.doSimUntilVoid("test") { dut =>
-      new BmbBridgeTester(
-        master = dut.io.input,
-        masterCd = dut.clockDomain,
-        slave = dut.io.output,
-        slaveCd = dut.clockDomain,
-        rspCountTarget = (300*durationFactor).toInt
-      )
+    test(prefix + "test3") {
+      SimConfig.compile {
+        val inputParameter = BmbAccessParameter(
+          addressWidth = 16,
+          dataWidth = 32
+        ).addSources(16, BmbSourceParameter(
+          lengthWidth = 5,
+          contextWidth = 3,
+          canRead  = canRead,
+          canWrite = canWrite,
+          alignment = BmbParameter.BurstAlignement.BYTE
+        )).toBmbParameter()
+        BmbUpSizerBridge(
+          inputParameter = inputParameter,
+          outputParameter = BmbUpSizerBridge.outputParameterFrom(inputParameter.access, 256).toBmbParameter()
+        )
+      }.doSimUntilVoid("test") { dut =>
+        new BmbBridgeTester(
+          master = dut.io.input,
+          masterCd = dut.clockDomain,
+          slave = dut.io.output,
+          slaveCd = dut.clockDomain,
+          rspCountTarget = (300 * durationFactor).toInt
+        )
+      }
     }
   }
 }
