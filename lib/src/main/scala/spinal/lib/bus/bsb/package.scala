3be1ab9d449f5ba7e452acb52aad51344852f5bd
Dolu1990 <charles.papon.90@gmail.com>
2020-09-10 10:40:25 +0200

    Add a few Bsb utilities

--- lib/src/main/scala/spinal/lib/bus/bsb/package.scala
+++ lib/src/main/scala/spinal/lib/bus/bsb/package.scala
@@ -1,14 +1,32 @@
 package spinal.lib.bus
+import spinal.core._
+import spinal.lib.{Fragment, Stream}
 
 import spinal.core._
 
 package object bsb {
   type Bsb = spinal.lib.Stream[BsbTransaction]
 
-  implicit class BsbPimper[T <: Data](pimped : Bsb){
+  implicit class BsbPimper(pimped : Bsb) {
     def firstSink = {
       val regs = (0 until 1 << pimped.p.sinkWidth).map(sinkId => RegNextWhen(pimped.last, pimped.fire && pimped.sink === sinkId, True))
       B(regs)
+    }
+
+    def toStreamFragment(omitMask : Boolean = false) = {
+      assert(omitMask || !pimped.p.withMask)
+      val s = Stream(Fragment(Bits(pimped.p.byteCount*8 bits)))
+      s.arbitrationFrom(pimped)
+      s.fragment := pimped.data
+      s.last := pimped.last
+      s
+    }
+    def toStream(omitMask : Boolean = false) = {
+      assert(omitMask || !pimped.p.withMask)
+      val s = Stream(Bits(pimped.p.byteCount*8 bits))
+      s.arbitrationFrom(pimped)
+      s.payload := pimped.data
+      s
     }
   }
 }
