546fa2e2d7b87fe667bf5e1adc5a46e775ea8656
Dolu1990 <charles.papon.90@gmail.com>
2020-09-21 20:11:40 +0200

    BsbDownSizerAlignedMultiWidth added

--- lib/src/main/scala/spinal/lib/bus/bsb/BsbDownSizerSparse.scala
+++ lib/src/main/scala/spinal/lib/bus/bsb/BsbDownSizerSparse.scala
@@ -26,3 +26,43 @@ class BsbDownSizerSparse(val p : BsbParameter, outputBytes : Int) extends Compon
   io.output.sink   := io.input.sink
   io.output.last   := io.input.last && end
 }
+
+
+case class BsbDownSizerAlignedMultiWidth(val p : BsbParameter, outputBytes : List[Int]) extends Component{
+  val io = new Bundle{
+    val sel = in UInt(log2Up(outputBytes.length) bits)
+    val input = slave(Bsb(p))
+    val output = master(Bsb(p.copy(byteCount = outputBytes.max, withMask = false)))
+  }
+
+  assert(p.byteCount >= outputBytes.max)
+  val counter = Reg(UInt(log2Up(p.byteCount) bits)) init(0)
+  val end = False
+
+  when(io.output.fire){
+    counter := counter + outputBytes.map(U(_)).read(io.sel).resized
+  }
+  when(io.input.fire){
+    counter := 0
+  }
+
+  io.input.ready := io.output.ready && end
+  io.output.valid := io.input.valid
+  io.output.source := io.input.source
+  io.output.sink   := io.input.sink
+  io.output.last   := io.input.last && end
+
+  io.output.data.assignDontCare()
+  io.output.mask := 0
+
+  switch(io.sel){
+    for((bytes, id) <- outputBytes.zipWithIndex){
+      is(id){
+        val masks = io.input.mask.subdivideIn(bytes bits)
+        io.output.data(0, bytes*8 bits) := io.input.data.subdivideIn(bytes*8 bits).read(counter >> log2Up(bytes))
+        io.output.mask(0, bytes bits)   := masks.read(counter >> log2Up(bytes))
+        end setWhen(counter === p.byteCount - bytes)
+      }
+    }
+  }
+}
--- tester/src/test/scala/spinal/tester/scalatest/SpinalSimBsbTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/SpinalSimBsbTester.scala
@@ -6,7 +6,7 @@ import spinal.core._
 import spinal.core.sim._
 import spinal.lib.bus.bmb.sim.{BmbDriver, BmbMemoryAgent}
 import spinal.lib.bus.bmb.{Bmb, BmbParameter, BmbSlaveFactory}
-import spinal.lib.bus.bsb.{Bsb, BsbDownSizerSparse, BsbParameter, BsbUpSizerDense, BsbUpSizerSparse}
+import spinal.lib.bus.bsb.{Bsb, BsbDownSizerAlignedMultiWidth, BsbDownSizerSparse, BsbParameter, BsbUpSizerDense, BsbUpSizerSparse}
 import spinal.lib.bus.bsb.sim.BsbBridgeTester
 import spinal.lib._
 import spinal.lib.system.dma.sg.{DmaSg, DmaSgTester, SgDmaTestsParameter}
@@ -52,6 +52,58 @@ class SpinalSimBsbTester extends FunSuite{
       )
     }
   }
+
+  for(id <- List(0,1)) {
+    test(s"BsbDownSizerAlignedMultiWidth0_$id") {
+      SimConfig.withWave.compile(new BsbDownSizerAlignedMultiWidth(
+        p = BsbParameter(
+          byteCount = 8,
+          sourceWidth = 3,
+          sinkWidth = 4
+        ),
+        outputBytes = List(2, 4)
+      ) {
+        val buffer = slave(Bsb(p))
+        io.input.setAsDirectionLess.allowDirectionLessIo << buffer.stage()
+      }).doSim(seed = 42) { dut =>
+        dut.clockDomain.forkStimulus(10)
+        dut.io.sel #= id
+        new BsbBridgeTester(
+          input = dut.buffer,
+          output = dut.io.output,
+          inputCd = dut.clockDomain,
+          outputCd = dut.clockDomain
+        )
+      }
+    }
+  }
+
+  for(id <- List(0,1)) {
+    test(s"BsbDownSizerAlignedMultiWidth1_$id") {
+      SimConfig.withWave.compile(new BsbDownSizerAlignedMultiWidth(
+        p = BsbParameter(
+          byteCount = 8,
+          sourceWidth = 3,
+          sinkWidth = 4
+        ),
+        outputBytes = List(1, 8)
+      ) {
+        val buffer = slave(Bsb(p))
+        io.input.setAsDirectionLess.allowDirectionLessIo << buffer.stage()
+      }).doSim(seed = 42) { dut =>
+        dut.clockDomain.forkStimulus(10)
+        dut.io.sel #= id
+        new BsbBridgeTester(
+          input = dut.buffer,
+          output = dut.io.output,
+          inputCd = dut.clockDomain,
+          outputCd = dut.clockDomain
+        )
+      }
+    }
+  }
+
+
 
 
   test("upSizerDense"){
