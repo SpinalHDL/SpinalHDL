5c86f0efdc204c49764518fd8ea3625ed8fb3fee
Dolu1990 <charles.papon.90@gmail.com>
2020-12-01 13:47:50 +0100

    BmbToAxi4 bridge higher pending default

--- lib/src/main/scala/spinal/lib/bus/bmb/BmbToAxi4Bridge.scala
+++ lib/src/main/scala/spinal/lib/bus/bmb/BmbToAxi4Bridge.scala
@@ -4,7 +4,7 @@ import spinal.core._
 import spinal.lib._
 import spinal.lib.bus.amba4.axi._
 
-case class BmbToAxi4SharedBridge(bmbConfig : BmbParameter, pendingMax : Int = 7) extends Component{
+case class BmbToAxi4SharedBridge(bmbConfig : BmbParameter, pendingMax : Int = 31) extends Component{
   val axiConfig = Axi4Config(
     addressWidth = bmbConfig.access.addressWidth,
     dataWidth    = bmbConfig.access.dataWidth,
@@ -28,12 +28,23 @@ case class BmbToAxi4SharedBridge(bmbConfig : BmbParameter, pendingMax : Int = 7)
     val output = master(Axi4Shared(axiConfig))
   }
 
-  val pendingCounter = CounterUpDown(
-    stateCount = pendingMax + 1,
-    incWhen = io.input.cmd.fire && io.input.cmd.last,
-    decWhen = io.input.rsp.fire && io.input.rsp.last
-  )
-  val pendingWrite = RegNextWhen(io.input.cmd.isWrite, io.input.cmd.fire)
+
+  val pendingWrite = Bool().assignDontCare()
+  val pendingCounter = UInt(log2Up(pendingMax + 1) bits).assignDontCare()
+
+  val states = for(sourceId <- bmbConfig.access.sources.keys) yield new Area{
+    val counter = CounterUpDown(
+      stateCount = pendingMax + 1,
+      incWhen = io.input.cmd.source === sourceId && io.input.cmd.fire && io.input.cmd.last,
+      decWhen = io.input.rsp.source === sourceId && io.input.rsp.fire && io.input.rsp.last
+    )
+    val write = RegNextWhen(io.input.cmd.isWrite, io.input.cmd.source === sourceId && io.input.cmd.fire)
+
+    when(io.input.cmd.source === sourceId){
+      pendingWrite := write
+      pendingCounter := counter
+    }
+  }
 
   val hazard = (io.input.cmd.isWrite =/= pendingWrite && pendingCounter =/= 0) || pendingCounter === pendingMax
   val (cmdFork, dataFork) = StreamFork2(io.input.cmd.haltWhen(hazard))
@@ -45,14 +56,18 @@ case class BmbToAxi4SharedBridge(bmbConfig : BmbParameter, pendingMax : Int = 7)
     val context = cloneOf(io.input.cmd.context)
   }
 
-  val cmdInfo = Stream(Info())
-  cmdInfo.valid := cmdStage.fire
-  cmdInfo.source := cmdStage.source
-  cmdInfo.context := cmdStage.context
+  val writeCmdInfo, readCmdInfo = Stream(Info())
+  writeCmdInfo.valid := cmdStage.fire && cmdStage.first && cmdStage.isWrite
+  writeCmdInfo.source := cmdStage.source
+  writeCmdInfo.context := cmdStage.context
+  readCmdInfo.valid  := cmdStage.fire && cmdStage.first && cmdStage.isRead
+  readCmdInfo.source := cmdStage.source
+  readCmdInfo.context := cmdStage.context
 
-  val rspInfo = cmdInfo.queue(size = 1 << log2Up(pendingMax)).halfPipe()
+  val writeRspInfo = writeCmdInfo.queue(size = 1 << log2Up(pendingMax)).halfPipe()
+  val readRspInfo = readCmdInfo.queue(size = 1 << log2Up(pendingMax)).halfPipe()
 
-  io.output.arw.arbitrationFrom(cmdStage)
+  io.output.arw.arbitrationFrom(cmdStage.haltWhen(!writeCmdInfo.ready || !readCmdInfo.ready))
   io.output.arw.write  := io.input.cmd.isWrite
   io.output.arw.addr   := io.input.cmd.address
   io.output.arw.len    := io.input.cmd.transferBeatCountMinusOne.resized
@@ -65,19 +80,33 @@ case class BmbToAxi4SharedBridge(bmbConfig : BmbParameter, pendingMax : Int = 7)
   io.output.w.strb := dataStage.mask
   io.output.w.last := dataStage.last
 
-  io.input.rsp.valid := (io.output.b.valid | io.output.r.valid) && rspInfo.valid
-  io.input.rsp.last := (pendingWrite ? True | io.output.r.last)
+  val rspSelLock = RegInit(False) setWhen(io.output.r.valid || io.output.b.valid) clearWhen((io.output.r.fire && io.output.r.last) || io.output.b.fire)
+  val rspSelReadLast = RegNextWhen(io.output.r.valid, !rspSelLock)
+  val rspSelRead = rspSelLock ? rspSelReadLast | io.output.r.valid
+
+  io.output.b.ready := io.input.rsp.ready && !rspSelRead && writeRspInfo.valid
+  io.output.r.ready := io.input.rsp.ready && rspSelRead && readRspInfo.valid
+  writeRspInfo.ready := io.input.rsp.lastFire && !rspSelRead
+  readRspInfo.ready  := io.input.rsp.lastFire && rspSelRead
+
   io.input.rsp.data := io.output.r.data
-  io.input.rsp.source := rspInfo.source
-  io.input.rsp.context := rspInfo.context
-  when((pendingWrite ? io.output.b.isOKAY() | io.output.r.isOKAY())){
+  when(rspSelRead){
+    io.input.rsp.valid := io.output.r.valid && readRspInfo.valid
+    io.input.rsp.last := io.output.r.last
+    io.input.rsp.source :=  readRspInfo.source
+    io.input.rsp.context := readRspInfo.context
+  } otherwise {
+    io.input.rsp.valid := io.output.b.valid && writeRspInfo.valid
+    io.input.rsp.last := True
+    io.input.rsp.source := writeRspInfo.source
+    io.input.rsp.context := writeRspInfo.context
+  }
+  when((rspSelRead ? io.output.r.isOKAY() | io.output.b.isOKAY())){
     io.input.rsp.setSuccess()
   } otherwise {
     io.input.rsp.setError()
   }
-  io.output.b.ready := io.input.rsp.ready && rspInfo.valid
-  io.output.r.ready := io.input.rsp.ready && rspInfo.valid
-  rspInfo.ready := io.input.rsp.fire && io.input.rsp.last
+
 }
 
 
