430021c5ff9edde8829e344dae718634f80f2553
Dolu1990 <charles.papon.90@gmail.com>
2021-01-01 23:57:10 +0100

    increase BmbDecoder default pendingMax, add BmbErrorSlave + BmbDecoderPerSource

--- lib/src/main/scala/spinal/lib/bus/bmb/BmbDecoder.scala
+++ lib/src/main/scala/spinal/lib/bus/bmb/BmbDecoder.scala
@@ -10,7 +10,7 @@ import spinal.lib._
 case class BmbDecoder(p : BmbParameter,
                       mappings : Seq[AddressMapping],
                       capabilities : Seq[BmbParameter],
-                      pendingMax : Int = 15) extends Component{
+                      pendingMax : Int = 63) extends Component{
   assert(!AddressMapping.verifyOverlapping(mappings), "BMB address decoding overlapping")
 
   val io = new Bundle {
@@ -85,6 +85,102 @@ case class BmbDecoder(p : BmbParameter,
       io.input.cmd.ready := False
       io.outputs.foreach(_.cmd.valid := False)
     }
+  }
+}
+
+
+case class BmbErrorSlave(p : BmbParameter) extends Component{
+  val io = new Bundle {
+    val input = slave(Bmb(p))
+  }
+
+  val busy = RegInit(False)
+  val beats = Reg(UInt(p.access.beatCounterWidth bits))
+  val source = Reg(UInt(p.access.sourceWidth bits))
+  val context = Reg(Bits(p.access.contextWidth bits))
+
+  io.input.rsp.setError()
+  io.input.rsp.source := source
+  io.input.rsp.context := context
+  if(p.access.canExclusive) io.input.rsp.exclusive := False
+  if(p.access.canWrite) io.input.rsp.data.assignDontCare()
+
+  when(!busy){
+    io.input.cmd.ready := True
+    busy := io.input.cmd.valid
+    beats := io.input.cmd.transferBeatCountMinusOne
+    source := io.input.cmd.source
+    context := io.input.cmd.context
+  } otherwise {
+    io.input.cmd.ready := False
+    io.input.rsp.valid := True
+    when(io.input.rsp.ready){
+      beats := beats - 1
+      when(beats === 0){
+        busy := False
+      }
+    }
+  }
+}
+
+case class BmbDecoderPerSource  (p : BmbParameter,
+                      mappings : Seq[AddressMapping],
+                      capabilities : Seq[BmbParameter],
+                      pendingMax : Int = 63) extends Component{
+  assert(!AddressMapping.verifyOverlapping(mappings), "BMB address decoding overlapping")
+
+  val io = new Bundle {
+    val input = slave(Bmb(p))
+    val outputs = Vec(master(Bmb(p)), mappings.size)
+  }
+  val hasDefault = mappings.contains(DefaultMapping)
+  assert(mappings.count(_ == DefaultMapping) < 2, "Multiple interface with DefaultMapping in decoder")
+  val logic = if(hasDefault && mappings.size == 1 && !(p.access.canWrite && !capabilities.head.access.canWrite) && !(p.access.canRead && !capabilities.head.access.canRead)){
+    io.outputs(0) << io.input
+  } else new Area {
+    var internalMapping = mappings
+    var internalOutputs = io.outputs.toList
+    if(!hasDefault){
+      val error = BmbErrorSlave(p)
+      internalMapping :+= DefaultMapping
+      internalOutputs :+= error.io.input
+    }
+
+    val hits = Vec(Bool, internalMapping.size)
+    for (portId <- 0 until internalMapping.length) yield {
+      val slaveBus = internalOutputs(portId)
+      val memorySpace = mappings(portId)
+      val capability = capabilities(portId)
+      val hit = hits(portId)
+      hit := (memorySpace match {
+        case DefaultMapping => !hits.filterNot(_ == hit).orR
+        case _ => memorySpace.hit(io.input.cmd.address)
+      })
+      if(!capability.access.canWrite) hit clearWhen(io.input.cmd.isWrite)
+      if(!capability.access.canRead) hit clearWhen(io.input.cmd.isRead)
+      slaveBus.cmd.valid := io.input.cmd.valid && hit
+      slaveBus.cmd.payload := io.input.cmd.payload.resized
+    }
+
+    io.input.cmd.ready := (hits, internalOutputs).zipped.map(_ && _.cmd.ready).orR
+
+    val sources = for((id, _) <- p.access.sources) yield new Area {
+      val hit = io.input.cmd.source === id
+      val rspHits = RegNextWhen(hits, io.input.cmd.fire && hit)
+      val rspPendingCounter = Reg(UInt(log2Up(pendingMax + 1) bits)) init(0)
+      rspPendingCounter := rspPendingCounter + U(io.input.cmd.lastFire && io.input.cmd.source === id) - U(io.input.rsp.lastFire && io.input.rsp.source === id)
+
+      val rspPending = rspPendingCounter =/= 0
+      val full = rspPendingCounter === pendingMax
+      val cmdWait = hit && (rspPending && hits =/= rspHits || full)
+    }
+
+    when(sources.map(_.cmdWait).toSeq.orR) {
+      io.input.cmd.ready := False
+      internalOutputs.foreach(_.cmd.valid := False)
+    }
+
+    io.input.rsp << StreamArbiterFactory.fragmentLock.roundRobin.on(internalOutputs.map(_.rsp))
   }
 }
 
