1297c91dd20890b6229a536aa87a900fdce174d5
Sebastien Van Cauwenberghe <svancau@gmail.com>
2021-02-14 20:03:10 +0100

    Replace in sim logging by trace file

--- lib/src/main/scala/spinal/lib/bus/bmb/BmbL2CacheRandomTester.scala
+++ lib/src/main/scala/spinal/lib/bus/bmb/BmbL2CacheRandomTester.scala
@@ -5,17 +5,18 @@ import spinal.lib.bus.bmb.sim.{BmbMasterAgent, BmbMemoryAgent, BmbMonitor, BmbRe
 import spinal.lib.bus.bmb.{Bmb, BmbAccessParameter, BmbExclusiveMonitor, BmbInvalidationParameter, BmbL2Cache, BmbL2CacheParameter, BmbParameter, BmbSourceParameter}
 import spinal.lib.bus.misc.SizeMapping
 import spinal.lib.sim.{Phase, SparseMemory}
+import java.io._
 
 import scala.util.Random
 object BmbL2CacheRandomTester extends App{
   val addrBusSize = 24
-  val addrRandomSize = 17
+  val addrRandomSize = 24
   assert(addrRandomSize <= addrBusSize)
 
   val p = BmbL2CacheParameter(
     lineLength = 64,
     waySize = 32768,
-    wayCount = 2
+    wayCount = 4
   )
   val ip = BmbParameter(
     access =  BmbAccessParameter(
@@ -49,7 +50,7 @@ object BmbL2CacheRandomTester extends App{
     ).addSources(
       count = 1,
       p = BmbSourceParameter(
-        contextWidth               = 11,
+        contextWidth               = 12,
         lengthWidth                = 6,
         alignment                  = BmbParameter.BurstAlignement.LENGTH,
         alignmentMin               = 0,
@@ -68,8 +69,8 @@ object BmbL2CacheRandomTester extends App{
     )
   )
 
-  val compiled = SimConfig.withFstWave.compile {
-  //val compiled = SimConfig.compile {
+  //val compiled = SimConfig.withFstWave.compile {
+  val compiled = SimConfig.compile {
     val dut = BmbL2Cache(p, ip, op)
     dut.s2.input.simPublic()
     dut.s2.S2out.simPublic()
@@ -83,10 +84,12 @@ object BmbL2CacheRandomTester extends App{
     //dut.io.input.inv.valid #= false
     //dut.io.input.ack.ready #= false
     //dut.io.input.sync.valid #= false
+    val trace = false
 
     val lineShift = 6
     val lineMask = (1 << 9) - 1
     def getLine(addr : Int) = (addr >> lineShift) & lineMask
+    val cache_tr = new PrintWriter(new File("cache.txt"))
 
     val memory = new BmbMemoryAgent()
 
@@ -103,38 +106,40 @@ object BmbL2CacheRandomTester extends App{
       val randomVal = Random.nextInt() & 0xFF
       memory.memory.write(i, randomVal)
       cacheState.write(i, randomVal)
-      memory.memory.write(i, i)
-      cacheState.write(i, i)
+      //memory.memory.write(i, i)
+      //cacheState.write(i, i)
     }
 
-    //def filterTransactions(addr : BigInt, data: Byte): Boolean = addr == 0x1CA8C
-    def filterTransactions(addr : BigInt, data: Byte): Boolean = getLine(addr.toInt) == 0x12A
+    def traceRW(address: BigInt, data: Byte, read : Boolean, transact : String): Unit = {
+      if (!trace)
+        return
+
+      val simtime = simTime()
+      val rw = if (read) "R" else 'W'
+      cache_tr.println(f"$simtime,$transact,$rw,$address%08X,$data%02X")
+      cache_tr.flush()
+    }
 
     val regions = BmbRegionAllocator()
     val agent = new BmbMasterAgent(dut.io.input, dut.clockDomain) {
       override def getCmd(): () => Unit = super.getCmd()
 
       override def onRspRead(address: BigInt, data: Byte): Unit = {
-        //print (f"addr 0x$address%X\n")
         val refData = cacheState.read(address.longValue())
-        val lineId = getLine(address.toInt)
-        if (filterTransactions(address, data)) {
-          val simtime = simTime()
-          println(f"IN  RD $address%x $data%02X ($simtime)")
-        }
+        traceRW(address, data, true, "I")
+
         if(refData != data){
-          simFailure(f"Invalid data output from cache got 0x$data%02X expected 0x$refData%02X at 0x$address%X (line 0x$lineId)")
+          simFailure(f"Invalid data output from cache got 0x$data%02X expected 0x$refData%02X at 0x$address%X")
         }
       }
 
-
+      override def onCmdRead(address: BigInt): Unit = {
+        traceRW(address, 0, true, "IR")
+      }
 
       override def onCmdWrite(address: BigInt, data: Byte): Unit = {
         cacheState.write(address.longValue(), data)
-        if (filterTransactions(address, data)) {
-          val simtime = simTime()
-          println(f"IN  WR $address%x $data%02X ($simtime)")
-        }
+        traceRW(address, data, false, "I")
       }
 
       override def regionAllocate(sizeMax: Int): SizeMapping = regions.allocate(Random.nextInt(1 << addrRandomSize), sizeMax, dut.io.input.p, boundarySize=64)
@@ -153,42 +158,35 @@ object BmbL2CacheRandomTester extends App{
         val opc = dut.s2.input.cmd.fragment.opcode.toInt
         val hitId = dut.s2.input.hitId.toInt
         val replaceId = dut.s2.S2out.wayId.toInt
-        val hit = if(hitId == 0) "MISS" else "HIT "
         val len = dut.s2.input.cmd.length.toInt >> 2
         val simtime = simTime()
         opCount += 1
         if (hitId == 0) {
           missCount += 1
         }
-        val rd = if(opc == 0) "RD" else "WR"
-        //val cond = (lineId == 0x282)
-        //val cond = (addr == 0x2F180)
-        val cond = false
-        if (filterTransactions(addr, 0x00)) {
-          print(f"$simtime $rd $hit 0x$addr%06x $replaceId%x 0x$lineId%x $len\n")
-        }
+        val rd = if(opc == 0) "R" else "W"
+       if(trace) {
+         cache_tr.println(f"$simtime,C,$rd,$addr%08X,$hitId%X,$replaceId%X")
+         cache_tr.flush()
+       }
       }
     }
 
     val bmbMon = new BmbMonitor(dut.io.output, dut.clockDomain) {
       var prevReadLine = 0l
       override def getByte(address: Long, value: Byte): Unit = {
-        if (filterTransactions(address, value)) {
-          val simtime = simTime()
-          println(f"DDR RD $address%x $value%02X ($simtime)")
-        }
+        traceRW(address, value, true, "D")
       }
 
       override def setByte(address: Long, value: Byte): Unit = {
-        if (filterTransactions(address, value)) {
-          val simtime = simTime()
-          println(f"DDR  WR $address%x $value%02X ($simtime)")
-        }
+        traceRW(address, value, false, "D")
       }
     }
 
     //dut.clockDomain.waitSampling(50000000)
-    dut.clockDomain.waitSampling(2500000)
+    dut.clockDomain.waitSampling(1000000)
     print (s"Operations $opCount misses $missCount\n")
+    cache_tr.flush()
+    cache_tr.close()
   }
 }
