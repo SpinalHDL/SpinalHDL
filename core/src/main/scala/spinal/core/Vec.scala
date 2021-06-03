cad0fa18058de04ff99051a2d530014916d0f946
Kartik <kartik.prmr@gmail.com>
2021-06-03 21:55:47 +0530

    Fixed issue with naming with vec indexing

--- core/src/main/scala/spinal/core/Vec.scala
+++ core/src/main/scala/spinal/core/Vec.scala
@@ -189,7 +189,7 @@ class Vec[T <: Data](val dataType: HardType[T], val vec: Vector[T]) extends Mult
     val retFlatten = ret.flatten
     for(i <- 0 until vecTransposed.length){
       val target = retFlatten(i)
-      target.setCompositeName(this, s"mux${i}")
+      target.setCompositeName(this, s"mux${i}", true)
       target match {
         case bv: BitVector => bv.unfixWidth()
         case _             =>
