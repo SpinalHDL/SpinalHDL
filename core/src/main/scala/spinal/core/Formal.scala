9f86e95ce839e7d73d5f6417cc5c7bba37229404
Dolu1990 <charles.papon.90@gmail.com>
2021-06-28 14:33:44 +0200

    Fix #359

--- core/src/main/scala/spinal/core/Formal.scala
+++ core/src/main/scala/spinal/core/Formal.scala
@@ -16,15 +16,20 @@ object Formal {
   }
   def past[T <: Data](that : T) : T = past(that, 1)
 
-  def rose(that : Bool) : Bool = that.wrapUnaryOperator(new Operator.Formal.Rose)
-  def fell(that : Bool) : Bool = that.wrapUnaryOperator(new Operator.Formal.Fell)
-  def changed[T <: BaseType](that : T) : Bool = that.wrapUnaryWithBool(new Operator.Formal.Changed)
-  def stable[T <: BaseType](that : T) : Bool = that.wrapUnaryWithBool(new Operator.Formal.Stable)
+  def rose(that : Bool) : Bool = that.rise(True)
+  def fell(that : Bool) : Bool = that.fall(False)
+  def changed[T <: BaseType](that : T, init : T = null) : Bool = RegNext(that, init = init) =/= that
+  def stable[T <: BaseType](that : T, init : T = null) : Bool = RegNext(that, init = init) === that
   def initstate() : Bool = {
     val ret = Bool()
     ret.assignFrom(new Operator.Formal.InitState)
     ret
   }
 
-//  def enabled[T](block : => T) : T = GenerationFlags.formal(block)
+//  def past[T <: Data](that : T, delay : Int) : T = that.formalPast(delay)
+//  def rose(that : Bool) : Bool = that.wrapUnaryOperator(new Operator.Formal.Rose)
+//  def fell(that : Bool) : Bool = that.wrapUnaryOperator(new Operator.Formal.Fell)
+//  def changed[T <: BaseType](that : T) : Bool = that.wrapUnaryWithBool(new Operator.Formal.Changed)
+//  def stable[T <: BaseType](that : T) : Bool = that.wrapUnaryWithBool(new Operator.Formal.Stable)
+  //  def enabled[T](block : => T) : T = GenerationFlags.formal(block)
 }
