787abe04d0aad1b0db010a5ece2735192638d783
Dolu1990 <charles.papon.90@gmail.com>
2021-11-03 16:44:04 +0100

    add Growable.addRet(value)

--- lib/src/main/scala/spinal/lib/Utils.scala
+++ lib/src/main/scala/spinal/lib/Utils.scala
@@ -19,15 +19,16 @@
 package spinal.lib
 
 import spinal.core.internals._
+
 import java.io.UTFDataFormatException
 import java.nio.charset.Charset
-
 import spinal.core._
 
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.ListBuffer
 import scala.collection.Seq
+import scala.collection.generic.Growable
 
 
 object UIntToOh {
@@ -842,7 +843,12 @@ object DelayEvent {
 class NoData extends Bundle {
 
 }
-
+class GrowableAnyPimped[T <: Any](pimped: Growable[T]) {
+  def addRet(that : T): T ={
+    pimped += that
+    that
+  }
+}
 
 class TraversableOnceAnyPimped[T <: Any](pimped: Seq[T]) {
   def apply(id : UInt)(gen : (T) => Unit): Unit ={
