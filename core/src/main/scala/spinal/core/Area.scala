7dea35aacc8ad2c2bc14acc0bde49f44f5bf9273
Dolu1990 <charles.papon.90@gmail.com>
2021-11-22 10:43:00 +0100

    AreaObject now have a strong name

--- core/src/main/scala/spinal/core/Area.scala
+++ core/src/main/scala/spinal/core/Area.scala
@@ -200,5 +200,5 @@ class ResetArea(reset: Bool, cumulative: Boolean) extends Area with PostInitCall
 
 
 trait AreaObject extends Area{
-  setWeakName(this.getClass.getSimpleName.replace("$",""))
+  setName(this.getClass.getSimpleName.replace("$",""))
 }
