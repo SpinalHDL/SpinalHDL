25e1c8991b79d87cbaef00650fab98afe2a17f12
Dolu1990 <charles.papon.90@gmail.com>
2022-03-02 13:26:31 +0100

    reenable idslPlugin

--- idslpayload/src/main/scala/spinal/idslplugin/Macro.scala
+++ idslpayload/src/main/scala/spinal/idslplugin/Macro.scala
@@ -2,23 +2,23 @@ package spinal.idslplugin
 
 import scala.quoted._
 
-//trait LocationPlatform {
-//  implicit inline def instance: Location =
-//    ${LocationPlatform.Location_impl}
-//}
-//
-//object LocationPlatform {
-//  def Location_impl(using ctx: Quotes): Expr[Location] = {
-//    val rootPosition = ctx.reflect.Position.ofMacroExpansion
-//    val file = Expr(rootPosition.sourceFile.name.replace(".scala", ""))
-//    val line = Expr(rootPosition.startLine + 1)
-//    '{Location($file, $line)}
-//  }
-//}
-//
-//final case class Location(file: String, line: Int) {
-//  override def toString =
-//    s"$file:$line"
-//}
-//
-//object Location extends LocationPlatform
+trait LocationPlatform {
+  implicit inline def instance: Location =
+    ${LocationPlatform.Location_impl}
+}
+
+object LocationPlatform {
+  def Location_impl(using ctx: Quotes): Expr[Location] = {
+    val rootPosition = ctx.reflect.Position.ofMacroExpansion
+    val file = Expr(rootPosition.sourceFile.name.replace(".scala", ""))
+    val line = Expr(rootPosition.startLine + 1)
+    '{Location($file, $line)}
+  }
+}
+
+final case class Location(file: String, line: Int) {
+  override def toString =
+    s"$file:$line"
+}
+
+object Location extends LocationPlatform
