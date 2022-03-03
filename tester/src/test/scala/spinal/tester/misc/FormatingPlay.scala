79858908608f0b1116b5d175d71f039ef396ffa9
Dolu1990 <charles.papon.90@gmail.com>
2022-03-03 18:34:08 +0100

    Add FormatingPlay #539

---
+++ tester/src/test/scala/spinal/tester/misc/FormatingPlay.scala
@@ -0,0 +1,29 @@
+package spinal.tester.misc
+
+class F1(a     : Int,
+         b     : Int,
+         ccccc : Int,
+         d     : Int,
+         e     : Int) {
+
+}
+
+object F2{
+  def f1(a     : Int,
+         b     : Int,
+         ccccc : Int,
+         d     : Int,
+         e     : Int) {
+
+  }
+
+  f1(1,2,3,4,5)
+  f1(
+    a     = 1,
+    b     = 1,
+    ccccc = 1,
+    d     = 1,
+    e     = 1
+  )
+}
+
