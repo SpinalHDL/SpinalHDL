83e70a72d45d0687121a1b1d0b54575d884ce3c2
Jijing-mac14 <goco.v@163.com>
2022-03-17 13:21:08 +0800

    fix-back object type mismatch of Serializable on Java11

--- sim/src/main/scala/spinal/sim/Misc.scala
+++ sim/src/main/scala/spinal/sim/Misc.scala
@@ -15,31 +15,35 @@ object SimError{
 
 object WaveFormat{
   //Common waveformat options
-  case object VCD extends WaveFormat("vcd")
-  case object FST extends WaveFormat("fst")
-  case object DEFAULT extends WaveFormat
-  case object NONE extends WaveFormat
+  object VCD extends WaveFormat("vcd")
+  object FST extends WaveFormat("fst")
+  object DEFAULT extends WaveFormat
+  object NONE extends WaveFormat
  
   //GHDL only
-  case object VCDGZ extends WaveFormat("vcdgz")
-  case object GHW extends WaveFormat("ghw")
+  object VCDGZ extends WaveFormat("vcdgz")
+  object GHW extends WaveFormat("ghw")
 
   //Icarus Verilator only
-  case object FST_SPEED extends WaveFormat("fst-speed")
-  case object FST_SPACE extends WaveFormat("fst-space")
-  case object LXT extends WaveFormat("lxt")
-  case object LXT_SPEED extends WaveFormat("lxt-speed")
-  case object LXT_SPACE extends WaveFormat("lxt-space")
-  case object LXT2 extends WaveFormat("lxt2")
-  case object LXT2_SPEED extends WaveFormat("lxt2-speed")
-  case object LXT2_SPACE extends WaveFormat("lxt2-space")
+  object FST_SPEED extends WaveFormat("fst-speed")
+  object FST_SPACE extends WaveFormat("fst-space")
+  object LXT extends WaveFormat("lxt")
+  object LXT_SPEED extends WaveFormat("lxt-speed")
+  object LXT_SPACE extends WaveFormat("lxt-space")
+  object LXT2 extends WaveFormat("lxt2")
+  object LXT2_SPEED extends WaveFormat("lxt2-speed")
+  object LXT2_SPACE extends WaveFormat("lxt2-space")
 
   // VCS only
-  case object VPD extends WaveFormat("vpd")
-  case object FSDB extends WaveFormat("fsdb")
+  object VPD extends WaveFormat("vpd")
+  object FSDB extends WaveFormat("fsdb")
 }
 
-class WaveFormat(val ext : String = "???")
+sealed class WaveFormat(val ext : String = "???"){
+  override def toString: String = {
+    getClass().getName().split("\\$").last
+  }
+}
 
 
 trait Backend{
