7bb3c8474d0c3956f1b1803ae02e43b6619d00e6
Yindong <xydarcher@uestc.edu.cn>
2022-03-21 00:15:25 +0800

    add test for High bits first with padding.

--- tester/src/test/scala/spinal/tester/scalatest/SpinalSimStreamWidthAdapterTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/SpinalSimStreamWidthAdapterTester.scala
@@ -380,4 +380,97 @@ class SpinalSimStreamWidthAdapterTester extends SpinalSimFunSuite {
       simSuccess()
     }
   }
+
+  test("test2xOutHighFirstPadding") {
+    //Compile the simulator
+    val baseWidth = 32
+    val extraWidth = -2
+    val compiled = SimConfig.allOptimisation.compile(rtl = new Component{
+      val input = slave Stream(UInt(baseWidth bits))
+      val output = master Stream(UInt(baseWidth*2+extraWidth bits))
+      val rtl = StreamWidthAdapter(input, output, order = HIGHER_FIRST, padding=true)
+    }
+    )
+
+    //Run the simulation
+    compiled.doSimUntilVoid { dut =>
+      val queueModel = mutable.Queue[BigInt]()
+
+      SimTimeout(1000000 * 8)
+      dut.clockDomain.forkStimulus(2)
+
+      val driver = StreamDriver(dut.input, dut.clockDomain) { _ =>
+        true
+      }
+      val inMonitor = StreamMonitor(dut.input, dut.clockDomain) { p => 
+        queueModel.enqueue(p.toBigInt)
+      }
+
+      val ready = StreamReadyRandomizer(dut.output, dut.clockDomain)
+      val outMonitor = StreamMonitor(dut.output, dut.clockDomain) { p =>
+        assert(queueModel.nonEmpty)
+        val high = queueModel.dequeue()
+        assert(queueModel.nonEmpty)
+        val low = queueModel.dequeue()
+        val mask = ((BigInt(1) << dut.output.payload.getBitsWidth) - 1)
+        val value = ((high << baseWidth) + low) & mask
+        assert(p.toBigInt == value)
+      }
+      
+      dut.clockDomain.waitSampling(10000)
+      simSuccess()
+    }
+  }
+
+  test("test2xInHighFirstPadding") {
+    //Compile the simulator
+    val baseWidth = 32
+    val extraWidth = -2
+    val compiled = SimConfig.allOptimisation.compile(rtl = new Component{
+      val input = slave Stream(UInt(baseWidth*2+extraWidth bits))
+      val output = master Stream(UInt(baseWidth bits))
+      val rtl = StreamWidthAdapter(input, output, order = HIGHER_FIRST, padding=true)
+    }
+    )
+
+    //Run the simulation
+    compiled.doSimUntilVoid { dut =>
+      val inQueue = mutable.Queue[BigInt]()
+      val lowQueue = mutable.Queue[BigInt]()
+      val highQueue = mutable.Queue[BigInt]()
+
+      SimTimeout(1000000 * 8)
+      dut.clockDomain.forkStimulus(2)
+      def check(){
+        if(inQueue.nonEmpty && lowQueue.nonEmpty && highQueue.nonEmpty){
+          val low = lowQueue.dequeue()
+          val high = highQueue.dequeue()
+          val mask = ((BigInt(1) << dut.output.payload.getBitsWidth) - 1)
+          val value = ((high << baseWidth) + low) & mask
+          assert(inQueue.dequeue() == value)
+        }
+      }
+
+      val driver = StreamDriver(dut.input, dut.clockDomain) { p =>
+        true
+      }
+      val inMonitor = StreamMonitor(dut.input, dut.clockDomain) { p =>
+        inQueue.enqueue(p.toBigInt)
+        check()
+      }
+
+      val ready = StreamReadyRandomizer(dut.output, dut.clockDomain)
+      val outMonitor = StreamMonitor(dut.output, dut.clockDomain) { p =>
+        if (lowQueue.length > highQueue.length){
+          lowQueue.enqueue(p.toBigInt)
+        } else {
+          highQueue.enqueue(p.toBigInt)
+        }
+        check()
+      }
+      
+      dut.clockDomain.waitSampling(10000)
+      simSuccess()
+    }
+  }
 }
