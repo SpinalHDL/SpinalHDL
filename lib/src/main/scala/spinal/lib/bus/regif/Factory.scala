5be62bc182a40ac5dc6252597d0fd0ec3d5d7cee
Jijing-mac14 <goco.v@163.com>
2022-04-13 01:45:39 +0800

    old interruptFactory deperacated comment

--- lib/src/main/scala/spinal/lib/bus/regif/Factory.scala
+++ lib/src/main/scala/spinal/lib/bus/regif/Factory.scala
@@ -4,5 +4,6 @@ import language.experimental.macros
 import spinal.core.Bool
 
 object InterruptFactory{
+  @deprecated(message = "busif.interruptFactory() is recommended", since = "2022-12-31")
   def apply(busif: BusIf, regNamePre: String, triggers: Bool*): Bool = macro Macros.interruptFactoryImpl
 }
