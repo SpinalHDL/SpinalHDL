95ea3239834fd21d526947de5ce6e5485622f965
Dolu1990 <charles.papon.90@gmail.com>
2022-04-25 13:31:35 +0200

    SpinalPhaseVerifier ++

--- tester/src/test/scala/spinal/tester/scalatest/SpinalSimPhaseTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/SpinalSimPhaseTester.scala
@@ -65,11 +65,12 @@ class SpinalSimPhaseTester extends SpinalSimFunSuite{
 }
 
 class SpinalPhaseVerifier extends SpinalFormalFunSuite {
-  test("fifo_order") {    
-    // import spinal.core._
-    import spinal.core.formal._
-    import spinal.core.GenerationFlags._
-    import spinal.core.Formal._
+  import spinal.core.formal._
+  import spinal.core.GenerationFlags._
+  import spinal.core.Formal._
+
+  test("fifo_order") {
+
 
     FormalConfig
       .withProve(10)
@@ -108,6 +109,61 @@ class SpinalPhaseVerifier extends SpinalFormalFunSuite {
         when(d2_in) { assume(d1_in === True) }
         when(d2_out && d2_in) { assert(d1_out === True) }
       })
+  }
+
+  //Little thing which will provide a done pulse after "delay" cycles following a start pulse. Only manage a single request at the time
+
+
+  def startDoneTest(maxDelay : Int): Unit ={
+    class StartDoneDut() extends Component {
+      val start = in(Bool())
+      val done = out(False)
+      val delay = in(UInt(4 bits))
+      val counter = Reg(UInt(4 bits))
+      val busy = RegInit(False)
+      when(!busy) {
+        when(start){
+          busy := True
+          counter := 0
+        }
+      } otherwise {
+        counter := counter + 1
+        when(counter === delay){
+          busy := False
+          done := True
+        }
+      }
+    }
+
+    FormalConfig.withProve(25).doVerify(new Component {
+      val dut = new StartDoneDut()
+      assumeInitial(ClockDomain.current.isResetActive)
+
+      val busy = RegInit(False) setWhen (dut.start) clearWhen (dut.done)
+      anyseq(dut.start)
+      anyconst(dut.delay)
+
+      val timeout = Timeout(maxDelay)
+      when(!busy) {
+        when(dut.start) {
+          timeout.clear()
+        }
+      } otherwise {
+        assert(!timeout.state)
+      }
+    })
+  }
+
+  test("StartDoneDut pass") {
+    startDoneTest(16)
+  }
+  test("StartDoneDut fail") {
+    assert(try{
+      startDoneTest(15)
+      false
+    } catch{
+      case e => println(e); true
+    })
   }
 }
 
