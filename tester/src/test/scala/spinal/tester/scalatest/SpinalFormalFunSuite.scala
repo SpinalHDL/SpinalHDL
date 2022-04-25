88504a4383986175c05a3a58b63d71185f8f2e84
Yindong <xydarcher@uestc.edu.cn>
2022-04-25 17:56:40 +0800

    rename the paramter of assert and assume.

--- tester/src/test/scala/spinal/tester/scalatest/SpinalFormalFunSuite.scala
+++ tester/src/test/scala/spinal/tester/scalatest/SpinalFormalFunSuite.scala
@@ -5,12 +5,12 @@ import spinal.core._
 import spinal.core.formal._
 
 class SpinalFormalFunSuite extends AnyFunSuite{  
-  def assert(target: Bool) = {
-    spinal.core.assert(target)
+  def assert(assertion: Bool) = {
+    spinal.core.assert(assertion)
   }
 
-  def assume(target: Bool) = {
-    spinal.core.assume(target)
+  def assume(assertion: Bool) = {
+    spinal.core.assume(assertion)
   }
 
   def test(testName: String)(testFun: => Unit): Unit = {
