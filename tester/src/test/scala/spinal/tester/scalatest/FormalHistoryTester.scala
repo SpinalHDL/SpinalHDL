f545f33800ebdbae224ee1b11f44f25a69e7ed45
Yindong <xydarcher@uestc.edu.cn>
2022-05-17 15:54:29 +0800

    reformat the test code.

--- tester/src/test/scala/spinal/tester/scalatest/FormalHistoryTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/FormalHistoryTester.scala
@@ -8,34 +8,38 @@ class FormalHistoryModifyableTester extends SpinalFormalFunSuite {
   test("pop_any") {
     FormalConfig
       .withBMC(10)
-      //   .withProve(10)
+      .withProve(10)
       .withCover(10)
       .withDebug
       .doVerify(new Component {
+        val outOnly = false
         val depth = 4
         val input = anyseq(Flow(UInt(4 bits)))
         val dut = HistoryModifyable(input, depth)
-        val result = Vec(master(Stream(input.payloadType)), depth - 1)
-        val control = Vec(slave(Stream(input.payloadType)), depth - 1)
-        dut.io.outStreams.zip(result).map(x => x._1 >> x._2)
-        dut.io.inStreams.zip(control).map(x => x._2 >> x._1)
+        val results = Vec(master(Stream(input.payloadType)), depth - 1)
+        val controls = Vec(slave(Stream(input.payloadType)), depth - 1)
+        dut.io.outStreams.zip(results).map(x => x._1 >> x._2)
+        dut.io.inStreams.zip(controls).map(x => x._2 >> x._1)
 
         val reset = ClockDomain.current.isResetActive
         assumeInitial(reset)
 
-        dut.io.outStreams.map(x => when(input.valid) { assume(input.payload =/= x.payload) })
+        results.map(x => when(input.valid) { assume(input.payload =/= x.payload) })
 
-        val exists = CountOne(dut.io.outStreams.map(_.valid))
-        val outFired = CountOne(dut.io.outStreams.map(_.fire))
-        dut.io.outStreams.map(x => when(past(x.fire)){ assert(exists === past(exists - outFired + U(input.valid))) })
+        val exists = CountOne(results.map(_.valid))
+        val outFired = CountOne(results.map(_.fire))
+        results.map(x => when(past(x.fire)){ assert(exists === past(exists - outFired + U(input.valid))) })
 
-        // dut.io.inStreams.map(x => cover(x.fire))
-        control.map(x => assume(x.valid === False)) // no modify.
-        dut.io.outStreams.map(x => cover(x.fire))
-        dut.io.outStreams.map(x => when(x.valid) { assume(x.payload =/= input.payload) })
-        // dut.map(x => x.withAsserts())
-        // cover(dut.io.outStreams(1).fire && dut.io.inStreams(1).fire)
-        cover(dut.io.outStreams(0).fire && dut.io.outStreams(2).fire)
+        if (outOnly) {
+          controls.map(x => assume(x.valid === False))
+        } else {
+          controls.map(x => cover(x.fire))
+          cover(results(1).fire && controls(1).fire)
+        }
+
+        results.map(x => cover(x.fire))
+        results.map(x => when(x.valid) { assume(x.payload =/= input.payload) })
+        cover(results(0).fire && results(2).fire)
       })
   }
 }
