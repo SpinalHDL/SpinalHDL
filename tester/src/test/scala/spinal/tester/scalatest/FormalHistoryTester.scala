c8d7404b8694d12c5c44ccf847ee80becc080550
Yindong <xydarcher@uestc.edu.cn>
2022-05-17 18:10:14 +0800

    fire at the same would fail.

--- tester/src/test/scala/spinal/tester/scalatest/FormalHistoryTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/FormalHistoryTester.scala
@@ -14,7 +14,7 @@ class FormalHistoryModifyableTester extends SpinalFormalFunSuite {
       .doVerify(new Component {
         val outOnly = false
         val depth = 4
-        val input = anyseq(Flow(UInt(4 bits)))
+        val input = anyseq(Flow(UInt(6 bits)))
         val dut = HistoryModifyable(input, depth)
         val results = Vec(master(Stream(input.payloadType)), depth - 1)
         val controls = Vec(slave(Stream(input.payloadType)), depth - 1)
@@ -24,19 +24,31 @@ class FormalHistoryModifyableTester extends SpinalFormalFunSuite {
         val reset = ClockDomain.current.isResetActive
         assumeInitial(reset)
 
-        results.map(x => when(input.valid) { assume(input.payload =/= x.payload) })
+        when(input.valid) { results.map(x => assume(input.payload =/= x.payload)) }
 
         val exists = CountOne(results.map(_.valid))
         val outFired = CountOne(results.map(_.fire))
         results.map(x => when(past(x.fire)){ assert(exists === past(exists - outFired + U(input.valid))) })
 
+        val dataOut = anyconst(UInt(6 bits))
+        assume(dut.io.outStreams(0).payload =/= dut.io.outStreams(1).payload 
+          && dut.io.outStreams(1).payload =/= dut.io.outStreams(2).payload 
+          && dut.io.outStreams(0).payload =/= dut.io.outStreams(2).payload)
+
         if (outOnly) {
           controls.map(x => assume(x.valid === False))
-        } else {
+        } else {          
+          assume(dut.io.inStreams(0).payload =/= dut.io.inStreams(1).payload 
+            && dut.io.inStreams(1).payload =/= dut.io.inStreams(2).payload 
+            && dut.io.inStreams(0).payload =/= dut.io.inStreams(2).payload)
+          when(input.valid) { assume(controls.map(x => input.payload =/= x.payload).reduce(_ && _)) }          
+          controls.map(y => assume(!results.sExist(x => x.valid && x.payload === y.payload)))
+
           controls.map(x => cover(x.fire))
           cover(results(1).fire && controls(1).fire)
         }
 
+        when(past(results(1).payload === dataOut && results(1).fire)) {assert(!results.sExist(x => x.valid && x.payload === dataOut))}
         results.map(x => cover(x.fire))
         results.map(x => when(x.valid) { assume(x.payload =/= input.payload) })
         cover(results(0).fire && results(2).fire)
