f69dd106f3c8c6cece1be3b82aad7013a53ff05b
Xiao Yindong <xydarcher@uestc.edu.cn>
2022-05-22 23:59:36 +0800

    history check passed first.

--- tester/src/test/scala/spinal/tester/scalatest/FormalAxi4DownsizerTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/FormalAxi4DownsizerTester.scala
@@ -34,7 +34,7 @@ case class FormalAxi4Record(val config: Axi4Config, maxStbs: Int) extends Bundle
   def assignFromW(w: Stream[Axi4W], selectCount: UInt) {
       seenLast := w.last & w.ready
       strbs(selectCount.resized) := w.strb
-      when(w.ready) { count := selectCount + 1 }
+      when(w.ready) { count := selectCount + 1 }.otherwise{ count := selectCount }
   }
 
   def assignFromB(b: Stream[Axi4B]) {
@@ -43,7 +43,7 @@ case class FormalAxi4Record(val config: Axi4Config, maxStbs: Int) extends Bundle
 }
 
 class FormalAxi4DownsizerTester extends SpinalFormalFunSuite {
-  def outputAsserter(axi: Axi4WriteOnly, maxBursts: Int = 16, maxStbs: Int = 256) {
+  def outputAsserter(axi: Axi4WriteOnly, maxBursts: Int = 16, maxStbs: Int = 256) = new Area {
     val oRecord = FormalAxi4Record(axi.config, maxStbs)
     oRecord.assignFromBits(B(0, oRecord.getBitsWidth bits))
 
@@ -52,42 +52,72 @@ class FormalAxi4DownsizerTester extends SpinalFormalFunSuite {
     histInput.valid := False
     val hist = HistoryModifyable(histInput, maxBursts)
     hist.io.inStreams.map(_.valid := False)
+    hist.io.inStreams.map(_.payload := oRecord)
     hist.io.outStreams.map(_.ready := False)
 
-    val (awExist, awId) = hist.io.outStreams.sFindFirst(x => x.valid && !x.awDone)
-    val (wExist, wId) = hist.io.outStreams.sFindFirst(x => x.valid && !x.seenLast)
-    val (bExist, bId) = hist.io.outStreams.sFindFirst(x => x.valid /*&& axi.b.id === x.id*/ && !x.bResp)
+    val (awExist, awFoundId) = hist.io.outStreams.reverse.sFindFirst(x => x.valid && !x.awDone)
+    val (wExist, wFoundId) = hist.io.outStreams.reverse.sFindFirst(x => x.valid && !x.seenLast)
+    val (bExist, bFoundId) = hist.io.outStreams.reverse.sFindFirst(x => x.valid /*&& axi.b.id === x.id*/ && !x.bResp)
+    val awId = maxBursts - 1 - awFoundId
+    val wId = maxBursts - 1 - wFoundId
+    val bId = maxBursts - 1 - bFoundId
+
+    val dataErrors = Vec(Bool(), 5)
+    dataErrors.map(_ := False)
 
     val awRecord = CombInit(oRecord)
     val awValid = False
+    val awSelect = CombInit(oRecord)
     when(axi.aw.valid) {
       val ax = axi.aw.asInstanceOf[Stream[Axi4Ax]]
       when(awExist) { 
         awRecord := hist.io.outStreams(awId)
+        awRecord.allowOverride
         awRecord.assignFromAx(ax)
         awValid := True
+        awSelect := hist.io.outStreams(awId)
+
+        val realLen = axi.aw.len +^ 1
+        dataErrors(0) := awSelect.seenLast & realLen > awSelect.count
+        // assert(!awRecord.seenLast & realLen === awRecord.count)
+        // assert(realLen < awRecord.count)
       }
       .otherwise { histInput.assignFromAx(ax) }
     }
-    hist.io.inStreams(awId).valid := awValid
+    when(awValid) {
+      hist.io.inStreams(awId).payload := awRecord
+      hist.io.inStreams(awId).valid := awValid
+    }
 
     val wRecord = CombInit(oRecord)
     val wValid = False
+    val wSelect = CombInit(oRecord)
     when(axi.w.valid) {
       when(wExist) {
         wRecord := hist.io.outStreams(wId)
+        wSelect := hist.io.outStreams(wId)
         when(awValid && wId === awId) {
-          awRecord.assignFromW(axi.w, wRecord.count)
-        }.otherwise { wRecord.assignFromW(axi.w, wRecord.count); wValid := True }
+          awRecord.assignFromW(axi.w, wSelect.count)
+        }.otherwise { wRecord.assignFromW(axi.w, wSelect.count); wValid := True }
       }.otherwise { histInput.assignFromW(axi.w, U(0, 9 bits)) }
+      // assert(wRecord.awDone & (axi.w.last & (wRecord.count =/= wRecord.len)))
+      // assert(wRecord.awDone & (!axi.w.last & (wRecord.count === wRecord.len)))
     }
-    hist.io.inStreams(wId).valid := wValid
+    when(wValid) {      
+      hist.io.inStreams(wId).payload := wRecord
+      hist.io.inStreams(wId).valid := wValid
+    }
+
+    val respErrors = Vec(Bool(), 4)
+    respErrors.map(_ := False)
 
     val bRecord = CombInit(oRecord)
     val bValid = False
+    val bSelect = CombInit(oRecord)
     when(axi.b.valid) {
       when(bExist) {  
         bRecord := hist.io.outStreams(bId)
+        bSelect := hist.io.outStreams(bId)
         when(awValid && bId === awId) {
           awRecord.assignFromB(axi.b)
         }.elsewhen(wValid && bId === wId) {
@@ -95,11 +125,22 @@ class FormalAxi4DownsizerTester extends SpinalFormalFunSuite {
         }.otherwise { bRecord.assignFromB(axi.b); bValid := True }
 
         hist.io.outStreams(bId).ready := axi.b.ready & bRecord.awDone & bRecord.seenLast
-      } //.otherwise {histInput.assignFromB(axi.b)}
-    }
-    hist.io.inStreams(bId).valid := bValid
 
-    when(!(awExist || wExist)) {
+        respErrors(0) := !bSelect.awDone
+        respErrors(1) := bSelect.awDone & !bSelect.seenLast
+        // respErrors(2) := bSelect.awDone & bSelect.isLockExclusive & axi.b.resp === Axi4.resp.EXOKAY
+        // respErrors(3) := axi.b.ready & bSelect.awDone & bSelect.seenLast & checkStrb()
+      }.otherwise {
+        respErrors(0) := True
+      }
+      // when(axi.b.ready) { assert(bExist) }
+    }
+    when(bValid) {      
+      hist.io.inStreams(bId).payload := bRecord
+      hist.io.inStreams(bId).valid := bValid
+    }
+
+    when((axi.aw.valid & !awExist) | (axi.w.valid & !wExist)) {
       histInput.valid := True
     }
 
@@ -127,8 +168,10 @@ class FormalAxi4DownsizerTester extends SpinalFormalFunSuite {
         val inputAssumes = input.withAssumes(maxStall)
         val outputAsserts = output.withAsserts(maxStall)
 
-        outputAsserter(output, 4, 4)
-
+        val outChecker = outputAsserter(output, 4, 4)
+        outChecker.dataErrors.map(x => assert(!x))
+        outChecker.respErrors.map(x => assume(!x))
+        
         output.withCovers()
         input.withCovers()
       })
