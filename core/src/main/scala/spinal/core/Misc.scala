dcc517596415384a6a00118a592b85ed478c3588
Dolu1990 <charles.papon.90@gmail.com>
2022-05-23 16:22:05 +0200

    spinal.core.ContextSwapper.outsideCondScope added

--- core/src/main/scala/spinal/core/Misc.scala
+++ core/src/main/scala/spinal/core/Misc.scala
@@ -20,11 +20,10 @@
 \*                                                                           */
 package spinal.core
 
+import spinal.core.fiber.AsyncThread
 import spinal.core.internals._
 
-
 import java.lang.reflect.Field
-
 import scala.collection.mutable
 import scala.collection.mutable.Stack
 import scala.reflect.ClassTag
@@ -551,4 +550,19 @@ object LutInputs extends ScopeProperty[Int]{
 
 object ClassName{
   def apply(that : Any) =  that.getClass.getSimpleName.replace("$","")
+}
+
+object ContextSwapper{
+  def outsideCondScope[T](that : => T) : T = {
+    val t = AsyncThread.current
+    t.allowSuspend = false
+    val body = Component.current.dslBody  // Get the head of the current component symboles tree (AST in other words)
+    val ctx = body.push()                 // Now all access to the SpinalHDL API will be append to it (instead of the current context)
+    val swapContext = body.swap()         // Empty the symbole tree (but keep a reference to the old content)
+    val ret = that                        // Execute the block of code (will be added to the recently empty body)
+    ctx.restore()                         // Restore the original context in which this function was called
+    swapContext.appendBack()              // append the original symboles tree to the modified body
+    t.allowSuspend = true
+    ret                                   // return the value returned by that
+  }
 }
