013149bc4d15c644000c36191bfb28b34614cfd3
Côme ALLART <numero_744@net-c.com>
2022-06-05 14:24:58 +0200

    style: fmt SpinalSimLibTester w/ Codium

--- tester/src/test/scala/spinal/tester/scalatest/SpinalSimLibTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/SpinalSimLibTester.scala
@@ -8,38 +8,42 @@ import spinal.lib.sim.{FlowMonitor, StreamDriver, StreamMonitor, StreamReadyRand
 import scala.collection.mutable
 import scala.util.Random
 
-class SpinalSimLibTester extends AnyFunSuite{
-
-  for(bitCount <- 0 until 12) {
+class SpinalSimLibTester extends AnyFunSuite {
+  for (bitCount <- 0 until 12) {
     test("CountOnes" + bitCount) {
       LutInputs(Random.nextInt(5) + 2).on {
-        SimConfig.noOptimisation.compile(new Component {
-          val input = in Bits (bitCount bits)
-          val output = out(CountOne(input))
-        }).doSim(seed = 42) { dut =>
-          for (_ <- 0 until 100 + (1 << bitCount) * 4) {
-            dut.input.randomize()
-            sleep(1)
-            assert(dut.output.toInt === dut.input.toBigInt.bitCount)
+        SimConfig.noOptimisation
+          .compile(new Component {
+            val input = in Bits (bitCount bits)
+            val output = out(CountOne(input))
+          })
+          .doSim(seed = 42) { dut =>
+            for (_ <- 0 until 100 + (1 << bitCount) * 4) {
+              dut.input.randomize()
+              sleep(1)
+              assert(dut.output.toInt === dut.input.toBigInt.bitCount)
+            }
           }
-        }
       }
     }
   }
 
-  for(bitCount <- 0 until 12) {
-    test("CountOneOnEach"+bitCount) {
-      SimConfig.noOptimisation.compile(new Component {
-        val input = in Bits(bitCount bits)
-        val output = out Vec(CountOneOnEach(input))
-      }).doSim(seed = 42){dut =>
-        for(_ <- 0 until 100+(1 << bitCount)*4){
-          dut.input.randomize()
-          sleep(1)
-          val input = dut.input.toBigInt
-          for(i <- 0 until bitCount; mask = ((1 << i+1)-1)) assert(dut.output(i).toInt === (input & mask).bitCount)
+  for (bitCount <- 0 until 12) {
+    test("CountOneOnEach" + bitCount) {
+      SimConfig.noOptimisation
+        .compile(new Component {
+          val input = in Bits (bitCount bits)
+          val output = out Vec (CountOneOnEach(input))
+        })
+        .doSim(seed = 42) { dut =>
+          for (_ <- 0 until 100 + (1 << bitCount) * 4) {
+            dut.input.randomize()
+            sleep(1)
+            val input = dut.input.toBigInt
+            for (i <- 0 until bitCount; mask = ((1 << i + 1) - 1))
+              assert(dut.output(i).toInt === (input & mask).bitCount)
+          }
         }
-      }
     }
   }
 }
