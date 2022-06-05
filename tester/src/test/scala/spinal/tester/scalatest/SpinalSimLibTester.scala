8517c50efc5e7ba995b8aa11ff35d2c031eed580
Côme ALLART <numero_744@net-c.com>
2022-06-05 15:00:07 +0200

    tests: add tests for Vec bitwise ops

--- tester/src/test/scala/spinal/tester/scalatest/SpinalSimLibTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/SpinalSimLibTester.scala
@@ -9,6 +9,34 @@ import scala.collection.mutable
 import scala.util.Random
 
 class SpinalSimLibTester extends AnyFunSuite {
+  for (n <- 0 until 12) {
+    test("Vec (op) Vec on " + n + " elements") {
+      SimConfig.noOptimisation
+        .compile(new Component {
+          val a, b = in Vec (SInt(8 bits), n)
+          val xor = out(a ^ b)
+          val or = out(a | b)
+          val and = out(a & b)
+          val not = out(~a)
+        })
+        .doSim(seed = 42) { dut =>
+          for (_ <- 0 until 100) {
+            dut.a.randomize()
+            dut.b.randomize()
+            sleep(1)
+            for (i <- 0 until n) {
+              assert(dut.xor(i).toInt == (dut.a(i).toInt ^ dut.b(i).toInt))
+              assert(dut.or(i).toInt == (dut.a(i).toInt | dut.b(i).toInt))
+              assert(dut.and(i).toInt == (dut.a(i).toInt & dut.b(i).toInt))
+              assert(dut.not(i).toInt == ~dut.a(i).toInt)
+            }
+          }
+
+          def isEq(a: UInt, b: UInt): Boolean = a.toInt == b.toInt
+        }
+    }
+  }
+
   for (bitCount <- 0 until 12) {
     test("CountOnes" + bitCount) {
       LutInputs(Random.nextInt(5) + 2).on {
