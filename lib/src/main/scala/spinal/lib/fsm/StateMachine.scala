0e9ba4d711646efc0ba91511aa1b5bfc29171857
Côme ALLART <numero_744@net-c.com>
2022-06-06 21:52:39 +0200

    fix: remove unallowed procedure syntax error

--- lib/src/main/scala/spinal/lib/fsm/StateMachine.scala
+++ lib/src/main/scala/spinal/lib/fsm/StateMachine.scala
@@ -112,7 +112,7 @@ class StateMachine extends Area with StateMachineAccessor with ScalaLocated {
     *
     * goto() will only have an effect, if condition is True
     */
-  def setTransitionCondition(condition : Bool) {
+  def setTransitionCondition(condition : Bool): Unit = {
     this.transitionCond = Bool()
     this.transitionCond := condition
   }
