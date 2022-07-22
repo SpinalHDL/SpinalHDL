02be060f33828d5283bd0e320a82a9f1bccf9aaa
Xiao Yindong <xydarcher@uestc.edu.cn>
2022-07-22 21:26:26 +0800

    fit the new location of FormalFunSuite.

--- tester/src/test/scala/spinal/tester/scalatest/FormalSimpleTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/FormalSimpleTester.scala
@@ -2,6 +2,7 @@ package spinal.tester.scalatest
 
 import spinal.core._
 import spinal.lib._
+import spinal.lib.formal._
 
 class FormalSimpleTester extends SpinalFormalFunSuite {
   import spinal.core.formal._
