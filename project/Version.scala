4581c70655826a1256f819042b3ce5538cb48403
Dolu1990 <charles.papon.90@gmail.com>
2022-07-22 09:30:05 +0200

    SpinalHDL dev now has "dev" as version instead of a number

--- project/Version.scala
+++ project/Version.scala
@@ -2,15 +2,16 @@ object SpinalVersion {
   val compilers = List("2.11.12", "2.12.13", "2.13.6")
   val compilerIsRC = false
 
-  val isSnapshot = true
+  val isDev = true
+  val isSnapshot = false
   private def snapshot = if (isSnapshot) "-SNAPSHOT" else ""
   private val major = "1.7.2"
-  val all         = s"$major$snapshot"
-  val sim         = s"$major$snapshot"
-  val core        = s"$major$snapshot"
-  val lib         = s"$major$snapshot"
-  val ip          = s"$major$snapshot"
-  val debugger    = s"$major$snapshot"
-  val demo        = s"$major$snapshot"
-  val tester      = s"$major$snapshot"
+  val all         = if(isDev) "dev" else s"$major$snapshot"
+  val sim         = all
+  val core        = all
+  val lib         = all
+  val ip          = all
+  val debugger    = all
+  val demo        = all
+  val tester      = all
 }
