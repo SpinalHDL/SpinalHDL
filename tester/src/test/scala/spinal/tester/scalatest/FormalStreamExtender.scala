d4c0f19d8bd2506fd3e51bac379408d38e7bf1ac
Yindong <xydarcher@uestc.edu.cn>
2022-07-25 09:59:44 +0800

    fix missing import of funsuite.

--- tester/src/test/scala/spinal/tester/scalatest/FormalStreamExtender.scala
+++ tester/src/test/scala/spinal/tester/scalatest/FormalStreamExtender.scala
@@ -1,8 +1,9 @@
 package spinal.tester.scalatest
 
-import spinal.core.formal._
 import spinal.core._
+import spinal.core.formal._
 import spinal.lib._
+import spinal.lib.formal._
 
 class FormalStreamExtender extends SpinalFormalFunSuite {
   def counterTester(noDelay: Boolean = false) {
