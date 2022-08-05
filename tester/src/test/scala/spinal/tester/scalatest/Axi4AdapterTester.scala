4971f8682354c6ed24576cdeef2f0f92f5790453
Aidan Klein <aidan@shader.me>
2022-08-04 21:05:47 -0400

    Axi4: Add Axi4IdRemover and tests

--- tester/src/test/scala/spinal/tester/scalatest/Axi4AdapterTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/Axi4AdapterTester.scala
@@ -5,7 +5,7 @@ import spinal.core.sim._
 import org.scalatest.funsuite.AnyFunSuite
 import spinal.core.sim.SimCompiled
 import spinal.lib.bus.amba4.axi.sim.{Axi4ReadOnlyMasterAgent, Axi4ReadOnlyMonitor, Axi4ReadOnlySlaveAgent, Axi4WriteOnlyMasterAgent, Axi4WriteOnlyMonitor, Axi4WriteOnlySlaveAgent}
-import spinal.lib.bus.amba4.axi.{Axi4Config, Axi4ReadOnlyUpsizer, Axi4WriteOnlyUpsizer, Axi4ReadOnlyDownsizer, Axi4WriteOnlyDownsizer}
+import spinal.lib.bus.amba4.axi.{Axi4, Axi4Config, Axi4ReadOnly, Axi4ReadOnlyDownsizer, Axi4ReadOnlyIdRemover, Axi4ReadOnlyUpsizer, Axi4SharedIdRemover, Axi4WriteOnly, Axi4WriteOnlyDownsizer, Axi4WriteOnlyIdRemover, Axi4WriteOnlyUpsizer}
 import spinal.lib.bus.misc.SizeMapping
 import spinal.lib.{master, slave}
 
@@ -325,4 +325,234 @@ class Axi4DownsizerTester extends AnyFunSuite {
             .compile(Axi4ReadOnlyDownsizer(Axi4Config(20, 128, 4), Axi4Config(20, 32, 4)))
             .doSim("test", 42)((dut: Axi4ReadOnlyDownsizer) => readTester(dut, true))
     }
+}
+
+class Axi4IdRemoverTester extends AnyFunSuite {
+
+  def writeTester(dut : Axi4WriteOnlyIdRemover): Unit ={
+    dut.clockDomain.forkStimulus(10)
+
+
+    val regions = mutable.Set[SizeMapping]()
+    val inputAgent = new Axi4WriteOnlyMasterAgent(dut.io.input, dut.clockDomain) {
+      override def genAddress(): BigInt = ((Random.nextInt(1 << 19)))// & 0xFFF00) | 6
+
+      override def bursts: List[Int] = List(1)
+
+      override def mappingAllocate(mapping: SizeMapping): Boolean = {
+        if(regions.exists(_.overlap(mapping))) return false
+        regions += mapping
+        true
+      }
+
+      override def mappingFree(mapping: SizeMapping): Unit = regions.remove(mapping)
+    }
+    val outputAgent = new Axi4WriteOnlySlaveAgent(dut.io.output, dut.clockDomain)
+
+    val writes = mutable.HashMap[BigInt, Byte]()
+    val inputMonitor = new Axi4WriteOnlyMonitor(dut.io.input, dut.clockDomain) {
+      override def onWriteByte(address: BigInt, data: Byte): Unit = {
+        //          println(s"I $address -> $data")
+        writes(address) = data
+      }
+    }
+
+    val outputMonitor = new Axi4WriteOnlyMonitor(dut.io.output, dut.clockDomain) {
+      override def onWriteByte(address: BigInt, data: Byte): Unit = {
+        //          println(s"O $address -> $data")
+        assert(writes(address) == data)
+        writes.remove(address)
+      }
+    }
+
+
+    dut.clockDomain.waitSamplingWhere(inputAgent.rspCounter > 10000)
+    inputAgent.allowGen = false
+    dut.clockDomain.waitSamplingWhere(!inputAgent.pending)
+    dut.clockDomain.waitSampling(100)
+    assert(writes.isEmpty)
+    println("done")
+  }
+
+  test("writeOnly") {
+    SimConfig.withWave.compile(new Axi4WriteOnlyIdRemover(Axi4Config(20, 32, 4))).doSim("test", 42)(writeTester)
+  }
+
+  def readTester(dut : Axi4ReadOnlyIdRemover): Unit ={
+    dut.clockDomain.forkStimulus(10)
+
+    val regions = mutable.Set[SizeMapping]()
+    val inputAgent = new Axi4ReadOnlyMasterAgent(dut.io.input, dut.clockDomain) {
+      override def genAddress(): BigInt = Random.nextInt(1 << 19)
+      override def bursts: List[Int] = List(1)
+      override def mappingAllocate(mapping: SizeMapping): Boolean = {
+        if(regions.exists(_.overlap(mapping))) return false
+        regions += mapping
+        true
+      }
+
+      override def mappingFree(mapping: SizeMapping): Unit = regions.remove(mapping)
+    }
+
+    val outputAgent = new Axi4ReadOnlySlaveAgent(dut.io.output, dut.clockDomain)
+
+
+
+    val reads = mutable.HashMap[BigInt, Byte]()
+    val inputMonitor = new Axi4ReadOnlyMonitor(dut.io.input, dut.clockDomain) {
+      override def onReadByte(address: BigInt, data: Byte, id : Int): Unit = {
+        reads(address) = data
+      }
+      override def onLast(id: Int): Unit = {}
+    }
+
+    val outputMonitor = new Axi4ReadOnlyMonitor(dut.io.output, dut.clockDomain) {
+      override def onReadByte(address: BigInt, data: Byte, id : Int): Unit = {
+        if(reads.contains(address)) {
+          assert(reads(address) == data)
+          reads.remove(address)
+        }
+      }
+
+      override def onLast(id: Int): Unit = assert(reads.isEmpty)
+    }
+
+
+
+    dut.clockDomain.waitSamplingWhere(inputAgent.rspCounter > 10000)
+    inputAgent.allowGen = false
+    dut.clockDomain.waitSamplingWhere(!inputAgent.pending)
+    dut.clockDomain.waitSampling(100)
+    assert(reads.isEmpty)
+    println("done")
+  }
+
+  test("readOnly") {
+    SimConfig.withWave.compile(new Axi4ReadOnlyIdRemover(Axi4Config(20, 32, 4))).doSim("test", 42)(readTester)
+  }
+
+  def sharedTester(dut: Axi4SharedIdRemoverDut): Unit = {
+    dut.clockDomain.forkStimulus(10)
+
+    val readFork = fork {
+      val regions = mutable.Set[SizeMapping]()
+      val inputAgent = new Axi4ReadOnlyMasterAgent(dut.io.inputs.read, dut.clockDomain) {
+        override def genAddress(): BigInt = Random.nextInt(1 << 19)
+
+        override def bursts: List[Int] = List(1)
+
+        override def mappingAllocate(mapping: SizeMapping): Boolean = {
+          if (regions.exists(_.overlap(mapping))) return false
+          regions += mapping
+          true
+        }
+
+        override def mappingFree(mapping: SizeMapping): Unit = regions.remove(mapping)
+      }
+      val outputAgent = new Axi4ReadOnlySlaveAgent(dut.io.outputs.read, dut.clockDomain)
+
+
+      val reads = mutable.HashMap[BigInt, Byte]()
+      val inputMonitor = new Axi4ReadOnlyMonitor(dut.io.inputs.read, dut.clockDomain) {
+        override def onReadByte(address: BigInt, data: Byte, id: Int): Unit = {
+          reads(address) = data
+        }
+
+        override def onLast(id: Int): Unit = {}
+      }
+
+      val outputMonitor = new Axi4ReadOnlyMonitor(dut.io.outputs.read, dut.clockDomain) {
+        override def onReadByte(address: BigInt, data: Byte, id: Int): Unit = {
+          if (reads.contains(address)) {
+            assert(reads(address) == data)
+            reads.remove(address)
+          }
+        }
+
+        override def onLast(id: Int): Unit = assert(reads.isEmpty)
+      }
+
+      dut.clockDomain.waitSamplingWhere(inputAgent.rspCounter > 10000)
+      inputAgent.allowGen = false
+      dut.clockDomain.waitSamplingWhere(!inputAgent.pending)
+      dut.clockDomain.waitSampling(100)
+      assert(reads.isEmpty)
+    }
+
+    val writeFork = fork {
+      val regions = mutable.Set[SizeMapping]()
+      val inputAgent = new Axi4WriteOnlyMasterAgent(dut.io.inputs.write, dut.clockDomain) {
+        override def genAddress(): BigInt = ((Random.nextInt(1 << 19))) // & 0xFFF00) | 6
+
+        override def bursts: List[Int] = List(1)
+
+        override def mappingAllocate(mapping: SizeMapping): Boolean = {
+          if (regions.exists(_.overlap(mapping))) return false
+          regions += mapping
+          true
+        }
+
+        override def mappingFree(mapping: SizeMapping): Unit = regions.remove(mapping)
+      }
+      val outputAgent = new Axi4WriteOnlySlaveAgent(dut.io.outputs.write, dut.clockDomain)
+
+      val writes = mutable.HashMap[BigInt, Byte]()
+      val inputMonitor = new Axi4WriteOnlyMonitor(dut.io.inputs.write, dut.clockDomain) {
+        override def onWriteByte(address: BigInt, data: Byte): Unit = {
+          //          println(s"I $address -> $data")
+          writes(address) = data
+        }
+      }
+
+      val outputMonitor = new Axi4WriteOnlyMonitor(dut.io.outputs.write, dut.clockDomain) {
+        override def onWriteByte(address: BigInt, data: Byte): Unit = {
+          //          println(s"O $address -> $data")
+          assert(writes(address) == data)
+          writes.remove(address)
+        }
+      }
+
+      dut.clockDomain.waitSamplingWhere(inputAgent.rspCounter > 10000)
+      inputAgent.allowGen = false
+      dut.clockDomain.waitSamplingWhere(!inputAgent.pending)
+      dut.clockDomain.waitSampling(100)
+      assert(writes.isEmpty)
+    }
+
+    readFork.join()
+    writeFork.join()
+
+    println("done")
+  }
+
+  class Axi4SharedIdRemoverDut(config: Axi4Config) extends Component {
+
+    val idRemover = new Axi4SharedIdRemover(config)
+
+    val io = new Bundle {
+      val inputs = new Bundle {
+        val read = slave(Axi4ReadOnly(config))
+        val write = slave(Axi4WriteOnly(config))
+      }
+      val outputs = new Bundle {
+        val read = master(Axi4ReadOnly(idRemover.io.output.config))
+        val write = master(Axi4WriteOnly(idRemover.io.output.config))
+      }
+    }
+
+    val input = Axi4(config)
+    input << io.inputs.read
+    input << io.inputs.write
+
+    idRemover.io.input << input.toShared()
+
+    val output = idRemover.io.output.toAxi4()
+
+    io.outputs.read << output
+    io.outputs.write << output
+  }
+
+  test("shared") {
+    SimConfig.withWave.compile(new Axi4SharedIdRemoverDut(Axi4Config(20, 32, 4))).doSim("test", 42)(sharedTester)
+  }
 }
