ddbb163184cb49dbaa6e4d9b9bd0b9e101a8e968
Dolu1990 <charles.papon.90@gmail.com>
2022-08-11 18:51:38 +0200

    scala3 bundle and area are now selectable

--- play/src/main/scala/spinal/core/Bundle.scala
+++ play/src/main/scala/spinal/core/Bundle.scala
@@ -3,10 +3,10 @@ package spinal.core
 import spinal.idslplugin.{PostInitCallback, ValCallback}
 import scala.reflect.Selectable
 class Namespace extends Selectable
-abstract class Bundle{
+abstract class Bundle extends Selectable {
 
 }
-class Area extends ValCallback{
+class Area extends ValCallback with Selectable {
   override def valCallback[T](ref: T, name: String) = {
     println(s"Area got val $name = $ref")
     ref
