d536777818cf34aabe1cd8aafe561e5597b233dd
Duang <1273208262@qq.com>
2022-09-02 05:26:59 +0800

    Formal async_fork first version
    BMC and Cover PASS,
    but prove has error

--- tester/src/test/scala/spinal/tester/scalatest/FormalForkTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/FormalForkTester.scala
@@ -5,46 +5,92 @@ import spinal.core.formal._
 import spinal.lib.{StreamFork, History}
 import spinal.lib.formal._
 
+
 class FormalForkTester extends SpinalFormalFunSuite {
-  test("fork-verify all") {
+  test("fork-verify sync") {    
     FormalConfig
-      .withBMC(20)
-      .withProve(20)
-      .withCover(20)
+      //.withBMC(20)
+      //.withProve(20)
+      //.withCover(20)
       // .withDebug
       .doVerify(new Component {
         val dut = FormalDut(new StreamFork(UInt(8 bits), 2, true))
         val reset = ClockDomain.current.isResetActive
 
         assumeInitial(reset)
+        val input = slave(Stream(UInt(8 bits)))
+        val output_0 = master(Stream(UInt(8 bits)))
+        val output_1 = master(Stream(UInt(8 bits)))
 
-        val inValue = anyseq(UInt(8 bits))
-        val inValid = anyseq(Bool())
-        val outReady_0 = anyseq(Bool())
-        val outReady_1 = anyseq(Bool())
-
-        dut.io.input.payload := inValue
-        dut.io.input.valid := inValid
-
-        dut.io.outputs(0).ready := outReady_0
-        dut.io.outputs(1).ready := outReady_1
-
+        input >> dut.io.input
+        output_0 << dut.io.outputs(0)
+        output_1 << dut.io.outputs(1)
 
         when(reset || past(reset)) {
-          assume(inValid === False)
+          assume(input.valid === False)
+        }     
+
+        assert(output_0.payload === input.payload)
+        assert(output_0.fire === input.fire)
+        assert(output_1.payload === input.payload)
+        assert(output_1.fire === input.fire)
+        
+        dut.io.outputs(0).withAsserts()
+        dut.io.outputs(1).withAsserts()  
+
+      })
+  }
+  test("fork-verify async") {    
+    FormalConfig
+      //.withBMC(100)
+      .withProve(20)
+      .withCover(100)
+      // .withDebug
+      .doVerify(new Component {
+        val dut = FormalDut(new StreamFork(UInt(8 bits), 2, false))
+        val reset = ClockDomain.current.isResetActive
+
+        assumeInitial(reset)
+        val input = slave(Stream(UInt(8 bits)))
+        val output_0 = master(Stream(UInt(8 bits)))
+        val output_1 = master(Stream(UInt(8 bits)))
+        val input_fire = input.fire
+
+        input >> dut.io.input
+        output_0 << dut.io.outputs(0)
+        output_1 << dut.io.outputs(1)
+        
+        when(reset || past(reset)) {
+          assume(input.valid === False)
         }
+        val fetched = Vec(RegInit(True),2)  
+      
 
-        assert(dut.io.input.ready === (outReady_0 && outReady_1))
+        when (input.ready) {
+          fetched(0) := False
+          fetched(1) := False
+        }
+        when ((!input.ready) && output_0.fire) {
+          fetched(0) := True
+          assert (output_0.payload === input.payload)
+        }
+        when ((!input.ready) && output_1.fire) {
+          fetched(1) := True
+          assert (output_1.payload === input.payload)
+        }
         
-        when((!outReady_0 || !outReady_1) && dut.io.outputs(0).valid) {          
-          assert(dut.io.outputs(0).payload === past(dut.io.outputs(0).payload))
-          assert(dut.io.outputs(0).valid === past(dut.io.outputs(0).valid))
-          }
-        
-        assert(dut.io.outputs(0).payload === dut.io.outputs(1).payload)
-        assert(dut.io.outputs(0).valid === dut.io.outputs(1).valid)        
+        cover(past(fetched(0)) && fetched(0) && past(!fetched(1)) && (!fetched(1)))
+        cover(past(fetched(1)) && fetched(1) && past(!fetched(0)) && (!fetched(0)))
 
-        assert(dut.io.outputs(0).valid === (dut.io.input.valid && dut.io.input.ready))
+        when(past(fetched(0)) && fetched(0) && past(!fetched(1)) && (!fetched(1))) {
+          assert(output_0.fire === False)
+          assert((input_fire && output_1.fire) || (!(input_fire || output_1.fire)))
+        }
+        when(past(fetched(1)) && fetched(1) && past(!fetched(0)) && (!fetched(0))) {
+          assert(output_1.fire === False)
+          assert((input_fire && output_0.fire) || (!(input_fire || output_0.fire)))
+        }        
+
       })
   }
 }
