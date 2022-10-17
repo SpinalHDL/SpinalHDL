4c368452394c60404b95c53ceb9276fcb4fb60bd
Marek Pikuła <marek@pikula.co>
2022-10-17 14:17:32 +0200

    Add SystemRdlGenerator to RegIf tester

--- tester/src/main/scala/spinal/tester/code/RegIfExample.scala
+++ tester/src/main/scala/spinal/tester/code/RegIfExample.scala
@@ -221,5 +221,6 @@ object getRegIfExample {
     example.toplevel.busif.accept(CHeaderGenerator("header", "AP"))
     example.toplevel.busif.accept(HtmlGenerator("regif", "AP"))
     example.toplevel.busif.accept(JsonGenerator("regif"))
+    example.toplevel.busif.accept(SystemRdlGenerator("regif", "AP", Some("addrmap name"), Some("addrmap desc")))
   }
 }
