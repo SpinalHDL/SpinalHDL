f840bbc780cd6b0337f62d09033aeb2e21008ebc
Aidan Klein <aidan@shader.me>
2022-10-18 20:54:38 -0400

    Sim: PackedBundle: Add unpacked test. Finalize

--- tester/src/test/scala/spinal/tester/scalatest/SpinalSimPackedBundleTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/SpinalSimPackedBundleTester.scala
@@ -12,10 +12,10 @@ class SpinalSimPackedBundleTester extends AnyFunSuite {
         val a = Bits(3 bit) // 0 to 2
         val b = Bits(3 bit).packFrom(4) // 4 to 6
         val c = Bits(3 bit).packTo(9) // 7 to 9
-        val d = Bits(3 bit).pack(10 to 14, LITTLE) // 00 00 12 13 14
-        val e = Bits(3 bit).pack(15 to 19, BIG) // 15 16 17 00 00
-        val f = Bits(6 bit).pack(20 to 24, LITTLE) // (19 drop) 20 21 22 23 24
-        val g = Bits(6 bit).pack(25 to 29, BIG) // 25 26 27 28 29 (30 drop)
+        val d = Bits(3 bit).pack(10 to 14, LITTLE) // 10 11 12 00 00
+        val e = Bits(3 bit).pack(15 to 19, BIG) // 00 00 17 18 19
+        val f = Bits(6 bit).pack(20 to 24, LITTLE) // 19 20 21 22 23 (24 drop)
+        val g = Bits(6 bit).pack(25 to 29, BIG) // (25 drop) 26 27 28 29 30
       }
       val io = new Bundle {
         val a = in(Bits(3 bit))
@@ -44,8 +44,8 @@ class SpinalSimPackedBundleTester extends AnyFunSuite {
         res += a
         res += b << 4
         res += c << 7
-        res += d << 12
-        res += e << 15
+        res += d << 10
+        res += e << 17
         res += (f & 0x1f) << 20
         res += (g >> 1) << 25
         res
@@ -67,6 +67,72 @@ class SpinalSimPackedBundleTester extends AnyFunSuite {
           dut.io.e.toBigInt,
           dut.io.f.toBigInt,
           dut.io.g.toBigInt)
+        assert(dut.io.packed.toBigInt == packedCalc, s"0x${dut.io.packed.toBigInt.toString(16)} =!= 0x${packedCalc.toString(16)}\n")
+      }
+    })
+  }
+
+  test("unpack") {
+    SimConfig.withWave.compile(new Component {
+      val packedBundle = new PackedBundle {
+        val a = Bits(3 bit) // 0 to 2
+        val b = Bits(3 bit).packFrom(4) // 4 to 6
+        val c = Bits(3 bit).packTo(9) // 7 to 9
+        val d = Bits(3 bit).pack(10 to 14, LITTLE) // 10 11 12 00 00
+        val e = Bits(3 bit).pack(15 to 19, BIG) // 00 00 17 18 19
+        val f = Bits(6 bit).pack(20 to 24, LITTLE) // 19 20 21 22 23 (24 drop)
+        val g = Bits(6 bit).pack(25 to 29, BIG) // (25 drop) 26 27 28 29 30
+      }
+      val io = new Bundle {
+        val a = out(Bits(3 bit))
+        val b = out(Bits(3 bit))
+        val c = out(Bits(3 bit))
+        val d = out(Bits(3 bit))
+        val e = out(Bits(3 bit))
+        val f = out(Bits(6 bit))
+        val g = out(Bits(6 bit))
+        val packed = in(packedBundle.asBits.clone)
+      }
+
+      packedBundle.assignFromBits(RegNext(io.packed))
+      io.a := packedBundle.a
+      io.b := packedBundle.b
+      io.c := packedBundle.c
+      io.d := packedBundle.d
+      io.e := packedBundle.e
+      io.f := packedBundle.f
+      io.g := packedBundle.g
+    }).doSim(dut => {
+      dut.clockDomain.forkStimulus(10)
+
+      def pack(a: BigInt, b: BigInt, c: BigInt, d: BigInt, e: BigInt, f: BigInt, g: BigInt): BigInt = {
+        var res = BigInt(0)
+        res += a
+        res += b << 4
+        res += c << 7
+        res += d << 10
+        res += e << 17
+        res += (f & 0x1f) << 20
+        res += (g >> 1) << 25
+        res
+      }
+
+      for (_ <- 0 to 100) {
+        dut.io.packed.randomize()
+        dut.clockDomain.waitFallingEdge()
+        var packedCalc = pack(dut.io.a.toBigInt,
+          dut.io.b.toBigInt,
+          dut.io.c.toBigInt,
+          dut.io.d.toBigInt,
+          dut.io.e.toBigInt,
+          dut.io.f.toBigInt,
+          dut.io.g.toBigInt)
+        // Add the ignored bits back in
+        packedCalc |= (dut.io.packed.toBigInt & 1 << 3)
+        packedCalc |= (dut.io.packed.toBigInt & 1 << 13)
+        packedCalc |= (dut.io.packed.toBigInt & 1 << 14)
+        packedCalc |= (dut.io.packed.toBigInt & 1 << 15)
+        packedCalc |= (dut.io.packed.toBigInt & 1 << 16)
         assert(dut.io.packed.toBigInt == packedCalc, s"0x${dut.io.packed.toBigInt.toString(16)} =!= 0x${packedCalc.toString(16)}\n")
       }
     })
