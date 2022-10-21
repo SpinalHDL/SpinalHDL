f06b7ea3b9abd4779c8cf5dd4c3e922a7ef2779b
Côme ALLART <numero_744@net-c.com>
2022-10-21 21:39:02 +0200

    partially implement AhbLite3BusInterface
    Need more information about how it is used / documentation
    of parent class, to fill in the blanks.

--- lib/src/main/scala/spinal/lib/bus/regif/BusIfAdapter/AhbLite3BusInterface.scala
+++ lib/src/main/scala/spinal/lib/bus/regif/BusIfAdapter/AhbLite3BusInterface.scala
@@ -4,35 +4,66 @@ import spinal.core._
 import spinal.lib.bus.amba3.ahblite.AhbLite3
 import spinal.lib.bus.misc.SizeMapping
 
+/* Written to be compliant with:
+ * http://eecs.umich.edu/courses/eecs373/readings/ARM_IHI0033A_AMBA_AHB-Lite_SPEC.pdf
+ */
 case class AhbLite3BusInterface(bus: AhbLite3, sizeMap: SizeMapping, regPre: String = "")(implicit
     moduleName: ClassName
 ) extends BusIf {
+  import bus._
 
-  override def getModuleName = moduleName.name
+  HREADYOUT := True
+  HRESP := False
 
-  val readError = Bool()
-  val readData = Bits(bus.config.dataWidth bits)
+  /** Delays addr phase signal to get data phase signal.
+    *
+    * See Section 3.1 Basic transfers
+    */
+  private def dataPhase[T <: Data](what: T, init: T): T =
+    RegNextWhen(what, HREADY) init init
 
-  val writeData: Bits = bus.HWDATA
+  val askWrite: Bool = HSEL && HTRANS(1) && HWRITE
 
-  readError.setAsReg() init False
-  readData.setAsReg() init 0
+  val askRead: Bool = HSEL && HTRANS(1) && !HWRITE
 
-  val askWrite = bus.HSEL & bus.HTRANS(1) === True & bus.HWRITE
-  val askRead = bus.HSEL & bus.HTRANS(1) === True & !bus.HWRITE
-  val doWrite = bus.HREADY & RegNext(askWrite, False)
-  val doRead = bus.HREADY & askRead
+  val doWrite: Bool = dataPhase(askWrite, False)
 
-  val addressDelay = RegNextWhen(bus.HADDR, askRead | askWrite)
-  bus.HREADYOUT := True
-  bus.HRESP := False
-  bus.HRDATA := readData
+  val doRead: Bool = dataPhase(askRead, False)
 
-  def readAddress(): UInt = bus.HADDR
-  def writeAddress(): UInt = addressDelay
+  val readData: Bits = HRDATA
 
-  def readHalt(): Unit = bus.HREADY === False
-  def writeHalt(): Unit = bus.HREADY === False
+  val writeData: Bits = dataPhase(HWDATA, 0)
+
+  val readError: Bool = Bool
+
+  // Error management described in section 5.1
+  val error = new Area {
+    val cycle1, cycle2 = Bool
+
+    cycle1 := readError
+    cycle2 := RegNext(cycle1) init False
+
+    when(cycle1) {
+      HREADYOUT := False
+      HRESP := True
+    }
+    when(cycle2) {
+      HREADYOUT := True
+      HRESP := True
+    }
+  }
+
+  // TODO: How is it used? when askXxx or doXxx?
+  def readAddress(): UInt = ???
+
+  // TODO: How is it used? when askXxx or doXxx?
+  def writeAddress(): UInt = ???
+
+  def readHalt(): Unit = HREADYOUT := False
+
+  def writeHalt(): Unit = HREADYOUT := False
 
   def busDataWidth: Int = bus.config.dataWidth
+
+  def getModuleName: String = moduleName.name
 }
