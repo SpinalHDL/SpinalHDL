9334e785a80d695f3d6c2d09e65fadd2209a1cf2
Côme ALLART <numero_744@net-c.com>
2022-12-11 15:00:46 +0100

    refactor: move SpinalSimWishboneSimTester

--- lib/src/test/scala/spinal/lib/bus/wishbone/Wishbone.scala
+++ lib/src/test/scala/spinal/lib/bus/wishbone/Wishbone.scala
@@ -1,11 +1,10 @@
-package spinal.tester.scalatest
+package spinal.lib.bus.wishbone
 
 import org.scalatest.funsuite.AnyFunSuite
+
 import spinal.core._
-import spinal.core.sim.{SimCompiled, _}
-import spinal.sim._
+import spinal.core.sim._
 import spinal.lib._
-import spinal.lib.bus.wishbone._
 import spinal.lib.wishbone.sim._
 import spinal.lib.sim._
 
