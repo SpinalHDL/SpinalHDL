85beb51013e9939ed9e6460427763dfe404ada0b
Alexis Marquet <alexis@alexismarquet.com>
2023-04-22 16:10:57 +0200

    jtag-vpi: fix implementation and fmt remarks

--- lib/src/main/scala/spinal/lib/com/jtag/sim/JtagVpi.scala
+++ lib/src/main/scala/spinal/lib/com/jtag/sim/JtagVpi.scala
@@ -1,19 +1,21 @@
 package solsoc
 
+import spinal.core.TimeNumber
 import spinal.core.sim._
 import spinal.lib.com.jtag.Jtag
 
 import java.nio.{ByteBuffer, ByteOrder}
 import spinal.lib.com.jtag.sim.JtagDriver
+import spinal.sim.SimThread
 
 import java.io.{DataInputStream, DataOutputStream}
 import java.net.ServerSocket
 
 /*
-    * This is a JTAG VPI interface for openocd
-    * It is used to connect openocd to a JTAG interface over TCP
-    * the reference interface implementation is in https://github.com/openocd-org/openocd/blob/master/src/jtag/drivers/jtag_vpi.c
-    * Example openocd config:
+ * This is a JTAG VPI interface for openocd
+ * It is used to connect openocd to a JTAG interface over TCP
+ * the reference interface implementation is in https://github.com/openocd-org/openocd/blob/master/src/jtag/drivers/jtag_vpi.c
+ * Example openocd config:
 
 source [find interface/jtag_vpi.cfg]
 debug_level 1
@@ -25,7 +27,7 @@ init
  */
 
 object JtagVpi {
-  def apply(jtag: Jtag, port: Int = 5555, jtagClkPeriod: Long) = fork {
+  def apply(jtag: Jtag, port: Int = 5555, jtagClkPeriod: TimeNumber): SimThread = fork {
     val driver = JtagDriver(jtag, jtagClkPeriod)
     var inputStream: DataInputStream = null
     var outputStream: DataOutputStream = null
@@ -35,14 +37,14 @@ object JtagVpi {
       override def run(): Unit = {
         println("VPI Server started, waiting for connection...")
         while (true) {
-          val connection = try {
-            socket.accept()
-          } catch {
-            case e: Exception => return
-          }
-          //connection.setTcpNoDelay(true)
-          outputStream = new DataOutputStream(connection.getOutputStream())
-          inputStream = new DataInputStream(connection.getInputStream())
+          val connection =
+            try {
+              socket.accept()
+            } catch {
+              case _: Exception => return
+            }
+          outputStream = new DataOutputStream(connection.getOutputStream)
+          inputStream = new DataInputStream(connection.getInputStream)
           println("VPI Client connected")
         }
       }
@@ -54,30 +56,25 @@ object JtagVpi {
     // read the first message from the client and print it
     val buffer = new Array[Byte](MaxSizeOfVpiCmd)
     while (true) {
-      if(inputStream == null)
-      {
-        sleep(jtagClkPeriod)
-      }
-      else {
+      if (inputStream == null) {
+        sleep(timeToLong(jtagClkPeriod))
+      } else {
         inputStream.readFully(buffer)
 
         val vpiCmd = deserializeVpiCmd(buffer)
 
         vpiCmd match {
-          case VpiCmd(Cmds.RESET, _, _, _, _) => {
-            driver.doResetTap()
-          }
-          case VpiCmd(Cmds.TMS_SEQ, bufferOut, _, _, nbBits) => {
-            val tmsSeq = bufferOut.map(x => (0 until 8).map(i => (x & (1 << i)) != 0)).flatten.take(nbBits)
+          case VpiCmd(Cmds.RESET, _, _, _, _) => driver.doResetTap()
+          case VpiCmd(Cmds.TMS_SEQ, bufferOut, _, _, nbBits) =>
+            val tmsSeq = bufferOut.flatMap(x => (0 until 8).map(i => (x & (1 << i)) != 0)).take(nbBits)
             driver.doTmsSeq(tmsSeq)
-          }
-          case VpiCmd(Cmds.SCAN_CHAIN, bufferOut, bufferIn, length, nbBits) => {
+          case VpiCmd(Cmds.SCAN_CHAIN, bufferOut, bufferIn, length, nbBits) =>
             // bufferOut is a byte array, we need to convert it to a boolean array
             // bufferIn is a byte array, we need to convert it to a boolean array
             // each byte of bufferOut is a sequence of 8 bits (which is why nbBits exists)
             // convert bufferOut to a boolean array
-            val tdiSeq = bufferOut.map(x => (0 until 8).map(i => (x & (1 << i)) != 0)).flatten.take(nbBits)
-            val tdoSeq = driver.doScanChain(tdiSeq, false)
+            val tdiSeq = bufferOut.flatMap(x => (0 until 8).map(i => (x & (1 << i)) != 0)).take(nbBits)
+            val tdoSeq = driver.doScanChain(tdiSeq, flipTms = false)
             // convert tdoSeq to a byte array grouped by 8 bits
             val tdoSeqBytes = tdoSeq.grouped(8).map(_.foldLeft(0)((acc, b) => (acc << 1) | (if (b) 1 else 0))).toArray
             // copy the result to bufferIn
@@ -88,11 +85,10 @@ object JtagVpi {
             val vpiCmd = VpiCmd(Cmds.SCAN_CHAIN, bufferOut, bufferIn, length, nbBits)
             serialize(buffer, vpiCmd)
             outputStream.write(buffer)
-          }
-          case VpiCmd(Cmds.SCAN_CHAIN_FLIP_TMS, bufferOut, bufferIn, length, nbBits) => {
+          case VpiCmd(Cmds.SCAN_CHAIN_FLIP_TMS, bufferOut, bufferIn, length, nbBits) =>
             // same as SCAN_CHAIN but with the last TMS set to 1
-            val tdiSeq = bufferOut.map(x => (0 until 8).map(i => (x & (1 << i)) != 0)).flatten.take(nbBits)
-            val tdoSeq = driver.doScanChain(tdiSeq, true)
+            val tdiSeq = bufferOut.flatMap(x => (0 until 8).map(i => (x & (1 << i)) != 0)).take(nbBits)
+            val tdoSeq = driver.doScanChain(tdiSeq, flipTms = true)
             val tdoSeqBytes =
               tdoSeq.grouped(8).map(_.reverse.foldLeft(0)((acc, b) => (acc << 1) | (if (b) 1 else 0))).toArray
             for (i <- 0 until length) {
@@ -101,17 +97,15 @@ object JtagVpi {
             val vpiCmd = VpiCmd(Cmds.SCAN_CHAIN_FLIP_TMS, bufferOut, bufferIn, length, nbBits)
             serialize(buffer, vpiCmd)
             outputStream.write(buffer)
-          }
-          case VpiCmd(Cmds.STOP_SIMU, _, _, _, _) => {
+          case VpiCmd(Cmds.STOP_SIMU, _, _, _, _) =>
             println("Stop simulation")
             simSuccess()
-          }
         }
       }
     }
   }
 
-  def deserializeVpiCmd(buffer: Array[Byte]): VpiCmd = {
+  private def deserializeVpiCmd(buffer: Array[Byte]): VpiCmd = {
     val byteBuffer = ByteBuffer.wrap(buffer)
     byteBuffer.order(ByteOrder.LITTLE_ENDIAN)
     val cmd = byteBuffer.getInt match {
@@ -130,9 +124,7 @@ object JtagVpi {
     VpiCmd(cmd, bufferOut, bufferIn, length, nbBits)
   }
 
-  def XFERT_MAX_SIZE = 512
-
-  def serialize(buffer: Array[Byte], vpiCmd: VpiCmd) = {
+  private def serialize(buffer: Array[Byte], vpiCmd: VpiCmd) = {
     val byteBuffer = ByteBuffer.wrap(buffer)
     byteBuffer.order(ByteOrder.LITTLE_ENDIAN)
     vpiCmd.cmd match {
@@ -148,10 +140,13 @@ object JtagVpi {
     byteBuffer.putInt(vpiCmd.nbBits)
   }
 
-  def MaxSizeOfVpiCmd = 4 + 2 * XFERT_MAX_SIZE + 4 + 4
+  private def MaxSizeOfVpiCmd = 4 + 2 * XFERT_MAX_SIZE + 4 + 4
 
-  case class VpiCmd(cmd: Cmds.Cmd, bufferOut: Array[Byte], bufferIn: Array[Byte], length: Int, nbBits: Int)
-  object Cmds extends Enumeration {
+  private def XFERT_MAX_SIZE = 512
+
+  private case class VpiCmd(cmd: Cmds.Cmd, bufferOut: Array[Byte], bufferIn: Array[Byte], length: Int, nbBits: Int)
+
+  private object Cmds extends Enumeration {
     type Cmd = Value
     val RESET, TMS_SEQ, SCAN_CHAIN, SCAN_CHAIN_FLIP_TMS, STOP_SIMU = Value
   }
