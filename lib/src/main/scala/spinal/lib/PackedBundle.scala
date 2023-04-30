7598f785a2142c9b58e451197c333bf1f8447c29
Aidan Klein <aidan@shader.me>
2023-04-30 11:19:27 -0400

    Use element width for PackedBundle width
    The fixes a memory width issue when using PackedBundle and stream FIFOs

--- lib/src/main/scala/spinal/lib/PackedBundle.scala
+++ lib/src/main/scala/spinal/lib/PackedBundle.scala
@@ -102,7 +102,7 @@ class PackedBundle extends Bundle {
     }
   }
 
-  override def getBitsWidth: Int = mapBuilder.width
+  override def getBitsWidth: Int = flatten.map(_.getBitsWidth).sum
 
   implicit class DataPositionEnrich[T <: Data](t: T) {
     /**
