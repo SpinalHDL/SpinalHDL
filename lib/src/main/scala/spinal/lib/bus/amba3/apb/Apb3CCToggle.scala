f447738442c93650ba5eb385fa0d2c68371eab32
Dolu1990 <charles.papon.90@gmail.com>
2023-05-03 13:45:25 +0100

    Remove Apb3CCToggle, replaced by Apb3CC Improve Apb3CC cross clock

--- lib/src/main/scala/spinal/lib/bus/amba3/apb/Apb3CCToggle.scala
+++ lib/src/main/scala/spinal/lib/bus/amba3/apb/Apb3CCToggle.scala
@@ -30,108 +30,6 @@ import spinal.lib._
 import spinal.lib.fsm._
 
 
-/**
-  * Apb3 Cross Clock Domain by toggling
-  *
-  * @param busConfig  Apb3 configuration
-  * @param inClk      Input Clock Domain
-  * @param outClk     Output Clock Domain
-  * @param selIds     Sequence indicating to which PSEL(x) signals the component replies to.
-  */
-class Apb3CCToggle(busConfig: Apb3Config, inClk: ClockDomain, outClk: ClockDomain, selIds: Seq[Int] = Seq(0)) extends Component {
-
-  val io = new Bundle{
-    val input  = slave(Apb3(busConfig))
-    val output = master(Apb3(busConfig))
-  }
-
-  val outAreaHit       = Bool()
-  val outAreaPRDATA    = cloneOf(io.output.PRDATA)
-  val outAreaPSLVERROR = if(busConfig.useSlaveError) cloneOf(io.output.PSLVERROR) else null
-
-  /**
-    * Input clock domain
-    */
-  val inClkArea = new ClockingArea(inClk){
-
-    val hit    = BufferCC(outAreaHit, False)
-    val target = RegInit(False)
-
-    val pReady = False
-
-    io.input.PRDATA := BufferCC(outAreaPRDATA)
-    io.input.PREADY := RegNext(pReady, False)
-    if(busConfig.useSlaveError) io.input.PSLVERROR := BufferCC(outAreaPSLVERROR)
-
-    /** State machine */
-    val sm = new StateMachine{
-
-      val sIdle: State = new State with EntryPoint{
-        whenIsActive{
-          val selHit = selIds.map(io.input.PSEL(_)).reduce(_||_)
-          when(io.input.PENABLE && selHit && hit === target){
-            target := !target
-            goto(sAccess)
-          }
-        }
-      }
-
-      val sAccess: State = new State{
-        whenIsActive{
-          when(hit === target){
-            pReady := True
-            goto(sWait)
-          }
-        }
-      }
-
-      val sWait: State = new State{
-        whenIsActive{
-          goto(sIdle)
-        }
-      }
-    }
-  }
-
-
-  /**
-    * Output clock domain
-    */
-  val outClkArea = new ClockingArea(outClk){
-
-    val target = BufferCC(inClkArea.target, False)
-    val hit    = RegInit(False)
-
-    val pENABLE   = RegInit(False)
-    val pRDATA    = Reg(cloneOf(io.output.PRDATA))
-    val pSLVERROR = if(busConfig.useSlaveError) Reg(cloneOf(io.output.PSLVERROR)) else null
-
-    io.output.PENABLE := pENABLE
-    io.output.PADDR   := BufferCC(io.input.PADDR)
-    io.output.PSEL    := BufferCC(io.input.PSEL)
-    io.output.PWRITE  := BufferCC(io.input.PWRITE)
-    io.output.PWDATA  := BufferCC(io.input.PWDATA)
-
-    when(target =/= hit & !pENABLE){
-      pENABLE := True
-    }
-
-    when(pENABLE & io.output.PREADY){
-      hit     := !hit
-      pENABLE := False
-      pRDATA  := io.output.PRDATA
-      if(busConfig.useSlaveError) pSLVERROR := io.output.PSLVERROR
-    }
-
-    outAreaHit    := hit
-    outAreaPRDATA := pRDATA
-    if(busConfig.useSlaveError) outAreaPSLVERROR := pSLVERROR
-  }
-
-}
-
-
-
 
 case class Apb3CC(config : Apb3Config,
                   inputClock : ClockDomain,
@@ -153,7 +51,7 @@ case class Apb3CC(config : Apb3Config,
   }
 
   val inputLogic = new ClockingArea(inputClock){
-    val inputCmd = Stream(Cmd())
+    val inputCmd = Flow(Cmd())
     val inputRsp = Flow(Rsp())
     val state = RegInit(False) setWhen(inputCmd.fire) clearWhen(inputRsp.valid)
     inputCmd.valid := io.input.PSEL.lsb && io.input.PENABLE && !state
@@ -167,7 +65,8 @@ case class Apb3CC(config : Apb3Config,
   }
 
   val outputLogic = new ClockingArea(outputClock){
-    val outputCmd = StreamCCByToggle(inputLogic.inputCmd, inputClock, outputClock)
+    val outputCmdFlow = FlowCCByToggle(inputLogic.inputCmd, inputClock, outputClock, withOutputM2sPipe = false)
+    val outputCmd = outputCmdFlow.toStream.m2sPipe(crossClockData = true)
     val state = RegInit(False)
 
     io.output.PENABLE := False
@@ -200,4 +99,29 @@ case class Apb3CC(config : Apb3Config,
     inputLogic.inputRsp := FlowCCByToggle(outputRsp, outputClock, inputClock)
   }
 }
+
+
+
+/**
+  * Apb3 Cross Clock Domain by toggling
+  *
+  * @param busConfig  Apb3 configuration
+  * @param inClk      Input Clock Domain
+  * @param outClk     Output Clock Domain
+  * @param selIds     Sequence indicating to which PSEL(x) signals the component replies to.
+  */
+class Apb3CCToggle(busConfig: Apb3Config, inClk: ClockDomain, outClk: ClockDomain, selIds: Seq[Int] = Seq(0)) extends Component {
+
+  val io = new Bundle{
+    val input  = slave(Apb3(busConfig))
+    val output = master(Apb3(busConfig))
+  }
+
+  assert(selIds == Seq(0))
+  val cc = Apb3CC(busConfig, inClk, outClk)
+  cc.io.input <> io.input
+  cc.io.output <> io.output
+}
+
+
 
