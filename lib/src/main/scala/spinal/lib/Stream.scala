3519cef171690a5808b91c8f0cb3a034dcec7d58
Dolu1990 <charles.papon.90@gmail.com>
2023-05-09 18:37:05 +0100

    fix #1111 by using signalCache on Stream.fire

--- lib/src/main/scala/spinal/lib/Stream.scala
+++ lib/src/main/scala/spinal/lib/Stream.scala
@@ -277,19 +277,19 @@ class Stream[T <: Data](val payloadType :  HardType[T]) extends Bundle with IMas
 
 /** Return True when a transaction is present on the bus but the ready signal is low
     */
-  def isStall : Bool = (valid && !ready).setCompositeName(this, "isStall", true)
+  def isStall : Bool = signalCache(this ->"isStall")((valid && !ready).setCompositeName(this, "isStall", true))
 
   /** Return True when a transaction has appeared (first cycle)
     */
-  def isNew : Bool = (valid && !(RegNext(isStall) init(False))).setCompositeName(this, "isNew", true)
+  def isNew : Bool = signalCache(this ->"isNew")((valid && !(RegNext(isStall) init(False))).setCompositeName(this, "isNew", true))
 
   /** Return True when a transaction occurs on the bus (valid && ready)
   */
-  override def fire: Bool = (valid & ready).setCompositeName(this, "fire", true)
+  override def fire: Bool = signalCache(this ->"fire")((valid & ready).setCompositeName(this, "fire", true))
 
 /** Return True when the bus is ready, but no data is present
   */
-  def isFree: Bool = (!valid || ready).setCompositeName(this, "isFree", true)
+  def isFree: Bool = signalCache(this ->"isFree")((!valid || ready).setCompositeName(this, "isFree", true))
   
   def connectFrom(that: Stream[T]): Stream[T] = {
     this.valid := that.valid
