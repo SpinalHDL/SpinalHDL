10122aacdb8498eb3dcc418962a561a3ca4267c3
Yindong Xiao <xydarcher@qq.com>
2023-07-04 06:35:08 +0000

    remove unnecessary verif code.

--- lib/src/main/scala/spinal/lib/Stream.scala
+++ lib/src/main/scala/spinal/lib/Stream.scala
@@ -2059,16 +2059,12 @@ class StreamTransactionCounter(
 
     def formalAsserts() = new Composite(this, "asserts") {
       val startedReg = Reg(Bool()) init False
-      val started = CombInit(startedReg)
-      val waiting = io.working & !started
       when(io.targetFire & io.working) {
-        started := True
         startedReg := True
       }
       when(done) { startedReg := False }
+      assert(startedReg === (counter.value > 0))
 
-      when(startedReg) { assert(io.working && counter.value > 0) }
-      when(counter.value > 0) { assert(started) }
       when(!io.working) { assert(counter.value === 0) }
       assert(counter.value <= expected)
     }
