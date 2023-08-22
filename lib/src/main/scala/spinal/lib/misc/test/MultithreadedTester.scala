9108faa7b362860238c36915fcae8825002f5f99
Dolu1990 <charles.papon.90@gmail.com>
2023-08-22 13:39:49 +0200

    Add lib.misc.test.MultithreadedTester

---
+++ lib/src/main/scala/spinal/lib/misc/test/MultithreadedTester.scala
@@ -0,0 +1,62 @@
+package spinal.lib.misc.test
+
+import org.apache.commons.io.FileUtils
+
+import java.io.{File, OutputStream, PrintStream, PrintWriter}
+import java.util.concurrent.ForkJoinPool
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext, Future}
+
+
+class MultithreadedTester(threadCount : Int = 0, logsPath : File = new File("logs")){
+  val finalThreadCount = if(threadCount > 0) threadCount else {
+    new oshi.SystemInfo().getHardware.getProcessor.getLogicalProcessorCount
+  }
+  implicit val ec = ExecutionContext.fromExecutorService(
+    new ForkJoinPool(finalThreadCount, ForkJoinPool.defaultForkJoinWorkerThreadFactory, null, true)
+  )
+
+  val jobs = ArrayBuffer[Job]()
+  class Job(testName : String) (body : => Unit){
+    FileUtils.forceMkdir(logsPath)
+    val file = new PrintStream(new File(logsPath, testName + ".log"))
+    val originalOutput = Console.out
+    var failed = false
+    val future = Future{
+      Console.withOut(file){
+        Console.withErr(file)(
+          try {
+            body
+          } catch {
+            case e: Throwable => {
+              failed = true
+              println(e.getMessage)
+              println(e.getStackTrace.map(_.toString).mkString("\n"))
+            }
+          }
+        )
+      }
+      if(failed){
+        println(s"$testName failed")
+      }
+    }
+
+    def join(): Unit = {
+      Await.result(future, Duration.Inf)
+      file.flush()
+      file.close()
+    }
+  }
+
+  def test(testName: String)(testFun: => Unit) {
+    jobs += new Job(testName)(testFun)
+  }
+
+  def await(): Unit = {
+    jobs.foreach(_.join())
+    jobs.foreach(j => if(j.failed) throw new Exception(s"Some jobs failed, see ${logsPath.getAbsolutePath}"))
+    jobs.clear()
+  }
+}
