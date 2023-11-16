c614c04226e75887451e82838bba99e69d5ee416
Ronan Lashermes <ronan.lashermes@inria.fr>
2023-11-16 17:25:55 +0100

    commenting idslpayload

--- idslpayload/src/main/scala/spinal/idslplugin/Annotation.scala
+++ idslpayload/src/main/scala/spinal/idslplugin/Annotation.scala
@@ -1,9 +1,43 @@
 package spinal.idslplugin
 
+
+/**
+ * A trait representing a callback mechanism for post-initialization operations.
+ * 
+ * This trait is used to define an action that should be taken after an object has been initialized.
+ * Classes or traits implementing `PostInitCallback` can define custom behavior that is executed
+ * after the initial construction and setup of the instance.
+ */
 trait PostInitCallback {
+  /**
+   * The method to be called after the initialization of the object.
+   *
+   * Implementing classes should override this method to define custom post-initialization behavior.
+   * This method is expected to return the current instance (`this`) of the class.
+   *
+   * @return the current instance of the class implementing this trait.
+   */
   def postInitCallback(): this.type
 }
 
+/**
+ * A trait for callback handling during value assignments.
+ * 
+ * This trait provides a mechanism to define custom behavior whenever a value is assigned to a variable.
+ * It is particularly useful in scenarios where additional actions need to be taken or side-effects need
+ * to be considered during value assignments.
+ */
 trait ValCallback {
+  /**
+   * A method to be called during the assignment of a value.
+   * 
+   * Implementors can override this method to define custom behavior that occurs when a value is assigned.
+   * This method can be used to perform additional checks, logging, or other side-effects.
+   *
+   * @param ref The value being assigned.
+   * @param name The name of the variable to which the value is being assigned.
+   * @tparam T The type of the value being assigned.
+   * @return The value after potential modifications or checks.
+   */
   def valCallback[T](ref: T, name: String): T
 }
--- idslpayload/src/main/scala/spinal/idslplugin/Macro.scala
+++ idslpayload/src/main/scala/spinal/idslplugin/Macro.scala
@@ -3,23 +3,47 @@ package spinal.idslplugin
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox.Context
 
-
-
-class Location(val file : String, val line: Int, val col : Int)
+/**
+ * Represents a source code location.
+ *
+ * This class is used to capture and represent the location of a piece of code in the source files.
+ * It includes information about the file name, line number, and column number.
+ *
+ * @param file The name of the source file.
+ * @param line The line number in the source file.
+ * @param col  The column number in the source file.
+ */
+class Location(val file: String, val line: Int, val col: Int)
 
 object Location {
+  /**
+   * Captures the current source code location.
+   *
+   * This method uses a Scala macro to capture the location in the source code where it is called.
+   * It is useful for debugging, logging, or providing more informative error messages.
+   *
+   * @return A Location instance representing the current position in the source code.
+   */
   implicit def capture: Location = macro locationMacro
 
+  /**
+   * Macro implementation to capture the source code location.
+   *
+   * @param x The macro context used by the Scala compiler.
+   * @return An expression that evaluates to a Location instance.
+   */
   def locationMacro(x: Context): x.Expr[Location] = {
     import x.universe._
 
-//    val className = Option(x.enclosingClass).map(_.symbol.toString).getOrElse("")
-//    val methodName = Option(x.enclosingMethod).map(_.symbol.toString).getOrElse("")
-    val pos  = x.enclosingPosition
-    val line =  pos.line
-    val col  =  pos.column
-    val file =  pos.source.toString().replace(".scala", "")
-//    val where = s"${line}"
+    // Capture the position in the source code where the macro is called.
+    val pos = x.enclosingPosition
+    val line = pos.line
+    val col = pos.column
+
+    // Extract the source file name, removing the ".scala" extension.
+    val file = pos.source.toString().replace(".scala", "")
+
+    // Create and return an expression that evaluates to a new Location instance.
     reify(new Location(x.literal(file).splice, x.literal(line).splice, x.literal(col).splice))
   }
 }
