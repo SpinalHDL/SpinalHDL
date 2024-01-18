16e79ae702ffc5383ee57fa90af722f8cd04227d
Pengcheng Xu <pengxu@ethz.ch>
2024-01-18 13:16:58 +0100

    document Axi4Stream master/slave

--- lib/src/main/scala/spinal/lib/bus/amba4/axis/sim/Axi4StreamMaster.scala
+++ lib/src/main/scala/spinal/lib/bus/amba4/axis/sim/Axi4StreamMaster.scala
@@ -1,4 +1,4 @@
-package axi.sim
+package spinal.lib.bus.amba4.axis.sim
 
 import spinal.core._
 import spinal.core.sim._
@@ -8,6 +8,25 @@ import spinal.lib.sim._
 
 import scala.collection.mutable
 
+/**
+ * Simulation master for the Axi4Stream bus protocol [[Axi4Stream]].
+ *
+ * @constructor create a simulation master with the given bus instance and clock domain
+ * @param axis bus master to drive
+ * @param clockDomain clock domain to sample data on
+ * @example
+ * {{{
+ *   SimConfig.compile(new Component {
+ *     val io = new Bundle {
+ *       val axisSlave = slave(Axi4Stream(Axi4StreamConfig(32)))
+ *     }
+ *     io.axisSlave.assignDontCare
+ *   }).doSim("sample") { dut =>
+ *     val master = Axi4StreamMaster(dut.io.axisSlave, dut.clockDomain)
+ *     master.send(Random.nextBytes(256).toList)
+ *   }
+ * }}}
+ */
 case class Axi4StreamMaster(axis: Axi4Stream, clockDomain: ClockDomain) {
   private val busConfig = axis.config
   private val queue = mutable.Queue[Axi4StreamBundle => Unit]()
@@ -16,6 +35,7 @@ case class Axi4StreamMaster(axis: Axi4Stream, clockDomain: ClockDomain) {
     println(s"Axi4StreamMaster: $msg")
   }
 
+  /** Send synchronously a full transaction to the bus. */
   def send(data: List[Byte]): Unit = {
     val mtx = SimMutex().lock()
     sendCB(data) {
@@ -24,6 +44,7 @@ case class Axi4StreamMaster(axis: Axi4Stream, clockDomain: ClockDomain) {
     mtx.await()
   }
 
+  /** Send asynchronously; same as {@link send}, but completion is delivered in a callback */
   def sendCB(data: List[Byte])(callback: => Unit): Unit = {
     val fullLength = roundUp(data.length, busConfig.dataWidth).toInt
     if (fullLength != data.length && !busConfig.useStrb && !busConfig.useKeep) {
@@ -58,6 +79,7 @@ case class Axi4StreamMaster(axis: Axi4Stream, clockDomain: ClockDomain) {
     }
   }
 
+  /** Reset bus slave (dropping all pending transactions) */
   def reset(): Unit = {
     queue.clear
 
--- lib/src/main/scala/spinal/lib/bus/amba4/axis/sim/Axi4StreamSlave.scala
+++ lib/src/main/scala/spinal/lib/bus/amba4/axis/sim/Axi4StreamSlave.scala
@@ -1,13 +1,36 @@
-package axi.sim
+package spinal.lib.bus.amba4.axis.sim
 
 import spinal.core._
 import spinal.core.sim._
 import spinal.lib.sim._
 import spinal.lib.bus.amba4.axis.Axi4Stream._
 
-import java.util.concurrent.atomic.AtomicReference
 import scala.collection.mutable
 
+/**
+ * Simulation slave for the Axi4Stream bus protocol [[Axi4Stream]].
+ *
+ * @constructor create a simulation slave with the given bus instance and clock domain
+ * @param axis bus slave to drive
+ * @param clockDomain clock domain to sample data on
+ * @example
+ * {{{
+ *   SimConfig.compile(new Component {
+ *     val io = new Bundle {
+ *       val axisMaster = master(Axi4Stream(Axi4StreamConfig(32)))
+ *     }
+ *     io.axisMaster.assignDontCare
+ *   }).doSim("sample") { dut =>
+ *     val slave = Axi4StreamSlave(dut.io.axisMaster, dut.clockDomain)
+ *     val data = slave.recv()
+ *   }
+ * }}}
+ *
+ * @note The current implementation does not buffer unexpected transactions (i.e. bus activity when no
+ * {@link recv} has been issued).  In some race conditions, this may result in incomplete captures due to the first
+ * beats being lost.  Consider enqueuing a asynchronous request with the callback interface ({@link recvCB}) before
+ * issuing the triggering action.
+ */
 case class Axi4StreamSlave(axis: Axi4Stream, clockDomain: ClockDomain) {
   private val busConfig = axis.config
   private val queue = mutable.Queue[Axi4StreamBundle => Unit]()
@@ -16,6 +39,7 @@ case class Axi4StreamSlave(axis: Axi4Stream, clockDomain: ClockDomain) {
     println(s"Axi4StreamSlave\t: $msg")
   }
 
+  /** Receive synchronously a full transaction from the bus. */
   def recv(): List[Byte] = {
     var result: List[Byte] = null
     val mtx = SimMutex().lock()
@@ -27,6 +51,7 @@ case class Axi4StreamSlave(axis: Axi4Stream, clockDomain: ClockDomain) {
     result
   }
 
+  /** Receive asynchronously; same as {@link recv}, but result is delivered in a callback */
   def recvCB()(callback: List[Byte] => Unit): Unit = {
     val builder = new mutable.ArrayBuilder.ofByte
 
@@ -61,6 +86,7 @@ case class Axi4StreamSlave(axis: Axi4Stream, clockDomain: ClockDomain) {
     }
   }
 
+  /** Reset bus slave (dropping all pending transactions) */
   def reset(): Unit = {
     queue.clear
   }
