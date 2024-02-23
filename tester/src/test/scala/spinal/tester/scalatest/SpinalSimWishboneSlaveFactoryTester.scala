77efadfc40842ce2d6661de7088faf3765073aa9
Justin Berger <j.david.berger@gmail.com>
2024-02-23 00:08:15 -0700

    Modify WishboneSlaveFactory to test memory locations

--- tester/src/test/scala/spinal/tester/scalatest/SpinalSimWishboneSlaveFactoryTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/SpinalSimWishboneSlaveFactoryTester.scala
@@ -1,21 +1,23 @@
 package spinal.tester.scalatest
 
-import org.scalatest.funsuite.AnyFunSuite
 import spinal.core._
 import spinal.core.sim._
-import spinal.sim._
 import spinal.lib._
 import spinal.lib.bus.wishbone._
 import spinal.lib.wishbone.sim._
 import spinal.lib.sim._
-import scala.util.Random
+
+import scala.language.postfixOps
 
 class WishboneSimpleSlave(config : WishboneConfig) extends Component{
   val io = new Bundle{
     val bus = slave(Wishbone(config))
   }
   val busCtrl = WishboneSlaveFactory(io.bus)
-  val reg = busCtrl.createReadWrite(Bits(busCtrl.busDataWidth bits),10)
+  val regA = busCtrl.createReadOnly(Bits(busCtrl.busDataWidth bits), 10 * config.dataWidth / 8)
+  regA := 100
+  val regB = busCtrl.createReadAndWrite(Bits(busCtrl.busDataWidth bits), 0)
+  regB.init(1)
 }
 
 class SpinalSimWishboneSlaveFactoryTester extends SpinalAnyFunSuite{
@@ -33,25 +35,34 @@ class SpinalSimWishboneSlaveFactoryTester extends SpinalAnyFunSuite{
       if(dut.io.bus.config.useLOCK) dut.io.bus.LOCK #= false
       if(dut.io.bus.config.isPipelined) dut.io.bus.STALL #= false
 
-      dut.io.bus.ACK #= false
-      dut.io.bus.DAT_MISO #= 0
+      dut.clockDomain.waitSampling()
 
       val scoreboard = ScoreboardInOrder[WishboneTransaction]()
-      val driver = new WishboneDriver(dut.io.bus, dut.clockDomain)
-      // val monitor = WishboneMonitor(dut.io.bus, dut.clockDomain){ bus =>
-      //   if(bus.WE.toBoolean)
-      //     scoreboard.pushRef(WishboneTransaction.sampleAsSlave(bus))
-      //   else
-      //     scoreboard.pushDut(WishboneTransaction.sampleAsMaster(bus))
-      // }
+      scoreboard.ref ++= List(
+        WishboneTransaction(10, 100), WishboneTransaction(0, 1),
+        WishboneTransaction(10, 100), WishboneTransaction(0, 1),
+        WishboneTransaction(10, 100), WishboneTransaction(0, 2)
+      )
 
-      for(repeat <- 0 until 1000){
-        driver.drive(for(x <- 1 to 10) yield WishboneTransaction(address=10).randomizeData(dut.io.bus.config.dataWidth),true)
-        dut.clockDomain.waitSampling(10)
-        driver.drive(for(x <- 1 to 10) yield WishboneTransaction(address=10),false)
-        dut.clockDomain.waitSampling(10)
-
+      val monitor = WishboneMonitor(dut.io.bus, dut.clockDomain){ bus =>
+           scoreboard.pushDut(WishboneTransaction.sampleAsMaster(bus))
       }
+
+      val dri = new WishboneDriver(dut.io.bus, dut.clockDomain)
+      dri.drive(scala.collection.immutable.Seq(WishboneTransaction(10), WishboneTransaction(0)), we = false)
+      dri.drive(scala.collection.immutable.Seq(WishboneTransaction(10, 200), WishboneTransaction(0, 2)), we = true)
+      dri.drive(scala.collection.immutable.Seq(WishboneTransaction(10), WishboneTransaction(0)), we = false)
+
+      println(scoreboard.dut)
+//
+//      for(repeat <- 0 until 100){
+//        driver.drive(for(x <- 1 to 10) yield WishboneTransaction(address=10).randomizeData(dut.io.bus.config.dataWidth), we = true)
+//        dut.clockDomain.waitSampling(10)
+//        driver.drive(for(x <- 1 to 10) yield WishboneTransaction(address=10), we = false)
+//        dut.clockDomain.waitSampling(10)
+//      }
+//
+
     }
   }
 
@@ -60,8 +71,13 @@ class SpinalSimWishboneSlaveFactoryTester extends SpinalAnyFunSuite{
     testBus(conf,description="classic")
   }
 
-  test("pipelined"){
-    val conf = WishboneConfig(8,8).pipelined
-    testBus(conf,description="pipelined")
+  test("classic32"){
+    val conf = WishboneConfig(32, 32)
+    testBus(conf, description="classic32")
   }
+
+//  test("pipelined"){
+//    val conf = WishboneConfig(8,8).pipelined
+//    testBus(conf,description="pipelined")
+//  }
 }
