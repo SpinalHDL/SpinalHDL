88525c427d8bbba301c2087397642c74d8e32a56
anton schulte <mail@anton-schulte.de>
2024-02-29 12:38:00 +0100

    Handle Quotes in Attribute values

---
+++ tester/src/test/scala/spinal/tester/scalatest/AttributeEmitTests.scala
@@ -0,0 +1,17 @@
+package spinal.tester.scalatest
+
+import spinal.tester.SpinalAnyFunSuite
+import spinal.core._
+
+class AttributeEmitTests extends SpinalAnyFunSuite {
+  test("check escaping of quotes in attributes") {
+    class Test extends Component {
+      val a = RegInit(False)
+      a.addAttribute("testname", "test \"value\" ")
+    }
+    val verilog = SpinalVerilog(new Test)
+    assert(verilog.getRtlString().contains("test \\\"value\\\""))
+    val vhdl = SpinalVhdl(new Test)
+    assert(vhdl.getRtlString().contains("test \"\"value\"\""))
+  }
+}
