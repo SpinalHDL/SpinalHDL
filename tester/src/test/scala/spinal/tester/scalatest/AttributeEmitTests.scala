9731dbfdf04787b64e3b49f91c736e1d66364d56
Anton Schulte <anton.schulte@hhi.fraunhofer.de>
2024-03-04 11:46:31 +0100

    added quotes to the test

--- tester/src/test/scala/spinal/tester/scalatest/AttributeEmitTests.scala
+++ tester/src/test/scala/spinal/tester/scalatest/AttributeEmitTests.scala
@@ -7,11 +7,11 @@ class AttributeEmitTests extends SpinalAnyFunSuite {
   test("check escaping of quotes in attributes") {
     class Test extends Component {
       val a = RegInit(False)
-      a.addAttribute("testname", "test \"value\" ")
+      a.addAttribute("testname", "test \"value\" bar")
     }
     val verilog = SpinalVerilog(new Test)
-    assert(verilog.getRtlString().contains(raw"""test \"value\""""))
+    assert(verilog.getRtlString().contains(raw""""test \"value\" bar""""))
     val vhdl = SpinalVhdl(new Test)
-    assert(vhdl.getRtlString().contains(raw"""test ""value"""""))
+    assert(vhdl.getRtlString().contains(raw""""test ""value"" bar""""))
   }
 }
