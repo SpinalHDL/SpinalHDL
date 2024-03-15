f03756889d858aed193887b6f5b329245e3836ba
Pengcheng Xu <pengxu@ethz.ch>
2024-03-15 19:23:27 +0100

    respReady should only assert when we actually have a read

--- lib/src/main/scala/spinal/lib/bus/misc/BusSlaveFactory.scala
+++ lib/src/main/scala/spinal/lib/bus/misc/BusSlaveFactory.scala
@@ -603,10 +603,12 @@ trait BusSlaveFactory extends Area{
     val counter = Counter(blockCycles.getWidth bits)
     val wordCount = (1 + that.payload.getBitsWidth - 1) / busDataWidth + 1
     val counterWillOverflow = counter === blockCycles
-    val respReady = RegNextWhen(True, counterWillOverflow || that.valid) init False
+    val readIssued = False
+    val respReady = RegNextWhen(True, counterWillOverflow || (readIssued && !counterWillOverflow && that.valid)) init False
     val counterOverflowed = RegNextWhen(True, counterWillOverflow) init False
     // we only halt the first beat, since if we got a stream payload we got it all
     onReadPrimitive(SingleMapping(address), haltSensitive = false, null) {
+      readIssued := True
       when(!respReady) {
         counter.increment()
         readHalt()
