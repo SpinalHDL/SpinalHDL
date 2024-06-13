c3fcf5204aa8d74d7cb7634efc4cdb8714b24823
Dolu1990 <charles.papon.90@gmail.com>
2024-06-13 14:30:37 +0200

    Vex construction now set elements parent #1443

--- core/src/main/scala/spinal/core/Vec.scala
+++ core/src/main/scala/spinal/core/Vec.scala
@@ -44,23 +44,15 @@ trait VecFactory {
 
   def Vec[T <: Data](gen: => T, size: Int): Vec[T] = Vec.fill(size)(gen)
   def Vec[T <: Data](gen: HardType[T], size: Int): Vec[T] = Vec.fill(size)(gen())
-
-//  def Vec[T <: Data](gen: Vec[T], size: Int): Vec[Vec[T]] = fill(size)(cloneOf(gen))
-
-//  def Vec[T <: Data](gen: (Int) => T, size: Int): Vec[T] = tabulate(size)(gen)
-
-  //def apply[T <: Data](gen : => Vec[T],size : Int) : Vec[Vec[T]] = fill(size)(gen)
-
-
   def Vec[T <: Data](firstElement: T, followingElements: T*): Vec[T] = Vec(List(firstElement) ++ followingElements)
 
   class VecBuilder{
     def tabulate[T <: Data](size: Int)(gen: (Int) => T): Vec[T] = {
-      Vec((0 until size).map(gen(_)))
+      Vec((0 until size).map(gen(_))).setElementsParents()
     }
 
     def fill[T <: Data](size: Int)(dataType: => T): Vec[T] = {
-      Vec((0 until size).map(_ => dataType), HardType(dataType))
+      Vec((0 until size).map(_ => dataType), HardType(dataType)).setElementsParents()
     }
   }
   val Vec = new VecBuilder()
@@ -99,6 +91,11 @@ class VecAccessAssign[T <: Data](enables: Seq[Bool], tos: Seq[BaseType], vec: Ve
   * @see  [[http://spinalhdl.github.io/SpinalDoc/spinal/core/types/Vector Vec Documentation]]
   */
 class Vec[T <: Data](var _dataType : HardType[T], val vec: Vector[T]) extends MultiData with collection.IndexedSeq[T] {
+
+  def setElementsParents(): this.type = {
+    vec.foreach(_.parent = this)
+    this
+  }
 
   def dataType = {
     if(_dataType == null){
