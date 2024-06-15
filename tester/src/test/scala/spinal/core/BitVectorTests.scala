13cbbeb9ca53fab75d20abf379801c7df73b484b
Readon <Readon@users.noreply.github.com>
2024-06-15 18:20:12 +0800

    Update BitVectorTests.scala to fix formal dut

--- tester/src/test/scala/spinal/core/BitVectorTests.scala
+++ tester/src/test/scala/spinal/core/BitVectorTests.scala
@@ -33,24 +33,28 @@ class SubdivideInTest extends SpinalFormalFunSuite {
     val i = Bits(width bit)
     val o = Bits(width bit)
     val split = i.subdivideIn(sliceWidth, strict = false)
-
-    if (width % sliceWidth.value == 0)
-      split.foreach(s => assert(s.getWidth == sliceWidth.value))
-    else
-      split.dropRight(1).foreach(s => assert(s.getWidth == sliceWidth.value))
-
+    
     o := Cat(split.asInstanceOf[Iterable[Data]])
-    assert(i === o)
+    
+    def formalAsserts() ={
+      if (width % sliceWidth.value == 0)
+        split.foreach(s => assert(s.getWidth == sliceWidth.value))
+      else
+        split.dropRight(1).foreach(s => assert(s.getWidth == sliceWidth.value))
+      assert(i === o)
+    }   
   }
 
   case class SliceDutSlices(width: Int, slicesCount: SlicesCount) extends Component {
     val i = Bits(width bit)
     val o = Bits(width bit)
     val split = i.subdivideIn(slicesCount, strict = false)
-    assert(split.size == slicesCount.value, s"split into ${split.size} slices, not ${slicesCount.value}")
-
     o := Cat(split.asInstanceOf[Iterable[Data]])
-    assert(i === o)
+
+    def formalAsserts() ={
+      assert(split.size == slicesCount.value, s"split into ${split.size} slices, not ${slicesCount.value}")
+      assert(i === o)
+    }
   }
   
   test("mix of slicing operations") {
@@ -58,13 +62,13 @@ class SubdivideInTest extends SpinalFormalFunSuite {
       .withBMC(5)
       .withProve(5)
       .doVerify(new Component {
-        val dut_8_2bit = SliceDutBits(8, 2 bit) // default case
-        val dut_8_5bit = SliceDutBits(8, 5 bit) // uneven case, https://github.com/SpinalHDL/SpinalHDL/issues/1049
-        val dut_9_4bit = SliceDutBits(9, 4 bit) // odd width
-        val dut_2_3bit = SliceDutBits(2, 3 bit) // width smaller than sliceWidth
-        val dut_8_2slices = SliceDutSlices(8, 2 slices) // default case
-        val dut_8_3slices = SliceDutSlices(8, 3 slices) // uneven case
-        val dut_9_4slices = SliceDutSlices(9, 4 slices) // odd width
+        val dut_8_2bit = FormalDut(SliceDutBits(8, 2 bit)).formalAsserts() // default case
+        val dut_8_5bit = FormalDut(SliceDutBits(8, 5 bit)).formalAsserts() // uneven case, https://github.com/SpinalHDL/SpinalHDL/issues/1049
+        val dut_9_4bit = FormalDut(SliceDutBits(9, 4 bit)).formalAsserts() // odd width
+        val dut_2_3bit = FormalDut(SliceDutBits(2, 3 bit)).formalAsserts() // width smaller than sliceWidth
+        val dut_8_2slices = FormalDut(SliceDutSlices(8, 2 slices)).formalAsserts() // default case
+        val dut_8_3slices = FormalDut(SliceDutSlices(8, 3 slices)).formalAsserts() // uneven case
+        val dut_9_4slices = FormalDut(SliceDutSlices(9, 4 slices)).formalAsserts() // odd width
       }.setDefinitionName("BitVectorTests_SubdivideInTest"))
   }
 }
