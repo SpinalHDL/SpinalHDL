f5d09b5941469e342542c13c62855f4a738f41ca
schelten <niklas.schelten@hhi.fraunhofer.de>
2024-07-05 11:58:18 +0200

    add newline to VpiBackend output

--- sim/src/main/scala/spinal/sim/VpiBackend.scala
+++ sim/src/main/scala/spinal/sim/VpiBackend.scala
@@ -94,8 +94,8 @@ abstract class VpiBackend(val config: VpiBackendConfig) extends Backend {
 
   class Logger extends ProcessLogger {
     val logs = new StringBuilder()
-    override def err(s: => String): Unit = { logs ++= (s) }
-    override def out(s: => String): Unit = { logs ++= (s) }
+    override def err(s: => String): Unit = { logs ++= (s ++ Properties.lineSeparator) }
+    override def out(s: => String): Unit = { logs ++= (s ++ Properties.lineSeparator) }
     override def buffer[T](f: => T) = f
   }
 
