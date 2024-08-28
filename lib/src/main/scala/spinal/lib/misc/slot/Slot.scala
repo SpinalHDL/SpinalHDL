a20473831dd23c529111a07dc833bc028362d12c
Dolu1990 <charles.papon.90@gmail.com>
2024-08-28 08:38:18 +0200

    Add lib.misc.slot API

---
+++ lib/src/main/scala/spinal/lib/misc/slot/Slot.scala
@@ -0,0 +1,21 @@
+package spinal.lib.misc.slot
+
+import spinal.core._
+import spinal.lib._
+
+class Slot extends Area {
+  val fire = False
+  val valid = RegInit(False) clearWhen (fire)
+}
+
+class SlotPool[T <: Slot](slotsCount: Int)(gen: => T) extends Area {
+  val slots = for (i <- 0 until slotsCount) yield gen
+  val allocate = new Area {
+    val full = slots.map(_.valid).andR
+    val oh = B(OHMasking.firstV2(Vec(slots.map(!_.valid))))
+    val id = OHToUInt(oh)
+    def apply(body : T => Unit ){
+      slots.onMask(oh){s => s.valid := True; body(s)}
+    }
+  }
+}
