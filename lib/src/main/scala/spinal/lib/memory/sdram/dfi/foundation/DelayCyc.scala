89e9f632df7378b83eb05429f436f2d3eeb61bbe
LiYaohong <864366810@qq.com>
2024-09-18 17:03:11 +0800

    Modified division and remainder operations

--- lib/src/main/scala/spinal/lib/memory/sdram/dfi/foundation/DelayCyc.scala
+++ lib/src/main/scala/spinal/lib/memory/sdram/dfi/foundation/DelayCyc.scala
@@ -8,8 +8,8 @@ case class DelayCyc(config: DfiConfig, timeConfig: DfiTimeConfig) {
 
   def findsp(bools: Seq[Bool]): UInt = OHToUInt(OHMasking.first(B(bools)).asBools)
   def findlp(bools: Seq[Bool]): UInt = OHToUInt(OHMasking.last(B(bools)).asBools)
-  def sp2np(startphase: UInt, delaycyc: Int): UInt = (startphase + delaycyc) % config.frequencyRatio
+  def sp2np(startphase: UInt, delaycyc: Int): UInt = (startphase + delaycyc).resize(log2Up(config.frequencyRatio))
   def sp2np(startphase: Int, delaycyc: Int): Int = (startphase + delaycyc) % config.frequencyRatio
-  def mcdelaycyc(startphase: UInt, phydelaycyc: Int): UInt = (phydelaycyc) / config.frequencyRatio
+  def mcdelaycyc(startphase: UInt, phydelaycyc: Int): UInt = (phydelaycyc) >> log2Up(config.frequencyRatio)
   def mcdelaycyc(startphase: Int, phydelaycyc: Int): Int = (phydelaycyc) / config.frequencyRatio
 }
