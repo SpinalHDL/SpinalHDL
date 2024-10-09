20efcbd3b5225b0075b15fd318e1b0bb1acc1adb
Dolu1990 <charles.papon.90@gmail.com>
2024-10-09 15:35:52 +0200

    dma sg2 write only tester

--- tester/src/test/scala/spinal/lib/SpinalSimDmaSg2WriteOnlyTester.scala
+++ tester/src/test/scala/spinal/lib/SpinalSimDmaSg2WriteOnlyTester.scala
@@ -21,13 +21,13 @@ class SpinalSimDmaSg2WriteOnlyTester extends SpinalAnyFunSuite{
       addressWidth = 32,
       dataWidth = 64,
       blockSize = 64,
-      bufferBytes = 512,
+      bufferBytes = 128,
       bsbDataBytes = 4,
-      pendingSlots = 4
+      pendingSlots = 3
     )
     val ctrlParam = p.getCtrlParam()
 
-    val compiled = SimConfig.withFstWave.compile(
+    val compiled = SimConfig.compile(
       new DmaSgWriteOnlyComp(p, ctrlParam)
     )
     compiled.doSim(seed = 42){dut =>
@@ -35,7 +35,7 @@ class SpinalSimDmaSg2WriteOnlyTester extends SpinalAnyFunSuite{
       val cd = dut.clockDomain
       cd.forkStimulus(10)
 
-
+      def log(that : String) = print(that)//{}
       val mem = new tilelink.sim.MemoryAgent(bus = dut.io.mem, cd)(null)
       val ctrlAgent = new tilelink.sim.MasterAgent(dut.io.ctrl, cd)(null)
       val ctrl = new SimCtrl(ctrlAgent, 0x0)
@@ -60,7 +60,7 @@ class SpinalSimDmaSg2WriteOnlyTester extends SpinalAnyFunSuite{
         }
       })
 
-      case class Task(descriptor : SimWriteOnlyDescriptor, data : Array[Byte], last : Boolean){
+      case class Task(descriptor : SimWriteOnlyDescriptor, data : Array[Byte], last : Boolean, address : Long){
         var ptr = 0
         def popData(): Byte = {
           ptr += 1
@@ -73,36 +73,66 @@ class SpinalSimDmaSg2WriteOnlyTester extends SpinalAnyFunSuite{
       val bsbDriver = new BsbDriver(dut.io.bsb, cd)
       periodicaly(10000){
         bsbDriver.sd.setFactor(simRandom.nextFloat())
+        mem.driver.driver.randomizeStallRate()
       }
 
       val allocator = new MemoryRegionAllocator(0, 0x1000000)
-      for(_ <- 0 until 10) {
+      for(_ <- 0 until 1000) {
         val chain = ArrayBuffer[sg2.sim.SimWriteOnlyDescriptor]()
 //        for (i <- 0 until simRandom.nextInt(10)) {
         val descriptorCount = simRandom.nextInt(10)
-        val packetData = ArrayBuffer[Byte]()
         val packets = ArrayBuffer[BsbPacket]()
-        println("Ref :")
-        for (i <- 0 until descriptorCount) {
+        log("Ref :\n")
+        if(simRandom.nextFloat() < 0.1){ //Overflow
           val d = new SimWriteOnlyDescriptor(allocator.allocateAligned(32, 32).base.toLong)
-          val bufferSize = simRandom.nextInt(256)
-          val buffer = allocator.allocate(bufferSize) //allocator.allocate(simRandom.nextInt(256)+1)
+          val bufferSize = simRandom.nextInt(256) + 1
+          val buffer = allocator.allocate(bufferSize)
           d.to = buffer.base.toLong
           d.controlBytes = buffer.size.toInt
-          val isLast = i == descriptorCount-1 || simRandom.nextBoolean()
-          val size = if(isLast) simRandom.nextInt(bufferSize) else bufferSize
+          val size = bufferSize+1+simRandom.nextInt(32)
           val data = Array.fill[Byte](size)(simRandom.nextInt().toByte)
-          val task = new Task(d, data, isLast)
-          for(i <- data.indices) expectedWrites(d.to + i) = data(i)
-          for(i <- 0 to 3) allowedWrites += d.physicalAddress + DmaSgWriteOnly.statusAt + i
-          packetData ++= data
+          val dataPartial = data.take(bufferSize)
+          val task = new Task(d, dataPartial, false, d.to)
+          for (i <- dataPartial.indices) expectedWrites(d.to + i) = data(i)
+          for (i <- 0 to 3) allowedWrites += d.physicalAddress + DmaSgWriteOnly.statusAt + i
           tasks += task
           chain += d
-          print(data.size + " -> ")
-          if(isLast) {
-            println("last")
-            packets += new BsbPacket(0,0,packetData.toArray)
-            packetData.clear()
+          log(data.size + " -> overflow\n")
+          packets += new BsbPacket(0, 0, data, true)
+        } else {
+          if(simRandom.nextFloat() < 1.1){
+            bsbDriver.push(new BsbPacket(0, 0, Array.fill[Byte](1 + simRandom.nextInt(16))(simRandom.nextInt().toByte), false))
+            while(bsbDriver.sources(0).nonEmpty) cd.waitSampling(10)
+            bsbDriver.push(new BsbPacket(0, 0, Array.fill[Byte](simRandom.nextInt(64))(simRandom.nextInt().toByte), true))
+          }
+
+          val packetData = ArrayBuffer[Byte]()
+          for (i <- 0 until descriptorCount) { //Regular
+            val d = new SimWriteOnlyDescriptor(allocator.allocateAligned(32, 32).base.toLong)
+            val bufferSize = simRandom.nextInt(256) + 1
+            val buffer = allocator.allocate(bufferSize) //allocator.allocate(simRandom.nextInt(256)+1)
+            d.to = buffer.base.toLong
+            d.controlBytes = buffer.size.toInt
+            val isLast = i == descriptorCount - 1 || simRandom.nextBoolean()
+            val size = if (isLast) simRandom.nextInt(bufferSize) else bufferSize
+            val data = Array.fill[Byte](size)(simRandom.nextInt().toByte)
+            val task = new Task(d, data, isLast, d.to)
+            for (i <- data.indices) expectedWrites(d.to + i) = data(i)
+            for (i <- 0 to 3) allowedWrites += d.physicalAddress + DmaSgWriteOnly.statusAt + i
+            if (size == 0) {
+              //Needed to ensure we do not merge zero byte packets
+              packets += new BsbPacket(0, 0, packetData.toArray, false)
+              packetData.clear()
+            }
+            packetData ++= data
+            tasks += task
+            chain += d
+            log(data.size + " -> ")
+            if (isLast) {
+              log("last\n")
+              packets += new BsbPacket(0, 0, packetData.toArray, isLast)
+              packetData.clear()
+            }
           }
         }
 
@@ -124,20 +154,22 @@ class SpinalSimDmaSg2WriteOnlyTester extends SpinalAnyFunSuite{
         packets.foreach(bsbDriver.push)
 
         while (ctrl.busy()) {}
-        var packetId = 0
-        var byteId = 0
-        println("Got :")
+        log("Got :\n")
         for(d <- chain.dropRight(1)){
           d.read(mem.mem)
-          print(d.statusBytes + " -> ")
-          if(d.statusLast) println(" last")
+          log(d.statusBytes + " -> ")
+          if(d.statusLast) log(" last\n")
+          val t = tasks.dequeue()
+          assert(t.descriptor == d)
+          assert(t.last == d.statusLast)
+          assert(t.data.sameElements(mem.mem.readBytes(d.to, t.data.size)))
           assert(d.statusCompleted)
           allocator.free(d.physicalAddress)
-          if(d != chain.last) allocator.free(d.to)
+          if(d.controlBytes != 0) allocator.free(d.to)
         }
-        println("")
-        println("")
-        cd.waitSampling(1000)
+        log("\n\n")
+        while(bsbDriver.sources(0).nonEmpty) cd.waitSampling(10)
+//        cd.waitSampling(1000)
       }
       println(simTime())
       cd.waitSampling(10000)
@@ -148,6 +180,7 @@ class SpinalSimDmaSg2WriteOnlyTester extends SpinalAnyFunSuite{
       cd.waitSampling(1000)
       assert(expectedWrites.isEmpty)
       assert(allowedWrites.isEmpty)
+      assert(tasks.isEmpty)
     }
   }
 }
