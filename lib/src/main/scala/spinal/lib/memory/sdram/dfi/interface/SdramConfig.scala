3d79eb51cd9cd0b6c414cd94bfec09dae0027e69
Liyaohong <864366810@qq.com>
2024-12-26 23:16:20 +0800

    Fixed SdramConfig.scala.

--- lib/src/main/scala/spinal/lib/memory/sdram/dfi/interface/SdramConfig.scala
+++ lib/src/main/scala/spinal/lib/memory/sdram/dfi/interface/SdramConfig.scala
@@ -1,46 +1,69 @@
-package spinal.lib.memory.sdram.dfi.interface
+package spinal.lib.memory.sdram.dfi
 
 import spinal.core._
 
 class SdramGeneration(
-    val useFAW: Boolean,
-    val burstLength: Int,
-    val dataRate: Int
-)
+                       val RESETn: Boolean,
+                       val ODT: Boolean,
+                       val DQS: Boolean,
+                       val FAW: Boolean,
+                       val CCD: Int,
+                       val burstLength: Int,
+                       val dataRate: Int
+                     )
 
 object SdramGeneration {
   val SDR = new SdramGeneration(
-    useFAW = false,
+    RESETn = false,
+    ODT = false,
+    DQS = false,
+    FAW = false,
+    CCD = 1,
     burstLength = 1,
     dataRate = 1
-  )
+    )
   val DDR2 = new SdramGeneration(
-    useFAW = true,
+    RESETn = false,
+    ODT = true,
+    DQS = true,
+    FAW = true,
+    CCD = 2,
     burstLength = 4,
     dataRate = 2
-  )
+    )
   val DDR3 = new SdramGeneration(
-    useFAW = true,
+    RESETn = true,
+    ODT = true,
+    DQS = true,
+    FAW = true,
+    CCD = 4,
     burstLength = 8,
     dataRate = 2
-  )
+    )
+  val MYDDR = new SdramGeneration(
+    RESETn = true,
+    ODT = true,
+    DQS = true,
+    FAW = true,
+    CCD = 4,
+    burstLength = 8,
+    dataRate = 2
+    )
 }
 
 case class SdramConfig(
-    generation: SdramGeneration,
-    bgWidth: Int,
-    cidWidth: Int,
-    bankWidth: Int,
-    columnWidth: Int,
-    rowWidth: Int,
-    dataWidth: Int,
-    ddrMHZ: Int,
-    ddrWrLat: Int,
-    ddrRdLat: Int,
-    sdramTime: SdramTiming
-) {
+                        generation: SdramGeneration,
+                        bankWidth: Int,
+                        columnWidth: Int,
+                        rowWidth: Int,
+                        dataWidth: Int,
+                        ddrMHZ: Int,
+                        ddrWrLat: Int,
+                        ddrRdLat: Int,
+                        sdramtime: SdramTiming
+                      ) {
 
-  import sdramTime._
+  import sdramtime._
 
   def burstLength = generation.burstLength
   def wordAddressWidth = bankWidth + columnWidth + rowWidth
@@ -64,31 +87,30 @@ case class SdramConfig(
   def tWR = 5 + 1
 
   def tRAS = timeCycle(RAS, cycleTime_ns)
-
   def tRTP = math.max(timeCycle(RTP, cycleTime_ns), generation.burstLength / generation.dataRate)
-
-  def timeCycle(time: Int, cycTime: Int) = (time + cycTime - 1) / cycTime
-
-  def cycleTime_ns = 1000 / ddrMHZ
 
   def tRRD = math.max(timeCycle(RRD, cycleTime_ns), generation.burstLength / generation.dataRate)
 
   def tFAW = timeCycle(FAW, cycleTime_ns)
+
+  def timeCycle(time: Int, cycTime: Int) = (time + cycTime - 1) / cycTime
+
+  def cycleTime_ns = 1000 / ddrMHZ
 
   def tPhyWrlat = ddrWrLat - 2
   def tRddataEn = ddrRdLat - 2
 }
 
 case class SdramTiming(
-    generation: Int,
-    RFC: Int, // ns // Command Period (REF to ACT)
-    RAS: Int, // ns// Command Period (ACT to PRE)   Per bank
-    RP: Int, // ns // Command Period (PRE to ACT)
-    RCD: Int, // ns // Active Command To Read / Write Command Delay Time
-    WTR: Int, // ns// WRITE to READ
-    WTP: Int, // ns// WRITE to PRE (WRITE recovery time)
-    RTP: Int, // ns// READ to PRE
-    RRD: Int, // ns// ACT to ACT cross bank
-    REF: Int, // us // Refresh Cycle Time (single row)
-    FAW: Int,
-) //ns // Four ACTIVATE windows
+                        generation: Int,
+                        RFC: Int, // ns // Command Period (REF to ACT)
+                        RAS: Int, // ns// Command Period (ACT to PRE)   Per bank
+                        RP: Int, // ns // Command Period (PRE to ACT)
+                        RCD: Int, // ns // Active Command To Read / Write Command Delay Time
+                        WTR: Int, // ns// WRITE to READ
+                        WTP: Int, // ns// WRITE to PRE (WRITE recovery time)
+                        RTP: Int, // ns// READ to PRE
+                        RRD: Int, // ns// ACT to ACT cross bank
+                        REF: Int, // us // Refresh Cycle Time (single row)
+                        FAW: Int
+                      ) //ns // Four ACTIVATE windows
