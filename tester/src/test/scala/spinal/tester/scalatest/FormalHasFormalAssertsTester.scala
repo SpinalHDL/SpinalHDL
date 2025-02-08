e28a9384d624e5dd0a9d6f118e97f632ba171cc4
Justin Berger <j.david.berger@gmail.com>
2025-02-08 09:56:57 -0700

    Add usages of formalChecks and assertOrAssume into the unit tests

--- tester/src/test/scala/spinal/tester/scalatest/FormalHasFormalAssertsTester.scala
+++ tester/src/test/scala/spinal/tester/scalatest/FormalHasFormalAssertsTester.scala
@@ -21,22 +21,34 @@ case class TestBundle() extends Bundle with IMasterSlave with FormalMasterSlave 
   override def formalIsConsumerValid() = never_true === False
 }
 
-class ChildComponent extends ComponentWithFormalAsserts {
+class ChildComponent(val addHelperAssertion : Boolean = true) extends ComponentWithFormalAsserts {
   val io = new Bundle {
     val contract = slave(TestBundle())
   }
 
-  io.contract.never_true := io.contract.input === 0xdeadbeefL
+  var shifter = Reg(Bits(16 bits)) init(0)
+  shifter := (shifter << 1).resize(15 bits) ## (io.contract.input === 0xdeadbeefL)
+  io.contract.never_true := shifter.msb
+
+  override protected def formalChecks()(implicit useAssumes: Boolean): Unit = {
+    if(addHelperAssertion) {
+      // When doing formal induction with less than shifter.getBitsWidth states, we need to help the solver out --
+      // the current state of sby isn't able to resolve this property on it's own.
+      assertOrAssume(shifter === 0)
+    }
+
+    super.formalChecks()
+  }
 }
 
-class FormalChildComponent extends Component {
-  val dut = FormalDut(new ChildComponent())
+class FormalChildComponent(val addHelperAssertion : Boolean = false) extends Component {
+  val dut = FormalDut(new ChildComponent(addHelperAssertion))
   assumeInitial(ClockDomain.current.isResetActive)
 
   dut.anyseq_inputs()
 }
 
-class ParentComponent extends Component with HasFormalAsserts {
+class ParentComponent() extends Component with HasFormalAsserts {
   val io = new Bundle {
     val contract = slave(TestBundle())
   }
@@ -61,17 +73,32 @@ class FormalParentComponent(assumeInputs : Boolean = true) extends Component {
 
 class FormalHasFormalTester extends SpinalFormalFunSuite {
   test("Check child") {
-    FormalConfig.withBMC(15).doVerify(new FormalChildComponent())
-  }
-  test("Check Parent") {
-    FormalConfig.withBMC(15).doVerify(new FormalParentComponent())
+    // Show that if prove has a long enough induction length, it does not need the helper assertions.
+    FormalConfig.withProve(15).doVerify(new FormalChildComponent(addHelperAssertion = false))
   }
 
-  // Shows that if we don't assume proper inputs into the parent, that the assumptions in the child class are correctly
-  // predicated on the inputs being correct
-  test("Check Parent - no input assumes") {
+  test("CheckChildHelper") {
+    // Show that if prove has the helper assertions, it does not need a long induction length
+    FormalConfig.withProve(5).doVerify(new FormalChildComponent(addHelperAssertion = true))
+  }
+
+  test("CheckChildNoHelper") {
+    // Show that if prove does not have the helper assertions, and has too short of induction length, it fails.
     shouldFail(
-      FormalConfig.withBMC(15).doVerify(new FormalParentComponent(false))
+      FormalConfig.withProve(5).doVerify(new FormalChildComponent(addHelperAssertion = false))
+    )
+  }
+
+  test("Check Parent") {
+    // Show that when we assume the inputs are valid, it can prove it is valid.
+    FormalConfig.withProve(15).doVerify(new FormalParentComponent(assumeInputs = true))
+  }
+
+  test("Check Parent - no input assumes") {
+    // Shows that if we don't assume proper inputs into the parent, that the assumptions in the child class are correctly
+    // predicated on the inputs being correct
+    shouldFail(
+      FormalConfig.withProve(15).doVerify(new FormalParentComponent(assumeInputs = false))
     )
   }
 }
