1054c4418037b08816b1e1941e647b227f67d6ae
pluveto <zijing.zhang@proton.me>
2025-05-25 13:40:37 +0800

    Enhance AssertStatementHelper to support additional numeric and decimal types in nested sequences

--- core/src/main/scala/spinal/core/internals/Statement.scala
+++ core/src/main/scala/spinal/core/internals/Statement.scala
@@ -601,6 +601,9 @@ object AssertStatementHelper {
     def process(item: Any): Unit = {
       item match {
         case nestedSeq: Seq[_] =>
+        case m @ (_: Byte | _: Short | _: Int | _: Long | _: Float | _: Double | _: Char | _: Boolean) => builder += m.toString
+        case m: BigInt => builder += m.toString
+        case m: BigDecimal => builder += m.toString
         case other =>
           builder += other
       }
