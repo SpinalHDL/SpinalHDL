e763c94b350e46fcdd583c5ae3b14133f7269081
Inochi Amaoto <inochiama@gmail.com>
2026-03-31 15:43:42 +0800

    lib: misc: aia: Add Mux utiltys for ImsicFile
    Signed-off-by: Inochi Amaoto <inochiama@gmail.com>

--- lib/src/main/scala/spinal/lib/misc/aia/ImsicFile.scala
+++ lib/src/main/scala/spinal/lib/misc/aia/ImsicFile.scala
@@ -82,4 +82,33 @@ case class ImsicFile(hartId: Int, guestId: Int, sourceIds: Seq[Int]) extends Are
 
 object ImsicFile {
   def apply(hartId: Int, sourceIds: Seq[Int]): ImsicFile = ImsicFile(hartId, 0, sourceIds)
+
+  def currentFileStatus(files: Seq[ImsicFile], mux: UInt) = new Area {
+    assert(files.map(_.sourceIds).toSet.size == 1, "All file should have the same source configuration")
+    val sourceIds = files(0).sourceIds
+    val idWidth = files(0).idWidth
+    val vecMux = mux.resized
+
+    def toFileVec[T <: Data](f : ImsicFile => T) : Vec[T] = Vec(files.map(f(_)))
+
+    val threshold = toFileVec(_.threshold)(vecMux)
+
+    val interrupts = for ((sourceId, idx) <- sourceIds.zipWithIndex) yield new Area {
+      val id = sourceId
+      val ie = toFileVec(_.interrupts(idx).ie)(vecMux)
+      val ip = toFileVec(_.interrupts(idx).ip)(vecMux)
+    }
+
+    val identity = toFileVec(_.identity)(vecMux)
+
+    def claim(id: UInt) = new Area {
+      switch(id) {
+        for (i <- interrupts) {
+          is (i.id) {
+            i.ip.clear()
+          }
+        }
+      }
+    }
+  }
 }
