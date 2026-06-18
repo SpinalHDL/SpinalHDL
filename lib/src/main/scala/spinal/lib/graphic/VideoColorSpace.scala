8b88b30dcb0ef2208e18a38a495677776e59b778
Brian Sune <briansune@gmail.com>
2026-06-19 01:51:23 +0800

    LIB: GRAPHIC: Add Video Color Space IP
    Current graphic library provide LUT based color space conversion which
    is good but more algorithmic approach can be used.
    
    This new video color space IPs provide working ground for filters space
    conversion etc, which also includes alpha channels mixing.
    
    TEST: LIB: GRAPHIC: Add Video Color Space Tester
    
    In order to verify the error rates and sanity of the color space
    conversion an automatic testbench is provided.
    
    Remarks: Current convertor have 4 ticks of latencies
    
    Signed-off-by: Brian Sune <briansune@gmail.com>

---
+++ lib/src/main/scala/spinal/lib/graphic/VideoColorSpace.scala
@@ -0,0 +1,359 @@
+// =======================================================================
+//  ____         _                ____
+// | __ )  _ __ (_)  __ _  _ __  / ___|  _   _  _ __    ___
+// |  _ \ | '__|| | / _` || '_ \ \___ \ | | | || '_ \  / _ \
+// | |_) || |   | || (_| || | | | ___) || |_| || | | ||  __/
+// |____/ |_|   |_| \__,_||_| |_||____/  \__,_||_| |_| \___|
+//
+// =======================================================================
+// Revision: 0.9.0
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Trial Version
+//
+// Latency 4 ticks or 1 tick
+//
+// Real FPGA fitting shows good t_su and t_h @ > 175MHz
+//
+// Errors are kept @ log2(bits/pixel-elements)
+// 8bits <= +/-2 LSb ... < 2%
+//
+// Date: 2026/06
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// File: VideoColorSpace.scala
+// Designed By: BrianSune
+// Contact: briansune@gmail.com
+// =======================================================================
+
+package spinal.lib.graphic
+
+import spinal.lib._
+import spinal.core._
+
+object VideoColorSpace {
+  def apply(
+      vcsp: VideoSpaceParameter,
+      SPACE_IN: Bits = null
+  ): VideoColorSpace = {
+    val ret = VideoColorSpace(vcsp)
+    ret.io.SPACE_IN := SPACE_IN
+    ret
+  }
+}
+
+case class VideoSpaceParameter(
+    bitsPerContent: Int = 8,
+    useRGB2YUV: Boolean = true,
+    useYUV2RGB: Boolean = false,
+    stdBT470: Boolean = true,
+    stdBT601Full: Boolean = false,
+    stdBT601TV: Boolean = false
+) {
+  require(bitsPerContent > 0, "bitsPerContent must be greater than 0")
+  require(
+    (useRGB2YUV ^ useYUV2RGB) | (!(useRGB2YUV & useYUV2RGB)),
+    "Cannot use both RGB2YUV and YUV2RGB at the same time, please select one!"
+  )
+
+  require(
+    (List(
+      stdBT470,
+      stdBT601Full,
+      stdBT601TV
+    ).count(_ == true) == 1 & (useRGB2YUV ^ useYUV2RGB)) |
+      (!(useRGB2YUV & useYUV2RGB)),
+    "You must select ONE standard only!"
+  )
+}
+
+case class VideoColorSpace(
+    vcsp: VideoSpaceParameter
+) extends Component {
+  val io = new Bundle {
+    val SPACE_IN = in Bits ((vcsp.bitsPerContent * 3) bits)
+    val SPACE_OUT = out UInt ((vcsp.bitsPerContent * 3) bits)
+  }
+
+  var latency: Int = 1
+
+  if (vcsp.useRGB2YUV) {
+    val rgb2yuv = VideoRGB2YUV(vcsp)
+
+    rgb2yuv.io.R := RegNext(io.SPACE_IN(3 * vcsp.bitsPerContent - 1 downto 2 * vcsp.bitsPerContent))
+    rgb2yuv.io.G := RegNext(io.SPACE_IN(2 * vcsp.bitsPerContent - 1 downto vcsp.bitsPerContent))
+    rgb2yuv.io.B := RegNext(io.SPACE_IN(1 * vcsp.bitsPerContent - 1 downto 0))
+    io.SPACE_OUT(3 * vcsp.bitsPerContent - 1 downto 2 * vcsp.bitsPerContent) := rgb2yuv.io.Y
+    io.SPACE_OUT(2 * vcsp.bitsPerContent - 1 downto vcsp.bitsPerContent) := rgb2yuv.io.U
+    io.SPACE_OUT(1 * vcsp.bitsPerContent - 1 downto 0) := rgb2yuv.io.V
+    latency = 4
+  } else if (vcsp.useYUV2RGB) {
+    val rgb2yuv = VideoYUV2RGB(vcsp)
+
+    rgb2yuv.io.Y := RegNext(io.SPACE_IN(3 * vcsp.bitsPerContent - 1 downto 2 * vcsp.bitsPerContent))
+    rgb2yuv.io.U := RegNext(io.SPACE_IN(2 * vcsp.bitsPerContent - 1 downto vcsp.bitsPerContent))
+    rgb2yuv.io.V := RegNext(io.SPACE_IN(1 * vcsp.bitsPerContent - 1 downto 0))
+    io.SPACE_OUT(3 * vcsp.bitsPerContent - 1 downto 2 * vcsp.bitsPerContent) := rgb2yuv.io.R
+    io.SPACE_OUT(2 * vcsp.bitsPerContent - 1 downto vcsp.bitsPerContent) := rgb2yuv.io.G
+    io.SPACE_OUT(1 * vcsp.bitsPerContent - 1 downto 0) := rgb2yuv.io.B
+    latency = 4
+  } else {
+    io.SPACE_OUT := Delay(io.SPACE_IN.asUInt, 1)
+    latency = 1
+  }
+}
+
+case class VideoYUV2RGB(
+    vcsp: VideoSpaceParameter
+) extends Component {
+  val io = new Bundle {
+    val Y = in Bits (vcsp.bitsPerContent bits)
+    val U = in Bits (vcsp.bitsPerContent bits)
+    val V = in Bits (vcsp.bitsPerContent bits)
+    val R = out UInt (vcsp.bitsPerContent bits)
+    val G = out UInt (vcsp.bitsPerContent bits)
+    val B = out UInt (vcsp.bitsPerContent bits)
+  }
+
+  def zp(value: Bits, factor: Int) = {
+    val paddingWidth = factor + (vcsp.bitsPerContent - 8)
+    (U(0, 1 bits) ## value ## U(0, paddingWidth bits)).asUInt
+  }
+
+  def sumSfts(input: Bits, formula: List[Int]): UInt = {
+    val head = zp(input, formula.head)
+    formula.tail.foldLeft(head) { (acc, shift) =>
+      val term = zp(input, shift.abs)
+      if (shift >= 0) acc +^ term
+      else acc -^ term
+    }
+  }
+
+  def clip(value: SInt): UInt = {
+    val result = UInt(vcsp.bitsPerContent bits)
+    when(value > ((1 << vcsp.bitsPerContent)) - 1) {
+      result := (1 << vcsp.bitsPerContent) - 1
+    } elsewhen (value < 0) {
+      result := 0
+    } otherwise {
+      result := value.asUInt.resize(vcsp.bitsPerContent bits)
+    }
+    result
+  }
+
+  // Y∈ [0,1] U,V∈[-0.5,0.5]
+  // BT.470
+  val BT_470 = List(
+    // R:
+    // 1 * Y + 0 * U + 1.13983 * V
+    List(List(vcsp.bitsPerContent), List(), List(vcsp.bitsPerContent, 5, 2)),
+    // G:
+    // 1 * Y - 0.39465 * U - 0.5806 * V
+    List(List(vcsp.bitsPerContent), List(6, 5, 2, 0), List(7, 4, 2)),
+    // V:
+    // 1 * Y + 2.03211 * U + 0 * V
+    List(List(vcsp.bitsPerContent), List(vcsp.bitsPerContent + 1, 3), List())
+  )
+
+  // BT.601_FULL
+  val BT_601_FULL = List(
+    // R:
+    // 1 * Y + 0 * U + 1.4075 * V
+    List(List(vcsp.bitsPerContent), List(), List(vcsp.bitsPerContent, 6, 5, 3)),
+    // G:
+    // 1 * Y - 0.3455 * U - 0.7169 * V
+    List(List(vcsp.bitsPerContent), List(6, 4, 3), List(7, 6, -3)),
+    // V:
+    // 1 * Y + 1.779 * U + 0 * V
+    List(List(vcsp.bitsPerContent), List(vcsp.bitsPerContent, 7, 6, 3), List())
+  )
+
+  // BT.601_TV
+  val BT_601_TV = List(
+    // R:
+    // 1.164 * Y + 0 * U + 1.4075 * V
+    List(List(vcsp.bitsPerContent, 5, 3, 1), List(), List(vcsp.bitsPerContent, 7, 5, -3)),
+    // G:
+    // 1.164 * Y - 0.3455 * U - 0.7169 * V
+    List(List(vcsp.bitsPerContent, 5, 3, 1), List(6, 5, 3), List(7, 6, 4)),
+    // V:
+    // 1.164 * Y + 1.779 * U + 0 * V
+    List(List(vcsp.bitsPerContent, 5, 3, 1), List(vcsp.bitsPerContent + 1, 2), List())
+  )
+
+  val BT_standard = vcsp match {
+    case v if v.stdBT601Full => BT_601_FULL
+    case v if v.stdBT601TV   => BT_601_TV
+    case _                   => BT_470
+  }
+
+  val r_offset = ((vcsp match {
+    case v if v.stdBT601Full => 1.4075
+    case v if v.stdBT601TV   => List(1.164 / 8, 1.596).sum
+    case _                   => 1.13983
+  }) * (1 << (vcsp.bitsPerContent - 1))).toInt
+
+  val g_offset = ((vcsp match {
+    case v if v.stdBT601Full => List(0.3455, 0.7169).sum
+    case v if v.stdBT601TV   => List(-1.164 / 8, 0.392, 0.812).sum
+    case _                   => List(0.39465, 0.5806).sum
+  }) * (1 << (vcsp.bitsPerContent - 1))).toInt
+
+  val b_offset = ((vcsp match {
+    case v if v.stdBT601Full => 1.779
+    case v if v.stdBT601TV   => List(1.164 / 8, 2.016).sum
+    case _                   => 2.03211
+  }) * (1 << (vcsp.bitsPerContent - 1))).toInt
+
+  val r_y = RegNext(sumSfts(io.Y, BT_standard(0)(0)))
+  val r_v = RegNext(sumSfts(io.V, BT_standard(0)(2)))
+  val r_sum = RegNext((r_y + r_v) >> vcsp.bitsPerContent)
+  val r = RegNext(clip(r_sum.asSInt - S(r_offset)))
+
+  val g_y = RegNext(sumSfts(io.Y, BT_standard(1)(0)))
+  val g_u = RegNext(sumSfts(io.U, BT_standard(1)(1)))
+  val g_v = RegNext(sumSfts(io.V, BT_standard(1)(2)))
+  val g_sum = RegNext((g_y.asSInt - g_u.asSInt - g_v.asSInt) >> vcsp.bitsPerContent)
+  val g = RegNext(clip(g_sum + S(g_offset)))
+
+  val b_y = RegNext(sumSfts(io.Y, BT_standard(2)(0)))
+  val b_u = RegNext(sumSfts(io.U, BT_standard(2)(1)))
+  val b_sum = RegNext((b_y + b_u) >> vcsp.bitsPerContent)
+  val b = RegNext(clip(b_sum.asSInt - S(b_offset)))
+
+  io.R := r
+  io.G := g
+  io.B := b
+}
+
+case class VideoRGB2YUV(
+    vcsp: VideoSpaceParameter
+) extends Component {
+  val io = new Bundle {
+    val R = in Bits (vcsp.bitsPerContent bits)
+    val G = in Bits (vcsp.bitsPerContent bits)
+    val B = in Bits (vcsp.bitsPerContent bits)
+    val Y = out UInt (vcsp.bitsPerContent bits)
+    val U = out UInt (vcsp.bitsPerContent bits)
+    val V = out UInt (vcsp.bitsPerContent bits)
+  }
+
+  def zp(value: Bits, factor: Int) = {
+    val paddingWidth = factor + (vcsp.bitsPerContent - 8)
+    (U(0, 1 bits) ## value ## U(0, paddingWidth bits)).asUInt
+  }
+
+  def sumSfts(input: Bits, formula: List[Int]): UInt = {
+    val head = zp(input, formula.head)
+    formula.tail.foldLeft(head) { (acc, shift) =>
+      val term = zp(input, shift.abs)
+      if (shift >= 0) acc +^ term
+      else acc -^ term
+    }
+  }
+
+  def clip(value: SInt): UInt = {
+    val result = UInt(vcsp.bitsPerContent bits)
+    val mid = (U(1) << (vcsp.bitsPerContent - 1)).resize(vcsp.bitsPerContent + 1 bits).asSInt
+    when(value > ((1 << (vcsp.bitsPerContent - 1)) - 1)) {
+      result := (1 << vcsp.bitsPerContent) - 1
+    } elsewhen (value < -(1 << (vcsp.bitsPerContent - 1))) {
+      result := 0
+    } otherwise {
+      result := (mid + value).asUInt.resize(vcsp.bitsPerContent bits)
+    }
+    result
+  }
+
+  // Y∈ [0,1] U,V∈[-0.5,0.5]
+  // BT.470: https://www.itu.int/rec/R-REC-BT.470/en
+  val BT_470 = List(
+    // Y:
+    // 0.299 * R + 0.587 * G + 0.114 * B
+    List(List(6, 3, 2), List(7, 4, 2, 1), List(4, 3, 2, 0)),
+    // U:
+    // -0.14713 * R - 0.28886 * G + 0.436 * B + (mid point)
+    List(List(5, 2, 0), List(6, 3, 1), List(6, 5, 4)),
+    // V:
+    // 0.615 * R - 0.51499 * G - 0.10001 * B + (mid point)
+    List(List(7, 5, -1), List(7, 2), List(4, 3, 1))
+  )
+
+  // Y∈ [0,1] U,V∈[-0.5,0.5]
+  // BT.601.full / [analog  + mid-point]
+  val BT_601_FULL = List(
+    // Y:
+    // 0.299 * R + 0.587 * G + 0.114 * B
+    List(List(6, 3, 2), List(7, 4, 2, 1), List(4, 3, 2, 0)),
+    // U:
+    // -0.169 * R - 0.331 * G + 0.5 * B + (mid point)
+    List(List(5, 3, 1, 0), List(6, 4, 2), List(7)),
+    // V:
+    // 0.5 * R - 0.419 * G - 0.081 * B + (mid point)
+    List(List(7), List(7, -4, -2), List(4, 2, 0))
+  )
+
+  // Y∈[16,235] Cb∈[16-240] Cr∈[16-240]
+  // BT.601.tv
+  val BT_601_TV = List(
+    // Y:
+    // 0.257 * R + 0.504 * G + 0.098 * B + (full / 16)
+    List(List(6, 1), List(7, 0), List(4, 3, 0)),
+    // U/Cb:
+    // -0.148 * R - 0.291 * G + 0.439 * B + (mid point)
+    List(List(5, 2, 1), List(6, 3, 1), List(7, -4)),
+    // V/Cr:
+    // 0.439 * R - 0.368 * G - 0.071 * B + (mid point)
+    List(List(7, -4), List(6, 5, -1), List(4, 1))
+  )
+
+  val BT_standard = vcsp match {
+    case v if v.stdBT601Full => BT_601_FULL
+    case v if v.stdBT601TV   => BT_601_TV
+    case _                   => BT_470
+  }
+
+  val y_r = RegNext(sumSfts(io.R, BT_standard(0)(0)))
+  val y_g = RegNext(sumSfts(io.G, BT_standard(0)(1)))
+  val y_b = RegNext(sumSfts(io.B, BT_standard(0)(2)))
+
+  val u_r = RegNext(sumSfts(io.R, BT_standard(1)(0)))
+  val u_g = RegNext(sumSfts(io.G, BT_standard(1)(1)))
+  val u_b = RegNext(sumSfts(io.B, BT_standard(1)(2)))
+
+  val v_r = RegNext(sumSfts(io.R, BT_standard(2)(0)))
+  val v_g = RegNext(sumSfts(io.G, BT_standard(2)(1)))
+  val v_b = RegNext(sumSfts(io.B, BT_standard(2)(2)))
+
+  val y_offset = U(if (vcsp.stdBT601TV) (1 << (vcsp.bitsPerContent - 4)) else 0)
+  val y_sum = RegNext((y_r + y_g + y_b) >> vcsp.bitsPerContent)
+  val y = RegNext((y_sum + y_offset).resize(vcsp.bitsPerContent bits))
+
+  val u_sum = RegNext((u_b.asSInt - u_r.asSInt - u_g.asSInt) >> vcsp.bitsPerContent)
+  val u = RegNext(clip(u_sum))
+
+  val v_sum = RegNext((v_r.asSInt - v_g.asSInt - v_b.asSInt) >> vcsp.bitsPerContent)
+  val v = RegNext(clip(v_sum))
+
+  io.Y := y
+  io.U := u
+  io.V := v
+}
+
+object GenerateVideoColorSpace {
+  def main(args: Array[String]): Unit = {
+
+    val report = SpinalVerilog(
+      new VideoColorSpace(
+        VideoSpaceParameter(
+          bitsPerContent = 8,
+          useRGB2YUV = false,
+          useYUV2RGB = true,
+          // stdBT470 = true
+          // stdBT601Full = true
+          stdBT601TV = true
+        )
+      )
+    )
+
+    report.printPruned()
+  }
+}
---
+++ tester/src/test/scala/spinal/lib/graphic/SpinalSimVideoColorSpace.scala
@@ -0,0 +1,205 @@
+// =======================================================================
+//  ____         _                ____                      
+// | __ )  _ __ (_)  __ _  _ __  / ___|  _   _  _ __    ___ 
+// |  _ \ | '__|| | / _` || '_ \ \___ \ | | | || '_ \  / _ \
+// | |_) || |   | || (_| || | | | ___) || |_| || | | ||  __/
+// |____/ |_|   |_| \__,_||_| |_||____/  \__,_||_| |_| \___|
+//                                                          
+// =======================================================================
+// Revision: 0.9.0
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Trial Version
+// Date: 2026/06
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// File: SpinalSimVideoTestPattern.scala
+// Designed By: BrianSune
+// Contact: briansune@gmail.com
+// =======================================================================
+
+package spinal.lib.graphic
+
+import spinal.core._
+import spinal.core.sim._
+import spinal.tester.{SpinalAnyFunSuite, SpinalSimTester}
+
+
+class SpinalSimVideoColorSpace extends SpinalAnyFunSuite {
+  def runVideoSim(
+    vcsp: VideoSpaceParameter,
+    name: String
+  ): Unit = {
+    // 1. Compile the design
+    val compiled = SimConfig
+    // .withWave
+    .withConfig(SpinalConfig(verbose = true)).compile(
+      VideoColorSpace(vcsp)
+    )
+
+    val full = (1 << (vcsp.bitsPerContent)) - 1
+    val half = (1 << (vcsp.bitsPerContent - 1))
+    val hex = (1 << (vcsp.bitsPerContent - 4))
+
+    def clamp(i: Int): Int = Math.max(0, Math.min(full, i))
+
+    def bt470(r: Int, g: Int, b: Int): (Int, Int, Int) = {
+      val y = (0.299 * r + 0.587 * g + 0.114 * b).toInt
+      val u = (0.436 * b - 0.14713 * r - 0.28886 * g).toInt
+      val v = (0.615 * r - 0.51499 * g - 0.10001 * b).toInt
+      (y, clamp(u + half), clamp(v + half))
+    }
+
+    def bt470_rev(y: Int, u: Int, v: Int): (Int, Int, Int) = {
+      val r = (y + 1.13983 * (v-half)).toInt
+      val g = (y -0.39465 * (u-half) - 0.5806 * (v-half)).toInt
+      val b = (y + 2.03211 * (u-half)).toInt
+      (clamp(r), clamp(g), clamp(b))
+    }
+
+    def bt601_full(r: Int, g: Int, b: Int): (Int, Int, Int) = {
+      val y = (0.299 * r + 0.587 * g + 0.114 * b).toInt
+      val u = (0.5 * b - 0.169 * r - 0.331 * g).toInt
+      val v = (0.5 * r - 0.419 * g - 0.081 * b).toInt
+      (y, clamp(u + half), clamp(v + half))
+    }
+
+    def bt601_full_rev(y: Int, u: Int, v: Int): (Int, Int, Int) = {
+      val r = (y + 1.4075 * (v-half)).toInt
+      val g = (y -0.3455 * (u-half) - 0.7169 * (v-half)).toInt
+      val b = (y + 1.799 * (u-half)).toInt
+      (clamp(r), clamp(g), clamp(b))
+    }
+
+    def bt601_tv(r: Int, g: Int, b: Int): (Int, Int, Int) = {
+      val y = (0.257 * r + 0.504 * g + 0.098 * b).toInt
+      val u = (0.439 * b - 0.148 * r - 0.291 * g).toInt
+      val v = (0.439 * r - 0.368 * g - 0.071 * b).toInt
+      (y + hex, clamp(u + half), clamp(v + half))
+    }
+
+    def bt601_tv_rev(y: Int, u: Int, v: Int): (Int, Int, Int) = {
+      val r = (1.164 * (y - hex) + 1.596 * (v-half)).toInt
+      val g = (1.164 * (y - hex) - 0.392 * (u-half) - 0.812 * (v-half)).toInt
+      val b = (1.164 * (y - hex) + 2.016 * (u-half)).toInt
+      (clamp(r), clamp(g), clamp(b))
+    }
+
+    // 2. Run the simulation
+    compiled.doSim(name) { dut =>
+      dut.clockDomain.forkStimulus(period = 10) // 100MHz clock
+
+      val inputQueue = scala.collection.mutable.Queue[(Int, Int, Int)]()
+      
+      dut.clockDomain.waitSampling()
+      dut.clockDomain.waitSampling()
+      
+      def check(d0: Int, d1: Int, d2: Int): Unit = {
+        val in0 = d0 & full
+        val in1 = d1 & full
+        val in2 = d2 & full
+        inputQueue.enqueue((in0, in1, in2))
+        val in_cat = (in0 << (2 * vcsp.bitsPerContent)) | (in1 << (vcsp.bitsPerContent)) | in2
+        
+        dut.io.SPACE_IN #= in_cat
+        dut.clockDomain.waitSampling()
+        val sim_o = dut.io.SPACE_OUT.toBigInt
+
+        if (inputQueue.size > 4) {
+          val (c0, c1, c2) = inputQueue.dequeue()
+
+          val a0 = (sim_o >> (2 * vcsp.bitsPerContent) & full);
+          val a1 = (sim_o >> (vcsp.bitsPerContent) & full);
+          val a2 = (sim_o & full);
+          
+          println(s"Input R:$c0 G:$c1 B:$c2")
+          val (e0, e1, e2) = vcsp match {
+            case v if v.stdBT601Full & v.useRGB2YUV => bt601_full(c0, c1, c2)
+            case v if v.stdBT601TV & v.useRGB2YUV   => bt601_tv(c0, c1, c2)
+            case v if v.stdBT470 & v.useRGB2YUV     => bt470(c0, c1, c2)
+            case v if v.stdBT470 & v.useYUV2RGB     => bt470_rev(c0, c1, c2)
+            case v if v.stdBT601Full & v.useYUV2RGB => bt601_full_rev(c0, c1, c2)
+            case v if v.stdBT601TV & v.useYUV2RGB   => bt601_tv_rev(c0, c1, c2)
+            case _                                  => (c0, c1, c2)
+          }
+
+          println(s"Expected $e0\t|\tGot $a0")
+          println(s"Expected $e1\t|\tGot $a1")
+          println(s"Expected $e2\t|\tGot $a2")
+          assert((a0 - e0).abs <= log2Up(vcsp.bitsPerContent), "Delta too much!")
+          assert((a1 - e1).abs <= log2Up(vcsp.bitsPerContent), "Delta too much!")
+          assert((a2 - e2).abs <= log2Up(vcsp.bitsPerContent), "Delta too much!")
+        }
+      }
+
+      if(vcsp.useRGB2YUV){
+        List(
+          (full, 0, 0),
+          (0, full, 0),
+          (0, 0, full),
+          (full, 0, full),
+          (full, full, 0),
+          (0, full, full),
+          (0, 0, 0),
+          (full, full, full),
+          (full, full, full),
+          (full, full, full),
+          (full, full, full),
+          (full, full, full),
+        ).foreach((check _).tupled)
+      }else if(vcsp.useYUV2RGB){
+        List(
+          (0x4C, 0x55, 0xFF), // Red
+          (0x96, 0x2C, 0x15), // Green
+          (0x1D, 0xFF, 0x6B), // Blue
+          (0x69, 0xD4, 0xEA), // Magenta
+          (0xE2, 0x01, 0x95), // Yellow
+          (0xB3, 0xB0, 0x10), // Cyan
+          (0x00, 0x80, 0x80), // Black
+          (0xFF, 0x80, 0x80), // White
+          (0xFF, 0x80, 0x80),
+          (0xFF, 0x80, 0x80),
+          (0xFF, 0x80, 0x80),
+          (0xFF, 0x80, 0x80)
+        ).foreach((check _).tupled)
+      }
+    }
+  }
+
+  test("VideoColorSpace") {
+
+    runVideoSim(
+      VideoSpaceParameter(
+        // bitsPerContent = 10,
+        useRGB2YUV = true,
+        useYUV2RGB = false,
+        stdBT470 = true,
+        stdBT601Full = false,
+        stdBT601TV = false
+      ),
+      "Standard BT.470 YUV2RGB"
+    )
+
+    runVideoSim(
+      VideoSpaceParameter(
+        // bitsPerContent = 10,
+        useRGB2YUV = true,
+        useYUV2RGB = false,
+        stdBT470 = false,
+        stdBT601Full = true,
+        stdBT601TV = false
+      ),
+      "Standard BT.601-Full YUV2RGB"
+    )
+
+    runVideoSim(
+      VideoSpaceParameter(
+        // bitsPerContent = 10,
+        useRGB2YUV = true,
+        useYUV2RGB = false,
+        stdBT470 = false,
+        stdBT601Full = false,
+        stdBT601TV = true
+      ),
+      "Standard BT.601-TV YUV2RGB"
+    )
+  }
+}
